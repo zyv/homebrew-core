b7f4898ce425d08241fd6f5f02442bb912440274
Patrick Linnane <patrick@linnane.io>
2023-11-04 07:35:42 -0700

    boost: revision bump (icu4c 74.2)

--- Formula/b/boost.rb
+++ Formula/b/boost.rb
@@ -4,6 +4,7 @@ class Boost < Formula
   url "https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.xz"
   sha256 "2e64e5d79a738d0fa6fb546c6e5c2bd28f88d268a2a080546f74e5ff98f29d0e"
   license "BSL-1.0"
+  revision 1
   head "https://github.com/boostorg/boost.git", branch: "master"
 
   livecheck do
