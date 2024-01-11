b1dbe7afb89d1860ed459b57d4e631de7c902462
Michael Cho <michael@michaelcho.dev>
2024-01-11 14:18:50 -0500

    postgis 3.4.2
    Signed-off-by: Michael Cho <michael@michaelcho.dev>

--- Formula/p/postgis.rb
+++ Formula/p/postgis.rb
@@ -1,10 +1,9 @@
 class Postgis < Formula
   desc "Adds support for geographic objects to PostgreSQL"
   homepage "https://postgis.net/"
-  url "https://download.osgeo.org/postgis/source/postgis-3.3.4.tar.gz"
-  sha256 "9d41eaef70e811a4fe2f4a431d144c0c57ce17c2c1a3c938ddaf4e5a3813b0d8"
+  url "https://download.osgeo.org/postgis/source/postgis-3.4.2.tar.gz"
+  sha256 "c8c874c00ba4a984a87030af6bf9544821502060ad473d5c96f1d4d0835c5892"
   license "GPL-2.0-or-later"
-  revision 3
 
   livecheck do
     url "https://download.osgeo.org/postgis/source/"
@@ -53,8 +52,6 @@ class Postgis < Formula
     # C++17 is required.
     ENV.append "CXXFLAGS", "-std=c++17"
 
-    ENV["PG_CONFIG"] = postgresql.opt_bin/"pg_config"
-
     args = [
       "--with-projdir=#{Formula["proj"].opt_prefix}",
       "--with-jsondir=#{Formula["json-c"].opt_prefix}",
@@ -68,36 +65,19 @@ class Postgis < Formula
     ]
 
     system "./autogen.sh" if build.head?
-    # Fixes config/install-sh: No such file or directory
-    # This is caused by a misalignment between ./configure in postgresql@14 and postgis
-    mv "build-aux", "config"
-    inreplace %w[configure utils/Makefile.in] do |s|
-      s.gsub! "build-aux", "config"
-    end
-    system "./configure", *args
+    # Pretend to install into HOMEBREW_PREFIX to allow PGXS to find PostgreSQL binaries
+    system "./configure", *args, *std_configure_args(prefix: HOMEBREW_PREFIX)
     system "make"
-
-    # Install to a staging directory to circumvent the hardcoded install paths
-    # set by the PGXS makefiles.
-    mkdir "stage"
-    system "make", "install", "DESTDIR=#{buildpath}/stage"
-
-    # Some files are stored in the stage directory with the cellar prefix of
-    # the version of postgresql used to build postgis.  Since we copy these
-    # files into the postgis keg and symlink them to HOMEBREW_PREFIX, postgis
-    # only needs to be rebuilt when there is a new major version of postgresql.
-    postgresql_prefix = postgresql.prefix.realpath
-    postgresql_stage_path = File.join("stage", postgresql_prefix)
-    bin.install (buildpath/postgresql_stage_path/"bin").children
-    doc.install (buildpath/postgresql_stage_path/"share/doc").children
-
-    stage_path = File.join("stage", HOMEBREW_PREFIX)
-    lib.install (buildpath/stage_path/"lib").children
-    share.install (buildpath/stage_path/"share").children
+    # Override the hardcoded install paths set by the PGXS makefiles
+    system "make", "install", "bindir=#{bin}",
+                              "docdir=#{doc}",
+                              "mandir=#{man}",
+                              "pkglibdir=#{lib/postgresql.name}",
+                              "datadir=#{share/postgresql.name}",
+                              "PG_SHAREDIR=#{share/postgresql.name}"
 
     # Extension scripts
     bin.install %w[
-      utils/create_undef.pl
       utils/create_upgrade.pl
       utils/postgis_restore.pl
       utils/profile_intersects.pl
