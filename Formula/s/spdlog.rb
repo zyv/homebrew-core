1abf48d0e24cc74e828f050a8c7cdf926883757b
Porkepix <porkepix@gmail.com>
2024-01-12 13:39:33 +0100

    spdlog 1.13.0

--- Formula/s/spdlog.rb
+++ Formula/s/spdlog.rb
@@ -1,8 +1,8 @@
 class Spdlog < Formula
   desc "Super fast C++ logging library"
   homepage "https://github.com/gabime/spdlog"
-  url "https://github.com/gabime/spdlog/archive/refs/tags/v1.12.0.tar.gz"
-  sha256 "4dccf2d10f410c1e2feaff89966bfc49a1abb29ef6f08246335b110e001e09a9"
+  url "https://github.com/gabime/spdlog/archive/refs/tags/v1.13.0.tar.gz"
+  sha256 "534f2ee1a4dcbeb22249856edfb2be76a1cf4f708a20b0ac2ed090ee24cfdbc9"
   license "MIT"
   head "https://github.com/gabime/spdlog.git", branch: "v1.x"
 
