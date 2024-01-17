786aa7d065ca7323e3cb6061652ed14d1c02bf86
Rui Chen <rui@chenrui.dev>
2024-01-17 08:12:21 -0500

    rustfmt 1.7.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    autobump: add rustfmt
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r/rustfmt.rb
+++ Formula/r/rustfmt.rb
@@ -1,8 +1,8 @@
 class Rustfmt < Formula
   desc "Format Rust code"
   homepage "https://rust-lang.github.io/rustfmt/"
-  url "https://github.com/rust-lang/rustfmt/archive/refs/tags/v1.6.0.tar.gz"
-  sha256 "32ba647a9715efe2699acd3d011e9f113891be02ac011d314b955a9beea723a2"
+  url "https://github.com/rust-lang/rustfmt/archive/refs/tags/v1.7.0.tar.gz"
+  sha256 "9f228d6192104a57efd73b20b8f0a2189c920373655b0c17b75758afab805be8"
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/rust-lang/rustfmt.git", branch: "master"
 
