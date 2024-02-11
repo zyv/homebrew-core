5e7a57534f000eb81f18c66b7479c62d120dc479
Rui Chen <rui@chenrui.dev>
2024-02-11 22:17:11 +0100

    gator 3.15.0

--- Formula/g/gator.rb
+++ Formula/g/gator.rb
@@ -1,8 +1,8 @@
 class Gator < Formula
   desc "CLI Utility for Open Policy Agent Gatekeeper"
   homepage "https://open-policy-agent.github.io/gatekeeper/website/docs/gator"
-  url "https://github.com/open-policy-agent/gatekeeper/archive/refs/tags/v3.14.0.tar.gz"
-  sha256 "85fd4da39be9b852f6be8ae519d091151e4991f8ba7c7870d705f396086e8038"
+  url "https://github.com/open-policy-agent/gatekeeper/archive/refs/tags/v3.15.0.tar.gz"
+  sha256 "956c954163dac8aa2a942cefdff0c5ced681769083821de06d7d3bc7cce6f838"
   license "Apache-2.0"
   head "https://github.com/open-policy-agent/gatekeeper.git", branch: "master"
 
