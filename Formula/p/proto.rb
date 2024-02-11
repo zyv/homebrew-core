c46357bdd7ebcd3edd5f66a52e4a52e68b08efb5
Casper Lindschouw <casper@lindschouw.me>
2024-02-11 21:30:07 +0100

    proto 0.30.0 (new formula)
    Co-authored-by: Patrick Linnane <patrick@linnane.io>
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

---
+++ Formula/p/proto.rb
@@ -0,0 +1,50 @@
+class Proto < Formula
+  desc "Pluggable multi-language version manager"
+  homepage "https://moonrepo.dev/proto"
+  url "https://github.com/moonrepo/proto/archive/refs/tags/v0.30.0.tar.gz"
+  sha256 "a564e4c8a4d2483b19e02bd1b3cdac5db4ad61cc0e2d217203f0a641897ba7f4"
+  license "MIT"
+  head "https://github.com/moonrepo/proto.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "xz"
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/cli")
+    generate_completions_from_executable(bin/"proto", "completions", "--shell")
+
+    bin.each_child do |f|
+      basename = f.basename
+
+      (libexec/"bin").install f
+      (bin/basename).write_env_script libexec/"bin"/basename, PROTO_INSTALL_DIR: opt_prefix/"bin"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      To finish the installation, run:
+        proto setup
+    EOS
+  end
+
+  test do
+    system bin/"proto", "install", "node", "19.0.1"
+    node = shell_output("#{bin}/proto bin node").chomp
+    assert_match "19.0.1", shell_output("#{node} --version")
+
+    path = testpath/"test.js"
+    path.write "console.log('hello');"
+    output = shell_output("#{testpath}/.proto/bin/node #{path}").strip
+    assert_equal "hello", output
+  end
+end
