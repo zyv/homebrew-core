fa5cb31f8c6b50574a90b517d941e7aa7228947e
Alexander Frisch <crunchtime-ali@users.noreply.github.com>
2024-02-12 00:56:04 +0000

    stgit 2.4.4

--- Formula/s/stgit.rb
+++ Formula/s/stgit.rb
@@ -1,8 +1,8 @@
 class Stgit < Formula
   desc "Manage Git commits as a stack of patches"
   homepage "https://stacked-git.github.io"
-  url "https://github.com/stacked-git/stgit/releases/download/v2.4.3/stgit-2.4.3.tar.gz"
-  sha256 "4a749fb6c521764523c8a2f583c6f0123b060c589133696615d5588d43be5027"
+  url "https://github.com/stacked-git/stgit/releases/download/v2.4.4/stgit-2.4.4.tar.gz"
+  sha256 "c789091819e9a36dab4cc797e7000e142c49abe8440098121bc9d9123279785b"
   license "GPL-2.0-only"
   head "https://github.com/stacked-git/stgit.git", branch: "master"
 
