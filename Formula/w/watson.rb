13e5aa0860b07c8fc3a777d0cd5ded7738377c31
Michka Popoff <michkapopoff@gmail.com>
2024-02-12 14:32:27 +0100

    watson: get rid of six

--- Formula/w/watson.rb
+++ Formula/w/watson.rb
@@ -24,7 +24,6 @@ class Watson < Formula
   depends_on "python-click"
   depends_on "python-dateutil"
   depends_on "python@3.12"
-  depends_on "six"
 
   resource "arrow" do
     url "https://files.pythonhosted.org/packages/2e/00/0f6e8fcdb23ea632c866620cc872729ff43ed91d284c866b515c6342b173/arrow-1.3.0.tar.gz"
