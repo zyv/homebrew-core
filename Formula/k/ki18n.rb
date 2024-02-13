fbdfa983c53957576adf6a4e72ea0315f434c67b
Rui Chen <rui@chenrui.dev>
2024-02-13 16:33:19 +0100

    ki18n 5.115.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/k/ki18n.rb
+++ Formula/k/ki18n.rb
@@ -8,8 +8,8 @@ class Ki18n < Formula
   ]
 
   stable do
-    url "https://download.kde.org/stable/frameworks/5.114/ki18n-5.114.0.tar.xz"
-    sha256 "1ba88bf8b6dcbe0136f193839a3e49354fab400c69f3b71e355e95ceb81ae0f9"
+    url "https://download.kde.org/stable/frameworks/5.115/ki18n-5.115.0.tar.xz"
+    sha256 "d4fc34762137b5f90df78294370ffb345b6932552137359d15cdd157dbd7d6fd"
     depends_on "qt@5"
   end
 
