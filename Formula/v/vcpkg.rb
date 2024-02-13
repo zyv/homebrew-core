d8d28948e646f636471745a5802e1e9c5421b482
Rui Chen <rui@chenrui.dev>
2024-02-13 18:25:20 +0100

    vcpkg 2024.02.07

--- Formula/v/vcpkg.rb
+++ Formula/v/vcpkg.rb
@@ -1,9 +1,9 @@
 class Vcpkg < Formula
   desc "C++ Library Manager"
   homepage "https://github.com/microsoft/vcpkg"
-  url "https://github.com/microsoft/vcpkg-tool/archive/refs/tags/2024-02-05.tar.gz"
-  version "2024.02.05"
-  sha256 "b816b7f49e8277ea7bb34ec836f7c24d6364e25cc9c1a128f84084d96d2760ad"
+  url "https://github.com/microsoft/vcpkg-tool/archive/refs/tags/2024-02-07.tar.gz"
+  version "2024.02.07"
+  sha256 "e4a659e447f73c988cd3e268a88f31fe402037977ecf60cdbec67d4411df08c8"
   license "MIT"
   head "https://github.com/microsoft/vcpkg-tool.git", branch: "main"
 
