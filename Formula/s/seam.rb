bd24cafb55f0ba49f36ddc71bdc4cdf37a767225
Patrick Linnane <patrick@linnane.io>
2024-02-13 03:31:36 +0000

    seam: add 0.0.39 bottle.

--- Formula/s/seam.rb
+++ Formula/s/seam.rb
@@ -7,6 +7,16 @@ class Seam < Formula
   sha256 "af448c0864801fe34466b2cb5c397a7774fcbfe3bbdae23bf046a1cd44fa4e09"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c30df84acb3804850cf637aa16ad8380532c7bd87c0063a6408b73c013090791"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c30df84acb3804850cf637aa16ad8380532c7bd87c0063a6408b73c013090791"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c30df84acb3804850cf637aa16ad8380532c7bd87c0063a6408b73c013090791"
+    sha256 cellar: :any_skip_relocation, sonoma:         "40ac16e4f218c19217742985c75aa2a5510cf97285ea6709e3d4b6d525dcdef8"
+    sha256 cellar: :any_skip_relocation, ventura:        "40ac16e4f218c19217742985c75aa2a5510cf97285ea6709e3d4b6d525dcdef8"
+    sha256 cellar: :any_skip_relocation, monterey:       "40ac16e4f218c19217742985c75aa2a5510cf97285ea6709e3d4b6d525dcdef8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c30df84acb3804850cf637aa16ad8380532c7bd87c0063a6408b73c013090791"
+  end
+
   depends_on "node"
 
   def install
