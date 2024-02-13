f739aa56b31947254e979273a315047d06947026
Rui Chen <rui@chenrui.dev>
2024-02-13 16:34:55 +0100

    kiota 1.11.1

--- Formula/k/kiota.rb
+++ Formula/k/kiota.rb
@@ -1,8 +1,8 @@
 class Kiota < Formula
   desc "OpenAPI based HTTP Client code generator"
   homepage "https://aka.ms/kiota/docs"
-  url "https://github.com/microsoft/kiota/archive/refs/tags/v1.11.0.tar.gz"
-  sha256 "be2ff164d54ddbf5a4c57ce9f797f2367df2741c51885262ad6ae688abd5025d"
+  url "https://github.com/microsoft/kiota/archive/refs/tags/v1.11.1.tar.gz"
+  sha256 "cab39c2bffd20db3c8f31653ba1f0cb9fa3be785f38ff62422be76a34030dee6"
   license "MIT"
   head "https://github.com/microsoft/kiota.git", branch: "main"
 
