ec731e0d134225ee7ceb32b76c57c8d9644f0c8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-14 16:28:26 +0000

    quicktype: update 23.0.103 bottle.

--- Formula/q/quicktype.rb
+++ Formula/q/quicktype.rb
@@ -9,13 +9,13 @@ class Quicktype < Formula
   head "https://github.com/quicktype/quicktype.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "83b78f4baf16d8afee97af538ecc669cda6ae633bdab2a2be93515cc7d9f7529"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "83b78f4baf16d8afee97af538ecc669cda6ae633bdab2a2be93515cc7d9f7529"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "83b78f4baf16d8afee97af538ecc669cda6ae633bdab2a2be93515cc7d9f7529"
-    sha256 cellar: :any_skip_relocation, sonoma:         "b9c5182493b7b7668003180adafd3ae337d0465014b2c8e6cabccd5b11d0596d"
-    sha256 cellar: :any_skip_relocation, ventura:        "b9c5182493b7b7668003180adafd3ae337d0465014b2c8e6cabccd5b11d0596d"
-    sha256 cellar: :any_skip_relocation, monterey:       "b9c5182493b7b7668003180adafd3ae337d0465014b2c8e6cabccd5b11d0596d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "83b78f4baf16d8afee97af538ecc669cda6ae633bdab2a2be93515cc7d9f7529"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "32e7fec82294aacdcd3bf0023074df411203c816d9cb46bfef762d8184129173"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "32e7fec82294aacdcd3bf0023074df411203c816d9cb46bfef762d8184129173"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "32e7fec82294aacdcd3bf0023074df411203c816d9cb46bfef762d8184129173"
+    sha256 cellar: :any_skip_relocation, sonoma:         "9e66fc2ece65981bdc93d604a1f5250b4ca8a77d969838800802dc4f773aa782"
+    sha256 cellar: :any_skip_relocation, ventura:        "9e66fc2ece65981bdc93d604a1f5250b4ca8a77d969838800802dc4f773aa782"
+    sha256 cellar: :any_skip_relocation, monterey:       "9e66fc2ece65981bdc93d604a1f5250b4ca8a77d969838800802dc4f773aa782"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "32e7fec82294aacdcd3bf0023074df411203c816d9cb46bfef762d8184129173"
   end
 
   depends_on "node"
