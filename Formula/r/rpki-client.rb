f3466db01e12fbd944511d9e14f4b6e78f3882fb
Porkepix <porkepix@gmail.com>
2024-02-14 04:18:17 +0100

    rpki-client 8.9

--- Formula/r/rpki-client.rb
+++ Formula/r/rpki-client.rb
@@ -1,8 +1,8 @@
 class RpkiClient < Formula
   desc "OpenBSD portable rpki-client"
   homepage "https://www.rpki-client.org/"
-  url "https://ftp.openbsd.org/pub/OpenBSD/rpki-client/rpki-client-8.8.tar.gz"
-  sha256 "c784b929c68ea57f674f3e0371c410c35d75da7397d654dd83af66c7072ee667"
+  url "https://ftp.openbsd.org/pub/OpenBSD/rpki-client/rpki-client-8.9.tar.gz"
+  sha256 "17d2e56f5231d0ac96e60a9ea739848b17da29f57b260648da478077a3cea7ff"
   license "ISC"
 
   livecheck do
