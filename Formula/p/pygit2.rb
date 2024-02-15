2648c1659743da76cad809e8868bcbd927cf205b
Porkepix <porkepix@gmail.com>
2024-02-15 14:10:07 +0100

    pygit2 1.14.1

--- Formula/p/pygit2.rb
+++ Formula/p/pygit2.rb
@@ -1,8 +1,8 @@
 class Pygit2 < Formula
   desc "Bindings to the libgit2 shared library"
   homepage "https://github.com/libgit2/pygit2"
-  url "https://files.pythonhosted.org/packages/7b/3c/697dbc6b7b27f599ea96fbe0cd59bc4bed05652372a550d59990ab460096/pygit2-1.14.0.tar.gz"
-  sha256 "f529ed9660edbf9b625ccae7e51098ef73662e61496609009772d4627a826aa8"
+  url "https://files.pythonhosted.org/packages/f0/5e/6e05213a9163bad15489beda5f958500881d45889b0df01d7b8964f031bf/pygit2-1.14.1.tar.gz"
+  sha256 "ec5958571b82a6351785ca645e5394c31ae45eec5384b2fa9c4e05dde3597ad6"
   license "GPL-2.0-only" => { with: "GCC-exception-2.0" }
   head "https://github.com/libgit2/pygit2.git", branch: "master"
 
