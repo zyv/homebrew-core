7cb9725106082a7471ff631f333a18ca82b0b40a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-15 19:09:53 +0000

    dasel: update 2.6.0 bottle.

--- Formula/d/dasel.rb
+++ Formula/d/dasel.rb
@@ -7,13 +7,13 @@ class Dasel < Formula
   head "https://github.com/TomWright/dasel.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2bd8b5be779104bb97fcde346900b45c17e4b56334aabfd4bfd0ff4d8439f4ec"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "02d2deaef439e1800a03ec00f64475a085bad5f92c96fdb494b22565f871be89"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "65c384e12e4ff2f8a3d47e758f20e2a2045034bf24ed759544a0ed23b900f464"
-    sha256 cellar: :any_skip_relocation, sonoma:         "5947c1896374fdfa734dec2c19a41e1a05b0e129dc4f46d090ff4a00eafb86e6"
-    sha256 cellar: :any_skip_relocation, ventura:        "3661367e510d7c60a2191f88ddabe4a408168368468b8508cffd83bf5869f5d3"
-    sha256 cellar: :any_skip_relocation, monterey:       "5d2c55d3bec0f0435423c35433881c7691cb605485d14f1d553f5beecd1edde1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fd7a28d465821579e9c63b4fee48d331ae245241bd092366f02a06999d0245a9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "f302e5a73b4dc2193656ab5c7192c69583cca9f7ec33ea6e64b1d93afe047db7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6ba93804868b13856e7be0179aaeec87b511d42e4c1add21d4f6e8ce65aa65be"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "83aa34cbd7c211d89f8efb4765d0f4279d24fbe18aed3a380e8ee0ca07ee0a07"
+    sha256 cellar: :any_skip_relocation, sonoma:         "b5a71876dedc77715472fbb6d8fa9b0ae341c446b6cca2d652b77864ae62f60d"
+    sha256 cellar: :any_skip_relocation, ventura:        "7c27fa171b3eb561675c383448b991d1d3e02759fba76444c22a1d29cff74dac"
+    sha256 cellar: :any_skip_relocation, monterey:       "70e5e1e43388d407f5beaa48265ea369c776df8457d06c9432876c1e0b768b73"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f8bd7f1a1800998ddc710edf7e1a81393365c83dcee4abb92c60714719189d61"
   end
 
   depends_on "go" => :build
