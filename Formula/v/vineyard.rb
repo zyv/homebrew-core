3238eef951990a7a5014e1274043f88c1e638f12
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-02-17 01:42:52 +0800

    vineyard: revision bump (protobuf 25.3)

--- Formula/v/vineyard.rb
+++ Formula/v/vineyard.rb
@@ -6,7 +6,7 @@ class Vineyard < Formula
   url "https://github.com/v6d-io/v6d/releases/download/v0.20.3/v6d-0.20.3.tar.gz"
   sha256 "99b5165c19e0e70809cb8b8d1172f1f2dcccf28667bc8b4df46754794b7e8c16"
   license "Apache-2.0"
-  revision 4
+  revision 5
 
   bottle do
     sha256                               arm64_sonoma:   "45182498a7d8bd694bd60459f1cebb9c7d2d565fd576180b677ffb81929b0ca9"
