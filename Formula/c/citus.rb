696838d32ce193d646d006298f7f2afa3b8a088f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-17 13:29:18 +0000

    citus: update 12.1.0_1 bottle.

--- Formula/c/citus.rb
+++ Formula/c/citus.rb
@@ -8,13 +8,13 @@ class Citus < Formula
   head "https://github.com/citusdata/citus.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "3feaa17bbc05d3902404413582f22f70b526f55797c357c775654f8c4e55c9b4"
-    sha256 cellar: :any,                 arm64_ventura:  "6d9282cc121e512b2cbe73e20f5047af310c9c7c8d2350026acceedced653dd2"
-    sha256 cellar: :any,                 arm64_monterey: "6098668e13f985faff1ca249be2999b13f60434a44250c3f5704994384140698"
-    sha256 cellar: :any,                 sonoma:         "002a5bd41e70330a8a7efe7c23ec7c320d7bcb1730324fb848a19e075d0a5b98"
-    sha256 cellar: :any,                 ventura:        "dff6698a79f2681322cd2dac6d7d190f45123e9a7db3addb985bd9aa61f52f02"
-    sha256 cellar: :any,                 monterey:       "50a2bf23c39566f1c65069d816b3dd23ff4c95477e9edb973c08a794db1a15eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ab0339a42b57a9bbd0ef4c243ffcc78868f5f7d6c7c72e6b4fd62dc9d576e107"
+    sha256 cellar: :any,                 arm64_sonoma:   "364d0636fcbd90fed0963570eba777c86c74b6ad800f0c81bb8e1670f3ca7188"
+    sha256 cellar: :any,                 arm64_ventura:  "dfaa4a894cdfc5f4b24b3e7eb18e269069733ec78f617df08bc2d8fec667a87f"
+    sha256 cellar: :any,                 arm64_monterey: "df109d09dbc40c1b91b855c7bb5b42fbbaf74ef9cf445889f81efd3c2f4f4f2e"
+    sha256 cellar: :any,                 sonoma:         "8b05f8fc7a92e046ed04c03aee4a8b68b46bffb6103d5d5b1a1ebd73bdd97203"
+    sha256 cellar: :any,                 ventura:        "ff1657832310307c91eca34532535ec4f72d9c00fe811a7e034e66085ae87e60"
+    sha256 cellar: :any,                 monterey:       "fe005cb58e3b5140b858899c4789b9fc934bdd30940947445af8b96e43124644"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0258702322d82d62975b89ebb448fbbb195a55ce941ecc13ee2957de6aa5441"
   end
 
   depends_on "lz4"
