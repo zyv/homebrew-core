99a214b1a643bc6158763a40cdfb749e7dcde22e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-17 19:36:50 +0000

    mrboom: update 5.4 bottle.

--- Formula/m/mrboom.rb
+++ Formula/m/mrboom.rb
@@ -6,13 +6,13 @@ class Mrboom < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "cdaaa2a50c27d99e9d66601ebd40897dc14e019b2def57077d2484af8297112c"
-    sha256 cellar: :any,                 arm64_ventura:  "d7812215deb1254ac2b4003ee0182d4ec03ae45f81c4d9f41d627efc8dff65f2"
-    sha256 cellar: :any,                 arm64_monterey: "5098c3b755f663af968243251760fe3a39ce38c4af256959df60fb09f12c82a2"
-    sha256 cellar: :any,                 sonoma:         "5a083da53a4c1a630b5c8b77cf8fb95572aa319caebd179bc0b285175df3ef91"
-    sha256 cellar: :any,                 ventura:        "e7080fdad61d206f0ab52a03d09c2e7a53347bed66e6f4993530ccbdf96d8c87"
-    sha256 cellar: :any,                 monterey:       "7009374a1fb96c001f1cffb96e08c004455b4e114670c906c82e78f8c01853ba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cf114b335c1056c8e6a1e94cb4eab46974c84ef2c8f2f5521440c46d5b36bc82"
+    sha256 cellar: :any,                 arm64_sonoma:   "52d35d88c499909adb4af2f64fda60551817a46b460c61a8c17b8474bcb1d232"
+    sha256 cellar: :any,                 arm64_ventura:  "3996483952b0adeffbbc4278ef3b6bda8126d95a546b654eee4b401f5de5723a"
+    sha256 cellar: :any,                 arm64_monterey: "7a31326ccb10334fa22093b1e5a8a7e6d89ce91dae5f8d4b07deff72f445773f"
+    sha256 cellar: :any,                 sonoma:         "2d96f2c47865c3aadc45f8575f9ab1e5f1be8dabab94aa599f53a7597622a9d3"
+    sha256 cellar: :any,                 ventura:        "b589ad16a54ccdda45a8d84548a775438239b58d9ffbd836d5a5e2a9c4e6e270"
+    sha256 cellar: :any,                 monterey:       "6dd5ff9f550fb144780e410f009a4e32ec4345a52b3f34fbace4cd9f59c09183"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9166996bcf8695dd6c5665e2d1c956b0d5614ea52d4a1af866f974bb497a7c57"
   end
 
   depends_on "cmake" => :build
