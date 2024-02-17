041f36b54b44f02170893b71618bed219239b98c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-17 19:08:40 +0000

    dcm2niix: update 1.0.20240202 bottle.

--- Formula/d/dcm2niix.rb
+++ Formula/d/dcm2niix.rb
@@ -13,15 +13,13 @@ class Dcm2niix < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9168cedeb1403132e6e32ea44c38ee566b458766eb2a9b2d2e1795ebe62128da"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "646b444246653c09158281f1eadc660dabaed791f61167c4975c9a964aa41986"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5cbf4e3e61a81ff0ac4daed870f71653c9a40b92ec85d640db9b6ff293787152"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e250cdb698d8db5e365a0f6a752904f80140e02e8d59daae195a604a0589917"
-    sha256 cellar: :any_skip_relocation, sonoma:         "efd82e8bf88135f9fec9dad66fca05b4ea9229dad17b284ea4b1bb9f4c4d2ea5"
-    sha256 cellar: :any_skip_relocation, ventura:        "bac453b5a1f5fce0fa171f60740b1d67518f3ad16f3b957ee828f55700ca450a"
-    sha256 cellar: :any_skip_relocation, monterey:       "5a06eac1f532dc970fd53c7032179f24bf279ad9aad3430cceee3859013bc5b7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7a6e9abfd7048110de17f69b436c3262cde76888aa4041b535fbf02fc056da56"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7c262a91f2cb0ce4dfa0716e36ae9963d4ed92652f8cae06f91a0bc7eae5d6c0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "f15f3bcddae629bf3491a9aa564e2746c80102b81452e427d331d58459a41e29"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8faddf2f767f588c92f49feb4596387ad9d0fb2cb4dd59ceb3d684374205c8b7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "02cc2718e8b43c5061208ea668c85eeb9a72167c7b3a23e2016414161d7b0acf"
+    sha256 cellar: :any_skip_relocation, sonoma:         "3714a3a18984472f62c78c885323c777775aa9e4a4a17a6d1b4e5c6d416b3a81"
+    sha256 cellar: :any_skip_relocation, ventura:        "b028f4cadcab9918740eef012b378a77b750f5740dcff58c00e6747cb086d97d"
+    sha256 cellar: :any_skip_relocation, monterey:       "c0475c59a38f6636c09d25423a7b9eac147d582adb67d46af93e2df805b62f1d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7618a1d4f9ae299c160ac51c8577e06a40112c858fe7d1abd9c17fb4d556db73"
   end
 
   depends_on "cmake" => :build
