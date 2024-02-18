c802f898826fb3f0bb70afd0859a409a782a926a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-18 12:54:27 +0000

    asitop: update 0.0.24 bottle.

--- Formula/a/asitop.rb
+++ Formula/a/asitop.rb
@@ -8,9 +8,10 @@ class Asitop < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "59ccc7576dbd652b5f0e830573f0e8051a2d068c5057eaaa0fbee7150c527aaf"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1332975b0e14cfcaff9174d94af5166d33cacbfbe5f398253c45fa7cb7f5d03b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e2c02496f76a70c1116e8cda3fce262427df4cd4c1c1288d047bda2d29627c10"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "eeab44a58dd6252a970dbc0383d562cbd540ef255e65111e04cfcdd0b720729c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "50d1ee2206a174682d11b33b7e193156886b323c2ce0cb44c54ddda926696330"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "57512f3dfa29da42031706f9768e0c86441ba377d08bc3ea22cafca61f779a73"
   end
 
   depends_on arch: :arm64
