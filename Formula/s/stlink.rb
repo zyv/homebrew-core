d85f499fb89a8cec45e348c767ec8ba9d57e7c6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-18 02:41:40 +0000

    stlink: update 1.8.0 bottle.

--- Formula/s/stlink.rb
+++ Formula/s/stlink.rb
@@ -7,17 +7,13 @@ class Stlink < Formula
   head "https://github.com/stlink-org/stlink.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "0fbf4050373180e7d9510579c77d836b48033b9b06e89a34156b777b38e5dd58"
-    sha256 cellar: :any,                 arm64_ventura:  "37bc6182a2709fbcd5046be4c1cb93936475f12d2ee42983e9f7802ed89bc97c"
-    sha256 cellar: :any,                 arm64_monterey: "2ea4763e208c5566d0ec1848a6d91c440fe745287aa7a5617af635fea7707af0"
-    sha256 cellar: :any,                 arm64_big_sur:  "79683924dac821a1744cf32a96c3296eecd1668b5f2f64dbdcf570f32480459f"
-    sha256 cellar: :any,                 sonoma:         "53e58898f8926563bbf42ea5eb8cb9611a5f1afe937b9daefa54b53e3bc0a5b9"
-    sha256 cellar: :any,                 ventura:        "4aff6679ab6726e3d7a397a17de9de8972d233971907811f2113c94a4a7c7560"
-    sha256 cellar: :any,                 monterey:       "32411be4437ac85b5b9feb0fc38306af2dfc1f895ad4661c2187eb70a8420b0f"
-    sha256 cellar: :any,                 big_sur:        "9ea7be4ae1c0b91ceeb40c6df9d07ad6a5660be80043895bcf29acc47988d10d"
-    sha256 cellar: :any,                 catalina:       "e162fb37d4a7e2a0e006c5cb9beae3b86784d6a0b3d371fc33d7ed9ba2140083"
-    sha256 cellar: :any,                 mojave:         "f112f45203b8c460da03ae840529d4564a677d0621ac0a9576bac510258a9ef5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "141e0873ed4745b1224f6996f5dac5461f27a87e17d36a9f56a080f27a922f23"
+    sha256 arm64_sonoma:   "182146c51940a4851235c5a1e66e0a1455d5833a112537c366b68314f4280d62"
+    sha256 arm64_ventura:  "11f6ede1d7a55e0ceb814ea59df7e88560f317fd9ed9d1bf47c9905bb1b28b68"
+    sha256 arm64_monterey: "5aec98fdb4a07aa5abfd1292ec15bf9c385869845fc107c905f35baf2c21bb75"
+    sha256 sonoma:         "123d84cd6f2bdeeabce247febb96aed963876789e3e23bec7312098b2590483c"
+    sha256 ventura:        "96b6ee1f313c0b377a3882eb33191164b751f171dd1ba2c6c9e8ef525b663798"
+    sha256 monterey:       "5c33e3d172d272295fa0d27e08d80fd86e0429156e44b72b934898c11d08ab11"
+    sha256 x86_64_linux:   "7872c14d351e27c5953bc0565d4eb64312d3e54abc898b05197af8a631abab2c"
   end
 
   depends_on "cmake" => :build
