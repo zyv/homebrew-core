04faa6725b4b683e93cf70dc82f3c812e282eb69
Michka Popoff <michkapopoff@gmail.com>
2024-02-18 08:43:41 +0100

    rdiff-backup: vendor deps

--- Formula/r/rdiff-backup.rb
+++ Formula/r/rdiff-backup.rb
@@ -1,4 +1,6 @@
 class RdiffBackup < Formula
+  include Language::Python::Virtualenv
+
   desc "Reverse differential backup tool, over a network or locally"
   homepage "https://rdiff-backup.net/"
   url "https://files.pythonhosted.org/packages/e9/9b/487229306904a54c33f485161105bb3f0a6c87951c90a54efdc0fc04a1c9/rdiff-backup-2.2.6.tar.gz"
@@ -16,17 +18,21 @@ class RdiffBackup < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "35f310b6fa334659767acb53075486db2ecb75ad84f0f134b296c6b6af30ee46"
   end
 
-  depends_on "python-setuptools" => :build
   depends_on "librsync"
   depends_on "python@3.12"
-  depends_on "pyyaml"
 
-  def python3
-    which("python3.12")
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/cd/e5/af35f7ea75cf72f2cd079c95ee16797de7cd71f29ea7c68ae5ce7be1eda0/PyYAML-6.0.1.tar.gz"
+    sha256 "bfdf460b1736c775f2ba9f6a92bca30bc2095067b8a9d77876d1fad6cc3b4a43"
+  end
+
+  resource "setuptools" do
+    url "https://files.pythonhosted.org/packages/c9/3d/74c56f1c9efd7353807f8f5fa22adccdba99dc72f34311c30a69627a0fad/setuptools-69.1.0.tar.gz"
+    sha256 "850894c4195f09c4ed30dba56213bf7c3f21d86ed6bdaafb5df5972593bfc401"
   end
 
   def install
-    system python3, "-m", "pip", "install", *std_pip_args, "."
+    virtualenv_install_with_resources
   end
 
   test do
