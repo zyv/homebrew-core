c6516edef2f59041b2b69b98017d6500d642e310
Patrick Linnane <patrick@linnane.io>
2024-02-19 23:19:26 +0000

    glasskube: add 0.0.2 bottle.

--- Formula/g/glasskube.rb
+++ Formula/g/glasskube.rb
@@ -6,6 +6,16 @@ class Glasskube < Formula
   license "Apache-2.0"
   head "https://github.com/glasskube/glasskube.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5b4de34d2d7d888b19dc92aec90d2939292ebd3e73543ae1cd6000fa74a5b4c1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "44072ea39b6b95934e599b310c18d6bfec59495b972e98857674c285a2175af5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2ee46f983c01584305de152dd114df1ea860e75f1abfb4c9b6e209121ce8d8e0"
+    sha256 cellar: :any_skip_relocation, sonoma:         "86bec5397943381aaf0a863797a33fdf17905e75b86b3fbd68acc92af55b55f3"
+    sha256 cellar: :any_skip_relocation, ventura:        "729c7983d8661d341546e5e80802557d36efac4656f2705b940c314b51143c64"
+    sha256 cellar: :any_skip_relocation, monterey:       "619488ec9778ee5d33656463b5f81e6e355579e0a0214888059a1aa5d3313b41"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b4447e345d432b0407173e5005f51506d177e7efdd24becb3ed9de142475c512"
+  end
+
   depends_on "go" => :build
 
   def install
