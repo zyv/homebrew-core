6a112fcba314ba0ae8aebcabad10b0e48d0d8ae6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-19 00:33:02 +0000

    cfonts 1.1.4

--- Formula/c/cfonts.rb
+++ Formula/c/cfonts.rb
@@ -1,8 +1,8 @@
 class Cfonts < Formula
   desc "Sexy ANSI fonts for the console"
   homepage "https://github.com/dominikwilkowski/cfonts"
-  url "https://github.com/dominikwilkowski/cfonts/archive/refs/tags/v1.1.3rust.tar.gz"
-  sha256 "e5b2be2d1ce4f4e5b345d755425bc9841a9c1e35c4025ec83dab4f6c38291d87"
+  url "https://github.com/dominikwilkowski/cfonts/archive/refs/tags/v1.1.4rust.tar.gz"
+  sha256 "49228dc3dd4529bd86d537f46c8aa9a47043f060ec7d050e5cb739a030222407"
   license "GPL-3.0-or-later"
   head "https://github.com/dominikwilkowski/cfonts.git", branch: "released"
 
