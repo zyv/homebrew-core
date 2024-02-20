7a96b6f12504ccb1692dd8550d62b61ac117032e
Michka Popoff <michkapopoff@gmail.com>
2024-02-20 21:45:18 +0100

    litani: vendor deps

--- Formula/l/litani.rb
+++ Formula/l/litani.rb
@@ -24,14 +24,23 @@ class Litani < Formula
   depends_on "scdoc" => :build
   depends_on "gnuplot"
   depends_on "graphviz"
+  depends_on "libyaml"
   depends_on "ninja"
-  depends_on "python-markupsafe"
   depends_on "python@3.12"
-  depends_on "pyyaml"
 
   resource "jinja2" do
     url "https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce/Jinja2-3.1.2.tar.gz"
     sha256 "31351a702a408a9e7595a8fc6150fc3f43bb6bf7e319770cbc0db9df9437e852"
+  end
+
+  resource "markupsafe" do
+    url "https://files.pythonhosted.org/packages/6d/7c/59a3248f411813f8ccba92a55feaac4bf360d29e2ff05ee7d8e1ef2d7dbf/MarkupSafe-2.1.3.tar.gz"
+    sha256 "af598ed32d6ae86f1b747b82783958b1a4ab8f617b06fe68795c7f026abbdcad"
+  end
+
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/cd/e5/af35f7ea75cf72f2cd079c95ee16797de7cd71f29ea7c68ae5ce7be1eda0/PyYAML-6.0.1.tar.gz"
+    sha256 "bfdf460b1736c775f2ba9f6a92bca30bc2095067b8a9d77876d1fad6cc3b4a43"
   end
 
   def install
