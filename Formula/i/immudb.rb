f4ff21f16f9dba8fa3614cb5ed6d6313d5178bf1
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-02-21 22:02:32 +0800

    immudb: fix shell completions generation
    By default, `generate_completions_from_executable` uses the formula name
    as the base name. Override that with the `base_name` argument.

--- Formula/i/immudb.rb
+++ Formula/i/immudb.rb
@@ -29,7 +29,7 @@ class Immudb < Formula
 
     %w[immudb immuclient immuadmin].each do |binary|
       bin.install binary
-      generate_completions_from_executable(bin/binary, "completion")
+      generate_completions_from_executable(bin/binary, "completion", base_name: binary)
     end
   end
 
