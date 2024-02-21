4065d0646267b39fafc5ad6d63d53be488d1ce96
Timothy Sutton <tim@synthist.net>
2024-02-20 19:37:06 -0500

    gcovr: update Python deps

--- Formula/g/gcovr.rb
+++ Formula/g/gcovr.rb
@@ -1,4 +1,6 @@
 class Gcovr < Formula
+  include Language::Python::Virtualenv
+
   desc "Reports from gcov test coverage program"
   homepage "https://gcovr.com/"
   url "https://files.pythonhosted.org/packages/a5/d9/fc88c9df718e44dad943fec14db80e49cc0b1a592bbbf691f56a3955ccaf/gcovr-7.0.tar.gz"
@@ -16,24 +18,33 @@ class Gcovr < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d4fe2eee6e597b885839701d147fcea5695ea45868046c5579922715bfce245"
   end
 
-  depends_on "python-setuptools" => :build
-  depends_on "pygments"
-  depends_on "python-jinja"
-  depends_on "python-lxml"
-  depends_on "python-markupsafe"
   depends_on "python@3.12"
 
-  def python3
-    "python3.12"
-  end
+  uses_from_macos "libxml2", since: :ventura
+  uses_from_macos "libxslt"
 
   resource "jinja2" do
     url "https://files.pythonhosted.org/packages/b2/5e/3a21abf3cd467d7876045335e681d276ac32492febe6d98ad89562d1a7e1/Jinja2-3.1.3.tar.gz"
     sha256 "ac8bd6544d4bb2c9792bf3a159e80bba8fda7f07e81bc3aed565432d5925ba90"
   end
 
+  resource "lxml" do
+    url "https://files.pythonhosted.org/packages/2b/b4/bbccb250adbee490553b6a52712c46c20ea1ba533a643f1424b27ffc6845/lxml-5.1.0.tar.gz"
+    sha256 "3eea6ed6e6c918e468e693c41ef07f3c3acc310b70ddd9cc72d9ef84bc9564ca"
+  end
+
+  resource "markupsafe" do
+    url "https://files.pythonhosted.org/packages/87/5b/aae44c6655f3801e81aa3eef09dbbf012431987ba564d7231722f68df02d/MarkupSafe-2.1.5.tar.gz"
+    sha256 "d283d37a890ba4c1ae73ffadf8046435c76e7bc2247bbb63c00bd1a709c6544b"
+  end
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/55/59/8bccf4157baf25e4aa5a0bb7fa3ba8600907de105ebc22b0c78cfbf6f565/pygments-2.17.2.tar.gz"
+    sha256 "da46cec9fd2de5be3a8a784f434e4c4ab670b4ff54d605c4c2717e9d49c4c367"
+  end
+
   def install
-    system python3, "-m", "pip", "install", *std_pip_args, "."
+    virtualenv_install_with_resources
   end
 
   test do
