a3293993dae73de73ac4fd0d1d0442e3c2f9cc28
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-22 12:36:22 +0000

    mariadb@11.2: add 11.2.3 bottle.

--- Formula/m/mariadb@11.2.rb
+++ Formula/m/mariadb@11.2.rb
@@ -17,6 +17,16 @@ class MariadbAT112 < Formula
     end
   end
 
+  bottle do
+    sha256 arm64_sonoma:   "342adcf290cc75007d4f1ad2664d4da293452bad231dcb518bd561b56600d6ec"
+    sha256 arm64_ventura:  "7a1e63956eacd755d84008503b091d8bca7c462189ab3f5ff30b7ddbf79e75b6"
+    sha256 arm64_monterey: "64d27c536ba0c6b93867ed753d316f59805d620fa3e6b659b6a3fa8340958566"
+    sha256 sonoma:         "c6cdae6aa1c2fa4dfbae970cd7f2f65e896c771efc42fc653e9c2d35ec5bd66e"
+    sha256 ventura:        "e58eb0a6b1bdcbbe28d496f1a53f34b32113b90d7981dbd37edc01b78fd3139f"
+    sha256 monterey:       "62674052f2d4264f681465b7c0b55be5955a0da6313c1311dbf27ff0c28f7d8f"
+    sha256 x86_64_linux:   "86afcc2d7a25d5803352f3b45390a585de896403c0527dabbafbcb081b2da00d"
+  end
+
   keg_only :versioned_formula
 
   # See: https://mariadb.com/kb/en/changes-improvements-in-mariadb-11-2/
