0d3dac57e3e654accb4be832aeb5926ce7ddaef8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-22 20:35:09 +0000

    tarsnapper: update 0.5.0_1 bottle.

--- Formula/t/tarsnapper.rb
+++ Formula/t/tarsnapper.rb
@@ -9,14 +9,14 @@ class Tarsnapper < Formula
   revision 1
 
   bottle do
-    rebuild 4
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0b48aef71d275be4e640ed1f99dea73050cee3600f0745f5bbc9c99b36454957"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d2f9c0a3797364ca219a20d6200ad6f0005c81ba6010e572f300ef39ec31b68a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9abbdd4147588aec9a0b33a804e8d672494330c14d6ec0e6e6a78fded43c25e0"
-    sha256 cellar: :any_skip_relocation, sonoma:         "d324865fef97a462dd0ca212179902e6f713da22769fb76477c0b8bd3d68314f"
-    sha256 cellar: :any_skip_relocation, ventura:        "fb6d4a1fcf598f49d227d4921f844a80e0c22edf374a758b5a1981be24a2b838"
-    sha256 cellar: :any_skip_relocation, monterey:       "12ef312292edb578e80d80f1ac2e7eaaad5ec1ddb446eda4a5e9dff8569be8bc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8e788ef51a4b7de45dcb87de4d509fe22852339b4257fa2946fb48113b01defc"
+    rebuild 5
+    sha256 cellar: :any,                 arm64_sonoma:   "9742393b148810a665b9af397ecb3084d84e83d5653ed1e76e955f8b9f897652"
+    sha256 cellar: :any,                 arm64_ventura:  "55097042c81e1a943970310f57c471ad3b8dd174665d9587d459dcd761a64b5b"
+    sha256 cellar: :any,                 arm64_monterey: "c37b0bca9437389af8569f18724d3510612a528c9a443e21b4ccfeafe319487c"
+    sha256 cellar: :any,                 sonoma:         "d1f61623dcc854af71e22d44f1efeec81d6c9a42c050eed02d5fa52f09d30982"
+    sha256 cellar: :any,                 ventura:        "bd26e2b4728637414f25ccce75e0a000ce7d4788070157370280ca8fef89487c"
+    sha256 cellar: :any,                 monterey:       "b6bf153731a8f5a0242c3b69ab84f32579e8a905522a1e3abcaf38d121ebfeae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ca4f4379c2c1309b50cdfaebb62082088a9af18a5a3353267d975413fe688b0f"
   end
 
   depends_on "libyaml"
