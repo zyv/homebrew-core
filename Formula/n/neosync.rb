f3494874b892bc9644cacd3842be8237e80ca769
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-02-22 17:16:06 -0500

    neosync 0.3.40

--- Formula/n/neosync.rb
+++ Formula/n/neosync.rb
@@ -1,8 +1,8 @@
 class Neosync < Formula
   desc "CLI for interfacing with Neosync"
   homepage "https://www.neosync.dev/"
-  url "https://github.com/nucleuscloud/neosync/archive/refs/tags/v0.3.36.tar.gz"
-  sha256 "c3afe66bc90de2ee1f4c6d88df78d50a9f9e6af41b85ec1675fdf1ead8c6b4fa"
+  url "https://github.com/nucleuscloud/neosync/archive/refs/tags/v0.3.40.tar.gz"
+  sha256 "5d7638b43ad8957b31183328bac86b6dee80a93f61a7cbe30fecd2d3d442aa2c"
   license "MIT"
   head "https://github.com/nucleuscloud/neosync.git", branch: "main"
 
