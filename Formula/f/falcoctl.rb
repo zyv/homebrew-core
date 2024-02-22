79213809405e7d85f57c5a71c9a0ee15cbdae661
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-22 15:41:39 +0000

    falcoctl 0.7.3

--- Formula/f/falcoctl.rb
+++ Formula/f/falcoctl.rb
@@ -1,8 +1,8 @@
 class Falcoctl < Formula
   desc "CLI tool for working with Falco and its ecosystem components"
   homepage "https://github.com/falcosecurity/falcoctl"
-  url "https://github.com/falcosecurity/falcoctl/archive/refs/tags/v0.7.2.tar.gz"
-  sha256 "d7114c77af92ec413859e152753ccd33fbc297de6964c5c8a8a214476f727d39"
+  url "https://github.com/falcosecurity/falcoctl/archive/refs/tags/v0.7.3.tar.gz"
+  sha256 "8b808c73e7c8296a88ce25923b4a289870546139b09ac06d6400c42916e1217d"
   license "Apache-2.0"
   head "https://github.com/falcosecurity/falcoctl.git", branch: "main"
 
