43a1d10021d0e43129b8cfcc4169e53fb7575b6e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-23 22:01:09 +0000

    eslint: update 8.57.0 bottle.

--- Formula/e/eslint.rb
+++ Formula/e/eslint.rb
@@ -8,13 +8,13 @@ class Eslint < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a7b6e00e0f1f0423799130fb2a809a4d743c6dc39ced2cc2765a539bfa37ec6c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a7b6e00e0f1f0423799130fb2a809a4d743c6dc39ced2cc2765a539bfa37ec6c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a7b6e00e0f1f0423799130fb2a809a4d743c6dc39ced2cc2765a539bfa37ec6c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "74349aac80a0c98eeb594881e5e80807514e7fe7cec01074f28767531b230e11"
-    sha256 cellar: :any_skip_relocation, ventura:        "74349aac80a0c98eeb594881e5e80807514e7fe7cec01074f28767531b230e11"
-    sha256 cellar: :any_skip_relocation, monterey:       "74349aac80a0c98eeb594881e5e80807514e7fe7cec01074f28767531b230e11"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a7b6e00e0f1f0423799130fb2a809a4d743c6dc39ced2cc2765a539bfa37ec6c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e50b1b608690ddd93ea13fed459d32f199a023e4d6c4c2011e4e5737cd00176d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e50b1b608690ddd93ea13fed459d32f199a023e4d6c4c2011e4e5737cd00176d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e50b1b608690ddd93ea13fed459d32f199a023e4d6c4c2011e4e5737cd00176d"
+    sha256 cellar: :any_skip_relocation, sonoma:         "f78b23845fc25b6b6bed7896cd027b114b065d2532cd9387b9bed9242783a95a"
+    sha256 cellar: :any_skip_relocation, ventura:        "f78b23845fc25b6b6bed7896cd027b114b065d2532cd9387b9bed9242783a95a"
+    sha256 cellar: :any_skip_relocation, monterey:       "f78b23845fc25b6b6bed7896cd027b114b065d2532cd9387b9bed9242783a95a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e50b1b608690ddd93ea13fed459d32f199a023e4d6c4c2011e4e5737cd00176d"
   end
 
   depends_on "node"
