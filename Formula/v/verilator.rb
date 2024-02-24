6cca479cea1edb489dd51225f1a898cb2bcf0602
Porkepix <porkepix@gmail.com>
2024-02-24 20:57:29 +0100

    verilator 5.022
    Signed-off-by: Michael Cho <michael@michaelcho.dev>

--- Formula/v/verilator.rb
+++ Formula/v/verilator.rb
@@ -1,8 +1,8 @@
 class Verilator < Formula
   desc "Verilog simulator"
   homepage "https://www.veripool.org/wiki/verilator"
-  url "https://github.com/verilator/verilator/archive/refs/tags/v5.018.tar.gz"
-  sha256 "8b544273eedee379e3c1a3bb849e14c754c9b5035d61ad03acdf3963092ba6c0"
+  url "https://github.com/verilator/verilator/archive/refs/tags/v5.022.tar.gz"
+  sha256 "3c2f5338f4b6ce7e2f47a142401acdd18cbf4c5da06092618d6d036c0afef12d"
   license any_of: ["LGPL-3.0-only", "Artistic-2.0"]
   head "https://github.com/verilator/verilator.git", branch: "master"
 
