c4edb8efaea9188c9463eb2ca48cf43a7857b248
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-24 09:57:18 +0000

    abseil 20240116.1

--- Formula/a/abseil.rb
+++ Formula/a/abseil.rb
@@ -1,8 +1,8 @@
 class Abseil < Formula
   desc "C++ Common Libraries"
   homepage "https://abseil.io"
-  url "https://github.com/abseil/abseil-cpp/archive/refs/tags/20230802.1.tar.gz"
-  sha256 "987ce98f02eefbaf930d6e38ab16aa05737234d7afbab2d5c4ea7adbe50c28ed"
+  url "https://github.com/abseil/abseil-cpp/archive/refs/tags/20240116.1.tar.gz"
+  sha256 "3c743204df78366ad2eaf236d6631d83f6bc928d1705dd0000b872e53b73dc6a"
   license "Apache-2.0"
   head "https://github.com/abseil/abseil-cpp.git", branch: "master"
 
