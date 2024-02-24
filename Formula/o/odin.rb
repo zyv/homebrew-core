4a1b9f05d3eaf404ec8d4b840528cca13c66ea94
Laytan Laats <laytanlaats@hotmail.com>
2024-02-24 23:15:16 +0100

    odin: add vendor collection and build vendor deps from source
    Follow up on the discussion in #158496 implementing the discussed
    resolution from that thread.
    
    All pre-compiled dependencies are first deleted and then built from
    source.
    
    Co-authored-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/o/odin.rb
+++ Formula/o/odin.rb
@@ -18,18 +18,72 @@ class Odin < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "43f62fc9cca1fad774659c6da3ce6a9ea23304318b88970e2509386b30e0d436"
   end
 
+  depends_on "glfw"
   depends_on "llvm"
+  depends_on "raylib"
 
   fails_with gcc: "5" # LLVM is built with GCC
 
+  resource "raygui" do
+    url "https://github.com/raysan5/raygui/archive/refs/tags/4.0.tar.gz"
+    sha256 "299c8fcabda68309a60dc858741b76c32d7d0fc533cdc2539a55988cee236812"
+  end
+
   def install
     llvm = deps.map(&:to_formula).find { |f| f.name.match?(/^llvm(@\d+(\.\d+)*)?$/) }
+
+    # Delete pre-compiled binaries which brew does not allow.
+    system "find", "vendor",
+                   "(",
+                     "-name", "*.lib",   "-o",
+                     "-name", "*.dll",   "-o",
+                     "-name", "*.a",     "-o",
+                     "-name", "*.dylib", "-o",
+                     "-name", "*.so.*",  "-o",
+                     "-name", "*.so",
+                   ")",
+                   "-delete"
+
+    cd buildpath/"vendor/miniaudio/src" do
+      system "make"
+    end
+
+    cd buildpath/"vendor/stb/src" do
+      system "make", "unix"
+    end
+
+    if OS.mac?
+      raylib_installpath = Hardware::CPU.arm? ? "vendor/raylib/macos-arm64" : "vendor/raylib/macos"
+
+      ln_s Formula["glfw"].lib/"libglfw3.a", buildpath/"vendor/glfw/lib/darwin/libglfw3.a"
+
+      ln_s Formula["raylib"].lib/"libraylib.a", buildpath/raylib_installpath/"libraylib.a"
+      # This is actually raylib 5.0, but upstream had not incremented this number yet when it released.
+      ln_s Formula["raylib"].lib/"libraylib.4.5.0.dylib", buildpath/raylib_installpath/"libraylib.500.dylib"
+
+      resource("raygui").stage do
+        cp "src/raygui.h", "src/raygui.c"
+
+        # build static library
+        system ENV.cc, "-c", "-o", "raygui.o", "src/raygui.c",
+          "-fpic", "-DRAYGUI_IMPLEMENTATION", "-I#{Formula["raylib"].include}"
+        system "ar", "-rcs", "libraygui.a", "raygui.o"
+        cp "libraygui.a", buildpath/raylib_installpath
+
+        # build shared library
+        system ENV.cc, "-o", "libraygui.dylib", "src/raygui.c",
+          "-shared", "-fpic", "-DRAYGUI_IMPLEMENTATION", "-framework", "OpenGL",
+          "-lm", "-lpthread", "-ldl",
+          "-I#{Formula["raylib"].include}", "-L#{Formula["raylib"].lib}", "-lraylib"
+        cp "libraygui.dylib", buildpath/raylib_installpath
+      end
+    end
 
     # Keep version number consistent and reproducible for tagged releases.
     args = []
     args << "ODIN_VERSION=dev-#{version}" unless build.head?
     system "make", "release", *args
-    libexec.install "odin", "core", "shared", "base"
+    libexec.install "odin", "core", "shared", "base", "vendor"
     (bin/"odin").write <<~EOS
       #!/bin/bash
       export PATH="#{llvm.opt_bin}:$PATH"
@@ -39,8 +93,6 @@ class Odin < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/odin version")
-
     (testpath/"hellope.odin").write <<~EOS
       package main
 
@@ -52,5 +104,51 @@ class Odin < Formula
     EOS
     system "#{bin}/odin", "build", "hellope.odin", "-file"
     assert_equal "Hellope!\n", shell_output("./hellope")
+
+    (testpath/"miniaudio.odin").write <<~EOS
+      package main
+
+      import "core:fmt"
+      import "vendor:miniaudio"
+
+      main :: proc() {
+        ver := miniaudio.version_string()
+        assert(len(ver) > 0)
+        fmt.println(ver)
+      }
+    EOS
+    system "#{bin}/odin", "run", "miniaudio.odin", "-file"
+
+    if OS.mac?
+      (testpath/"raylib.odin").write <<~EOS
+        package main
+
+        import rl "vendor:raylib"
+
+        main :: proc() {
+          // raygui.
+          assert(!rl.GuiIsLocked())
+
+          // raylib.
+          num := rl.GetRandomValue(42, 1337)
+          assert(42 <= num && num <= 1337)
+        }
+      EOS
+      system "#{bin}/odin", "run", "raylib.odin", "-file"
+      system "#{bin}/odin", "run", "raylib.odin", "-file",
+        "-define:RAYLIB_SHARED=true", "-define:RAYGUI_SHARED=true"
+
+      (testpath/"glfw.odin").write <<~EOS
+        package main
+
+        import "core:fmt"
+        import "vendor:glfw"
+
+        main :: proc() {
+          fmt.println(glfw.GetVersion())
+        }
+      EOS
+      system "#{bin}/odin", "run", "glfw.odin", "-file"
+    end
   end
 end
