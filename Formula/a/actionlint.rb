8324fc5610f213bbbc22f7c090aac2d6890efb9f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-24 15:20:39 +0000

    actionlint 1.6.27

--- Formula/a/actionlint.rb
+++ Formula/a/actionlint.rb
@@ -1,10 +1,9 @@
 class Actionlint < Formula
   desc "Static checker for GitHub Actions workflow files"
   homepage "https://rhysd.github.io/actionlint/"
-  url "https://github.com/rhysd/actionlint/archive/refs/tags/v1.6.26.tar.gz"
-  sha256 "507d771f4c863bf98dfe1db3500a4c9344e3a35592a6e2ac4183f00a63291feb"
+  url "https://github.com/rhysd/actionlint/archive/refs/tags/v1.6.27.tar.gz"
+  sha256 "211618132974a864e3451ecd5c81a6dc7a361456b5e7d97a23f212ad8f6abb2d"
   license "MIT"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "982f26e2a73cca9090ece47525b3448c1e11815b36c6a990bdccd09098e83994"
@@ -18,13 +17,6 @@ class Actionlint < Formula
 
   depends_on "go" => :build
   depends_on "ronn" => :build
-
-  # Temporarily apply patch to not flag new macOS runner names as invalid.
-  # Remove for >=1.6.26
-  patch do
-    url "https://github.com/rhysd/actionlint/commit/3123d5e319d8e7514be096d1762710e4b5d7e5e2.patch?full_index=1"
-    sha256 "8770ff3f7b93311a1849a512fdaa1649a0b23ff6dde3e031faaaa9b40f67c423"
-  end
 
   def install
     ldflags = "-s -w -X github.com/rhysd/actionlint.version=#{version}"
