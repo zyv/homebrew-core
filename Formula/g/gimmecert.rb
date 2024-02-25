e39f5482eba021b43a879ff30a84b01e83c95cf3
Patrick Linnane <patrick@linnane.io>
2024-02-25 09:51:03 +0000

    gimmecert: add 1.0.0 bottle.

--- Formula/g/gimmecert.rb
+++ Formula/g/gimmecert.rb
@@ -7,6 +7,16 @@ class Gimmecert < Formula
   sha256 "eb00848fab5295903b4d5ef997c411fe063abc5b0f520a78ca2cd23f77e3fd99"
   license "GPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5c5201da0004e7f9585bd5cb41aab648ab5f1039f1dd207685aed98812cdab14"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1752190b88dd16f222c29ffeefb8e4273ce41272df77f653140ece125909e897"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "35c4e73239bff368cf4f6aaa72a0b4b90c76e48039b74a3d89c1199ff1cfdda5"
+    sha256 cellar: :any_skip_relocation, sonoma:         "e3eea83a6e11530a8c4f98d9892ba49d7768b8c965d0adde074cf24b1e0d5afb"
+    sha256 cellar: :any_skip_relocation, ventura:        "f3d86da82e02647ab2a58b743eca70f1173f571966bb2524a35fa41d3dc456b3"
+    sha256 cellar: :any_skip_relocation, monterey:       "52adedab6ad1d8eb70ef10fa042a94a25a77a45b5e5169eb02df755c0f4ffc97"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2fb4c2af0f720f970c8aab3281801a3125547e484de41552d49330706dee53dd"
+  end
+
   depends_on "python-certifi"
   depends_on "python-cryptography"
   depends_on "python@3.12"
