671bb9cb9f2ee01189226f11a83fce531d8f4622
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-02-25 23:59:25 +0800

    netdata: revision bump (abseil 20240116.1)

--- Formula/n/netdata.rb
+++ Formula/n/netdata.rb
@@ -4,7 +4,7 @@ class Netdata < Formula
   url "https://github.com/netdata/netdata/releases/download/v1.44.3/netdata-v1.44.3.tar.gz"
   sha256 "50df30a9aaf60d550eb8e607230d982827e04194f7df3eba0e83ff7919270ad2"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
 
   livecheck do
     url :stable
