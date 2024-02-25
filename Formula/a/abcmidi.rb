e19ddd4d88e504962abc810f16a32faf030ca416
Rui Chen <rui@chenrui.dev>
2024-02-25 09:28:50 -0500

    abcmidi 2024.02.25

--- Formula/a/abcmidi.rb
+++ Formula/a/abcmidi.rb
@@ -1,8 +1,8 @@
 class Abcmidi < Formula
   desc "Converts abc music notation files to MIDI files"
   homepage "https://ifdo.ca/~seymour/runabc/top.html"
-  url "https://ifdo.ca/~seymour/runabc/abcMIDI-2024.02.23.zip"
-  sha256 "9e09a0653c8b66afb2d4ca4638234bc620cff71637856d7e5962ae1d450680ed"
+  url "https://ifdo.ca/~seymour/runabc/abcMIDI-2024.02.25.zip"
+  sha256 "d2a79243d27754808da89ab36abfc9d173b9dc1fb6c4825647fed3cadc09f92c"
   license "GPL-2.0-or-later"
 
   livecheck do
