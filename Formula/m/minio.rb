6889a14ed38e3a1cb3eefed869cf207fe0e428aa
Rui Chen <rui@chenrui.dev>
2024-02-25 09:46:06 -0500

    minio 20240224171114

--- Formula/m/minio.rb
+++ Formula/m/minio.rb
@@ -2,9 +2,9 @@ class Minio < Formula
   desc "High Performance, Kubernetes Native Object Storage"
   homepage "https://min.io"
   url "https://github.com/minio/minio.git",
-      tag:      "RELEASE.2024-02-17T01-15-57Z",
-      revision: "b6e98aed0161bb526aaf11fa1672d30eb89dca4b"
-  version "20240217011557"
+      tag:      "RELEASE.2024-02-24T17-11-14Z",
+      revision: "c2b54d92f66ea02642be4ede26f2b7ecac1f2056"
+  version "20240224171114"
   license "AGPL-3.0-or-later"
   head "https://github.com/minio/minio.git", branch: "master"
 
