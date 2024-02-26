848da8194ce42b15fdc0a3573e0e54585b5586f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-26 22:12:01 +0000

    tfautomv 0.6.2

--- Formula/t/tfautomv.rb
+++ Formula/t/tfautomv.rb
@@ -1,8 +1,8 @@
 class Tfautomv < Formula
   desc "Generate Terraform moved blocks automatically for painless refactoring"
   homepage "https://tfautomv.dev/"
-  url "https://github.com/busser/tfautomv/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "96f1b4ef04dca075e38ff8d5b09a9b9bd2dbf73625e25c4bbdf1757981aab778"
+  url "https://github.com/busser/tfautomv/archive/refs/tags/v0.6.2.tar.gz"
+  sha256 "d89e19c03c7cd1ea1714d091cea751289936b2d52e790fae3a26b23bc445313d"
   license "Apache-2.0"
   head "https://github.com/busser/tfautomv.git", branch: "main"
 
