e504196138a8854c1ae7095ef775a2415d317356
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-28 20:15:13 +0000

    px: update 3.5.4 bottle.

--- Formula/p/px.rb
+++ Formula/p/px.rb
@@ -9,13 +9,13 @@ class Px < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bfbce983a56a6cced3dbe043966be5d7a03db965e9010792e35323a4109c2cea"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9e191c21a2a3ae3cb26ca838fb38e098fa356f7bc7d6f4b6ccccf2a6f6ab6c36"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "672be80ea811076afd74fc132178f6d2875ea080021e0d1a80847b08fe052935"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a84e585d140a31deb4a74348db6ef72954c024b126715e075127a5547cfd0f77"
-    sha256 cellar: :any_skip_relocation, ventura:        "8cf0c8a2f23acb0849333491b2a5b0507427933f317213b38bb196b9ce79c283"
-    sha256 cellar: :any_skip_relocation, monterey:       "46eb85ce299da203e5c689234b91dbc20d53483e1c2a35042360ec9f33402062"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4179aff98025129416df49507dc78b09b133248e85265f7f7d074487d9192208"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9eb3fcc58593d79f98241bf8cb3d97d11a508160c9b20cc20e41d06eb144c131"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ae420a972cbe7d2625b24af048b6bf26ed2d34f92fa2435832ef91dcd1a538fd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f25bd7bd46fc9d89e24818596aafca798d6f107e0e52b354af7e701bae03aa1e"
+    sha256 cellar: :any_skip_relocation, sonoma:         "b87717d2ec82da62e3d6ad24e7d335cfd1e2ecedc524492c86676c6ecafcb638"
+    sha256 cellar: :any_skip_relocation, ventura:        "25390d4874f01c867651e7e4c9bd5ba11a0629c6ecb203e298acf1edb6aeeeef"
+    sha256 cellar: :any_skip_relocation, monterey:       "6147857916476c5d365d670065567b420bc69fadef289526b8c514df814bdb4d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f5d62a1d5185fff836a76ebb15803fde33a78fa09d38c1355bc3f054c2c4579"
   end
 
   depends_on "python@3.12"
