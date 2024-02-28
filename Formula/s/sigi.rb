54752e3b035564a08ec75627bb44be8d3b57ee89
Rui Chen <rui@chenrui.dev>
2024-02-28 20:16:07 +0000

    sigi: add 3.6.3 bottle.

--- Formula/s/sigi.rb
+++ Formula/s/sigi.rb
@@ -6,6 +6,16 @@ class Sigi < Formula
   license "GPL-2.0-only"
   head "https://github.com/sigi-cli/sigi.git", branch: "core"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6a20b2b6fa2f3c796e8eaef323e8f333639e1c14d89b0b2f34938b063412226a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c73a98b113bc2270b1e2dfed1e802e39b74644a0ff31f74f6934c91a9286d8a7"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8d37e2e8b7a7de64c2f714b76d0283e64fb3c96a3e87c9036881a59f3dda30ee"
+    sha256 cellar: :any_skip_relocation, sonoma:         "a96d1eb27fb5547a3b03999c8a2739e6df473faeb35f641a810fec2aa5d1b9d0"
+    sha256 cellar: :any_skip_relocation, ventura:        "b00f66958c7aeb62f02188a35d3b7df12891fe35d35d89f47abf5e220eaadfa9"
+    sha256 cellar: :any_skip_relocation, monterey:       "44d9a313f1faccce3d9d2488ef96c9d3930331788b66a6ca81508d1811543550"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d0ca10c639f442090a203f06f3f02db3f23d8cb2cf36e81cbc68bafaa3b9e0c"
+  end
+
   depends_on "rust" => :build
 
   def install
