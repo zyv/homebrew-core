0796e90ac90ac487fe1d59ec659fd3e88ec1fd7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-29 18:32:47 +0000

    kubeshark: add 52.1.63 bottle.

--- Formula/k/kubeshark.rb
+++ Formula/k/kubeshark.rb
@@ -5,6 +5,16 @@ class Kubeshark < Formula
   sha256 "2b2c66095430f7038fa665472a34e85165ad041396289c98001d10835fd98c63"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "772e0a413748a0f28b513dd26c453c22a714a9199f1f9ebd59d366a3819e16f3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "05a9f04723ceb8c5d4adbcb6559beab3effbc6780b818ed6c3f2344fc4ee1ffd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f4c545e6cdf1997afe932c79b0a74f4dd350b22f8ea5a0140858479088e9cced"
+    sha256 cellar: :any_skip_relocation, sonoma:         "e6c396e74709c6d35cb57f547bacba3fff5a4d0040d7cb51d28f2ae1a7e1efb2"
+    sha256 cellar: :any_skip_relocation, ventura:        "b35fab7ccb9efe12a47af22b55b52a1b602274920d38c00b3766d8c461ee3474"
+    sha256 cellar: :any_skip_relocation, monterey:       "d14c6b96a2691f4e0906cc0ab98be67d25d1b191825f213c6c2f01838c314584"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "94c8582acd60b75bec61ff8a416f455549f73be229045019be50145d16739cd8"
+  end
+
   depends_on "go" => :build
 
   def install
