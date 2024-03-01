475036c6244d1785fc576ef1cdc112f5d469c95b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-01 08:28:37 +0000

    python-dateutil: update 2.9.0 bottle.

--- Formula/p/python-dateutil.rb
+++ Formula/p/python-dateutil.rb
@@ -6,14 +6,13 @@ class PythonDateutil < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bb7e7a8095b6ef0faa231cb385e2edf152f93bd8b6b201f8f0283975aa8a0c6d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c9e043c7c21453f6613a65aae1b44a61ced3a69cf4152e6a83374d0b483acc96"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a45eb0c21908f79df8445fcef22c6b90d59c22d69d5e89679138a778623e1862"
-    sha256 cellar: :any_skip_relocation, sonoma:         "ca7324974470db0fb253c16f2a66c8131071a064425b3ea59e88b55111f10c76"
-    sha256 cellar: :any_skip_relocation, ventura:        "634ed37fc5639de61617a6a5b373801057bf2ab3a6b0e373997dd77cce60c5bd"
-    sha256 cellar: :any_skip_relocation, monterey:       "7b8d9a4e6d6e0c13b7d3d2c599fc10eb55d9edbb27d45dbd3a6ffb226b2cc114"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f5cddb58045c1b708645154c609bf0e1f2bdc1634a12d933e072417a99681456"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d2003c87754e04db1a9fa72148fd3ae10e8c01167472b8c1721d5b029b289c39"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "64a5fee86a47d791af5f0a2c3e38f79f7362476c4f678864048a0ebcaee6c00c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b04f10a11573c1014097b7b174aa87069de9b87be768d1523efc01a9496df7ac"
+    sha256 cellar: :any_skip_relocation, sonoma:         "b306c1b0c709f9a06eb9915ccd98e8d9be3cc766e101c4142ac74725b54a201c"
+    sha256 cellar: :any_skip_relocation, ventura:        "0a6281f3ad3fd1ec74314ea121261578c963145e422c05ca17dfd80aeb578e04"
+    sha256 cellar: :any_skip_relocation, monterey:       "f60075b312beaf9d4e5c23a7f8a6689f8ec1df7f03d97c6e3aff867905ac8103"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b5b4a5c21bd430e847dfd3def8edac1581b75e82c103019a54f1d9678a1d9f95"
   end
 
   depends_on "python-setuptools" => :build
