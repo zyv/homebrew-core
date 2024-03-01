f7a32de88c6773f1fceef3cf4ef16f1af7888838
Porkepix <porkepix@gmail.com>
2024-03-01 21:47:20 +0100

    wavpack 5.7.0

--- Formula/w/wavpack.rb
+++ Formula/w/wavpack.rb
@@ -1,8 +1,8 @@
 class Wavpack < Formula
   desc "Hybrid lossless audio compression"
   homepage "https://www.wavpack.com/"
-  url "https://www.wavpack.com/wavpack-5.6.0.tar.bz2"
-  sha256 "8cbfa15927d29bcf953db35c0cfca7424344ff43ebe4083daf161577fb839cc1"
+  url "https://www.wavpack.com/wavpack-5.7.0.tar.bz2"
+  sha256 "8944b237968a1b3976a1eb47cd556916e041a2aa8917495db65f82c3fcc2a225"
   license "BSD-3-Clause"
 
   bottle do
