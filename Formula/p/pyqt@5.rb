50a2b36739942794a6c56c4a639296fe9205ec5e
Michael Cho <michael@michaelcho.dev>
2024-03-01 18:16:50 -0500

    pyqt@5: drop `python@3.11` support and cleanup
    Also fix auto updating resources and match some changes in `pyqt`
    
    Signed-off-by: Michael Cho <michael@michaelcho.dev>

--- Formula/p/pyqt@5.rb
+++ Formula/p/pyqt@5.rb
@@ -16,88 +16,80 @@ class PyqtAT5 < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8a91fb71e099e85a26ae81d534e5fe123762c23153ebbfe581a2d011005a836"
   end
 
-  depends_on "pyqt-builder"      => :build
+  depends_on "pyqt-builder" => :build
   depends_on "python-setuptools" => :build
-  depends_on "python@3.11"       => [:build, :test]
-  depends_on "python@3.12"       => [:build, :test]
-  depends_on "sip"               => :build
+  depends_on "sip" => :build
+  depends_on "python@3.12"
   depends_on "qt@5"
 
   fails_with gcc: "5"
 
   # extra components
-  resource "PyQt5-sip" do
-    url "https://files.pythonhosted.org/packages/ee/81/fce2a475aa56c1f49707d9306b930695b6ff078c2242c9f2fd72a3214e1f/PyQt5_sip-12.13.0.tar.gz"
-    sha256 "7f321daf84b9c9dbca61b80e1ef37bdaffc0e93312edae2cd7da25b953971d91"
-  end
-
-  resource "PyQt3D" do
+  resource "pyqt3d" do
     url "https://files.pythonhosted.org/packages/a5/80/26e3394c25187854bd3b68865b2b46cfd285aae01bbf448ddcac6f466af0/PyQt3D-5.15.6.tar.gz"
     sha256 "7d6c6d55cd8fc221b313c995c0f8729a377114926f0377f8e9011d45ebf3881c"
   end
 
-  resource "PyQtChart" do
+  resource "pyqt5-sip" do
+    url "https://files.pythonhosted.org/packages/ee/81/fce2a475aa56c1f49707d9306b930695b6ff078c2242c9f2fd72a3214e1f/PyQt5_sip-12.13.0.tar.gz"
+    sha256 "7f321daf84b9c9dbca61b80e1ef37bdaffc0e93312edae2cd7da25b953971d91"
+  end
+
+  resource "pyqtchart" do
     url "https://files.pythonhosted.org/packages/eb/17/1d9bb859b3e09a06633264ad91249ede0abd68c1e3f2f948ae7df94702d3/PyQtChart-5.15.6.tar.gz"
     sha256 "2691796fe92a294a617592a5c5c35e785dc91f7759def9eb22da79df63762339"
   end
 
-  resource "PyQtDataVisualization" do
+  resource "pyqtdatavisualization" do
     url "https://files.pythonhosted.org/packages/9c/ff/6ba767b4e1dbc32c7ffb93cd5d657048f6a4edf318c5b8810c8931a1733b/PyQtDataVisualization-5.15.5.tar.gz"
     sha256 "8927f8f7aa70857ef00c51e3dfbf6f83dd9f3855f416e0d531592761cbb9dc7f"
   end
 
-  resource "PyQtNetworkAuth" do
+  resource "pyqtnetworkauth" do
     url "https://files.pythonhosted.org/packages/85/b6/6b8f30ebd7c15ded3d91ed8d6082dee8aebaf79c4e8d5af77b1172c805c2/PyQtNetworkAuth-5.15.5.tar.gz"
     sha256 "2230b6f56f4c9ad2e88bf5ac648e2f3bee9cd757550de0fb98fe0bcb31217b16"
   end
 
-  resource "PyQtWebEngine" do
-    url "https://files.pythonhosted.org/packages/cf/4b/ca01d875eff114ba5221ce9311912fbbc142b7bb4cbc4435e04f4f1f73cb/PyQtWebEngine-5.15.6.tar.gz"
-    sha256 "ae241ef2a61c782939c58b52c2aea53ad99b30f3934c8358d5e0a6ebb3fd0721"
-  end
-
-  resource "PyQtPurchasing" do
+  resource "pyqtpurchasing" do
     url "https://files.pythonhosted.org/packages/41/2a/354f0ae3fa02708719e2ed6a8c310da4283bf9a589e2a7fcf7dadb9638af/PyQtPurchasing-5.15.5.tar.gz"
     sha256 "8bb1df553ba6a615f8ec3d9b9c5270db3e15e831a6161773dabfdc1a7afe4834"
   end
 
-  def pythons
-    deps.map(&:to_formula)
-        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
-        .map { |f| f.opt_libexec/"bin/python" }
+  resource "pyqtwebengine" do
+    url "https://files.pythonhosted.org/packages/cf/4b/ca01d875eff114ba5221ce9311912fbbc142b7bb4cbc4435e04f4f1f73cb/PyQtWebEngine-5.15.6.tar.gz"
+    sha256 "ae241ef2a61c782939c58b52c2aea53ad99b30f3934c8358d5e0a6ebb3fd0721"
+  end
+
+  def python3
+    "python3.12"
   end
 
   def install
-    components = %w[PyQt3D PyQtChart PyQtDataVisualization PyQtNetworkAuth PyQtWebEngine PyQtPurchasing]
+    site_packages = prefix/Language::Python.site_packages(python3)
+    args = [
+      "--target-dir", site_packages,
+      "--scripts-dir", bin,
+      "--confirm-license",
+      "--no-designer-plugin",
+      "--no-qml-plugin"
+    ]
+    system "sip-install", *args
 
-    pythons.each do |python|
-      site_packages = prefix/Language::Python.site_packages(python)
-      args = [
-        "--target-dir", site_packages,
-        "--scripts-dir", bin,
-        "--confirm-license",
-        "--no-designer-plugin",
-        "--no-qml-plugin"
-      ]
-      system "sip-install", *args
-
-      resource("PyQt5-sip").stage do
-        system python, *Language::Python.setup_install_args(prefix, python)
-      end
-
-      components.each do |p|
-        resource(p).stage do
-          inreplace "pyproject.toml", "[tool.sip.project]", <<~EOS
-            [tool.sip.project]
-            sip-include-dirs = ["#{site_packages}/PyQt#{version.major}/bindings"]
-          EOS
-          system "sip-install", "--target-dir", site_packages
-        end
-      end
+    resource("pyqt5-sip").stage do
+      system python3, *Language::Python.setup_install_args(prefix, python3)
     end
 
-    # Replace hardcoded reference to Python version used with sip/pyqt-builder with generic python3.
-    bin.children.each { |script| inreplace script, Formula["python@3.12"].opt_bin/"python3.12", "python3" }
+    resources.each do |r|
+      next if r.name == "pyqt5-sip"
+
+      r.stage do
+        inreplace "pyproject.toml", "[tool.sip.project]", <<~EOS
+          [tool.sip.project]
+          sip-include-dirs = ["#{site_packages}/PyQt#{version.major}/bindings"]
+        EOS
+        system "sip-install", "--target-dir", site_packages
+      end
+    end
   end
 
   test do
@@ -116,9 +108,7 @@ class PyqtAT5 < Formula
       Xml
     ]
 
-    pythons.each do |python|
-      system python, "-c", "import PyQt#{version.major}"
-      components.each { |mod| system python, "-c", "import PyQt5.Qt#{mod}" }
-    end
+    system python3, "-c", "import PyQt#{version.major}"
+    components.each { |mod| system python3, "-c", "import PyQt5.Qt#{mod}" }
   end
 end
