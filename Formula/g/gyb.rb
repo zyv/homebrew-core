e887ec352bacb13317004fc89e5a3882cf42a9e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-02 17:51:37 +0000

    gyb: update 1.80 bottle.

--- Formula/g/gyb.rb
+++ Formula/g/gyb.rb
@@ -12,14 +12,14 @@ class Gyb < Formula
   head "https://github.com/GAM-team/got-your-back.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0dff4b1401d910ff0c5e216793b0844816e6b5a8945121ce802d1364599d6718"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6cff871be5e5cb05250945cc1713a7951561270c400c413b6e69b706d4e439b5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4cd5dc876b1ff24b3867ac9161798853f0593afc117af10085c719628f22efa7"
-    sha256 cellar: :any_skip_relocation, sonoma:         "424fd98779ad110758ad61468770821c2063d222ebc17e074ac8adf7ab3564c3"
-    sha256 cellar: :any_skip_relocation, ventura:        "c13a0abce8772cd396b81667049583a1d413b8970a58d75e93226d6cdba11a2f"
-    sha256 cellar: :any_skip_relocation, monterey:       "8fb11df7f0478abb642c23ca9bf6753f2171841c4feee02f70b694e4f2e7063e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "004b4223d8530cfc8b38448d748de2b6a3547191c9f4308f4d7cbfd940f51908"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "edda77a5091bdffdf731489e949e4dfec286420f38938e23299ea0aaf027b5e7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "03da0a9f157757194ab6499e61166628510e25b26d3ad2d4e4a522bdcfe3fcef"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ba242cc7a37289c40b8d05890cd83b84cdd73aa080e27f9473b01cdc9d67a424"
+    sha256 cellar: :any_skip_relocation, sonoma:         "0696959ca12cef91700224f7676a124897afd23eebe0006aeebd0ff6fa1cdf4f"
+    sha256 cellar: :any_skip_relocation, ventura:        "05ea328a9da6988d1fa913f07c6d825f292cdd8496d5cf7db6c5c631be99dac0"
+    sha256 cellar: :any_skip_relocation, monterey:       "84adf918e196a358bec1ec64d07708a97ebe25dcc77c3d77366ddbe47a6ecc10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "de29cdde581377fb6a67f587c676c5e3df28a9eade95188b30f25480b67c1e7e"
   end
 
   depends_on "python-certifi"
