fbe2b99710f62cccb0491a00f4ccacf331b36406
Porkepix <porkepix@gmail.com>
2024-03-02 12:03:35 +0100

    partio 1.17.3

--- Formula/p/partio.rb
+++ Formula/p/partio.rb
@@ -1,8 +1,8 @@
 class Partio < Formula
   desc "Particle library for 3D graphics"
   homepage "https://github.com/wdas/partio"
-  url "https://github.com/wdas/partio/archive/refs/tags/v1.17.1.tar.gz"
-  sha256 "5d00fbfc55817acb310bcaf61212a78b2c602e08b839060dfee69570b4b82464"
+  url "https://github.com/wdas/partio/archive/refs/tags/v1.17.3.tar.gz"
+  sha256 "08a571ca75cf133f373415dfd50b7d0e33a0dd1811dfb63409f0ae46652033c1"
   license "BSD-3-Clause"
 
   bottle do
