4a9e7456bbaed30110afe344aa7d4f340bfbc4aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-02 04:11:54 +0000

    openjpeg: update 2.5.2 bottle.

--- Formula/o/openjpeg.rb
+++ Formula/o/openjpeg.rb
@@ -7,13 +7,13 @@ class Openjpeg < Formula
   head "https://github.com/uclouvain/openjpeg.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "3e5e234a15bfb9327665c345bde4663b4883ebc14f2362bbbcb29941366d1f1b"
-    sha256 cellar: :any,                 arm64_ventura:  "c986d4c75923d4978323504ebd9b8bac32c3a87cc68bbe82044d927c9bdd6626"
-    sha256 cellar: :any,                 arm64_monterey: "13cb2341f7d6b057e708c30194e6e0140b17da7cc66b5c122bc8489b1ee7b39c"
-    sha256 cellar: :any,                 sonoma:         "ce3fa393dbd776295c254e4ec575b138d975d68976080f543b31caee9f9bbda3"
-    sha256 cellar: :any,                 ventura:        "268c6bc9cf31bd4545be45f71326159536f54d00d7204a002b9a053ebeaacf2f"
-    sha256 cellar: :any,                 monterey:       "f63cc5ed1b043efdec0e5ef1a652b8be4859b568f2303e03cc6c312afee01b99"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "43773d25db13d3a65292053864e485be8812de07086391cdbf5d5de0f001681b"
+    sha256 cellar: :any,                 arm64_sonoma:   "eef55952d48f1852f5e95a4f8bf336019ebf3800d5dc0fba79cf85ae1fe475d6"
+    sha256 cellar: :any,                 arm64_ventura:  "23d1814c74d0e84518faa642272a7b88a8b4e2e78e9fe8b3f3313483c180cbbe"
+    sha256 cellar: :any,                 arm64_monterey: "f9aa864e7bb14b36df6d7f83f5f5a10a65c9fb6b4227d7e6edcdecb9af2263dc"
+    sha256 cellar: :any,                 sonoma:         "db5e5cb9eac6d4f910b9845d8decbd3eb19ef25570eddaaec235a29c637ba929"
+    sha256 cellar: :any,                 ventura:        "02d67cf70fb174e35ce003e3914b0c867ce8b5f0aafde47eb34d24edd4e5443a"
+    sha256 cellar: :any,                 monterey:       "cf986cc726d5f07c6d033653ae19c27cc3cccc7e30dafc7cbb054ffe56fd0f57"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "28c920d3a84fbe6254d89f3a2a15db655d700066565701e57ce3e9210a56b343"
   end
 
   depends_on "cmake" => :build
