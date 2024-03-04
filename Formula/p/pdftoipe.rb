1506182c001fde2c483496b33b9cacac97b3d7d5
Rui Chen <rui@chenrui.dev>
2024-03-03 20:46:04 -0500

    pdftoipe: revision bump (poppler 24.03.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pdftoipe.rb
+++ Formula/p/pdftoipe.rb
@@ -4,7 +4,7 @@ class Pdftoipe < Formula
   url "https://github.com/otfried/ipe-tools/archive/refs/tags/v7.2.24.1.tar.gz"
   sha256 "561b18fc2a7ae45c37c5d0390443b37f4585549f09cd7765d856456be24e5dbc"
   license "GPL-2.0-or-later"
-  revision 18
+  revision 19
 
   bottle do
     sha256 cellar: :any,                 arm64_sonoma:   "b1ba8c8b3d670015b0bd005e31a7d01f37e5079ec6599e395530c2a97996b69d"
