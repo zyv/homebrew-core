d38b15cf1d861f3020e3e10d9516bc046f5b288e
FX Coudert <fxcoudert@gmail.com>
2024-03-04 10:45:27 +0000

    wstunnel: add 9.2.2 bottle.

--- Formula/w/wstunnel.rb
+++ Formula/w/wstunnel.rb
@@ -14,6 +14,16 @@ class Wstunnel < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "927f96da7e98774e30ba1c9733a508a8326af5434238823301c4feb82ed0e28b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e9b9b01db399833bf3e9895ccf3b9e822b6d5d78065166040e42a489e53c2896"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "493679b430bb00611c8faf3e211f29afb3194281f1abeb3e6917eb2da788a2ba"
+    sha256 cellar: :any_skip_relocation, sonoma:         "20d80d17752335f5ab3a04143c88e41bc4579d2216fa067109dd4928736877df"
+    sha256 cellar: :any_skip_relocation, ventura:        "9a90431041a7832500aaf28303993edda3027f879b145b9dcb6bac280a3cd0c7"
+    sha256 cellar: :any_skip_relocation, monterey:       "008dd09214a75431ad56a848569769c2b7d7c73dffc164e2302ab7adce3be558"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b8a722038a3a1722817d4cc55f19dec7f93005bd3bebba816c07ccf31c68c22d"
+  end
+
   depends_on "rust" => :build
 
   def install
