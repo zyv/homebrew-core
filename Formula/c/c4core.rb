4aa87b8ce4e04785a6de49c133b722cb5b98df70
Rui Chen <rui@chenrui.dev>
2024-03-03 19:32:09 -0500

    c4core 0.1.11 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/c4core.rb
@@ -0,0 +1,59 @@
+class C4core < Formula
+  desc "C++ utilities"
+  homepage "https://github.com/biojppm/c4core"
+  url "https://github.com/biojppm/c4core/releases/download/v0.1.11/c4core-0.1.11-src.tgz"
+  sha256 "67f4443f3742424f42453594e26e656f616dddfcf225a1d810e00473a741408c"
+  license all_of: ["MIT", "BSL-1.0"]
+
+  depends_on "cmake" => [:build, :test]
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION 3.5)
+      project(c4core_test)
+
+      find_package(c4core)
+
+      add_executable(c4core_test test.cpp)
+      target_link_libraries(c4core_test c4core::c4core)
+    EOS
+
+    (testpath/"test.cpp").write <<~EOS
+      #include "c4/charconv.hpp" // header file for character conversion utilities
+      #include "c4/format.hpp"   // header file for formatting utilities
+      #include <iostream>
+      #include <string>
+
+      int main() {
+          // using c4core to do integer to string conversion
+          int number = 42;
+          char buf[64];
+          c4::substr buf_sub(buf, sizeof(buf));
+          size_t num_chars = c4::itoa(buf_sub, number);
+          buf[num_chars] = '\0'; // Ensuring the buffer is null-terminated
+          std::cout << "The number is: " << buf << std::endl;
+
+          // For formatted output, first format into a buffer, then create a std::string from it
+          char format_buf[64];
+          snprintf(format_buf, sizeof(format_buf), "Formatted number: %d", number);
+          std::string formatted_string = format_buf;
+          std::cout << formatted_string << std::endl;
+
+          return 0;
+      }
+    EOS
+
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    assert_equal <<~EOS, shell_output("./build/c4core_test")
+      The number is: 42
+      Formatted number: 42
+    EOS
+  end
+end
