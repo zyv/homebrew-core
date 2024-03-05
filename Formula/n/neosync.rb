bc151943acdddee94776d5ff164c76c56d5937f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-05 21:57:06 +0000

    neosync 0.3.53

--- Formula/n/neosync.rb
+++ Formula/n/neosync.rb
@@ -1,8 +1,8 @@
 class Neosync < Formula
   desc "CLI for interfacing with Neosync"
   homepage "https://www.neosync.dev/"
-  url "https://github.com/nucleuscloud/neosync/archive/refs/tags/v0.3.52.tar.gz"
-  sha256 "2af163eafb66a7e70f5ee1e41a42fe33374f4a90e0b1966819039d51bbcbce2b"
+  url "https://github.com/nucleuscloud/neosync/archive/refs/tags/v0.3.53.tar.gz"
+  sha256 "d035bbc44e36bec3ebcde2cfee2d1fb152807ac0811dee1c7acfcb17a5eee67b"
   license "MIT"
   head "https://github.com/nucleuscloud/neosync.git", branch: "main"
 
