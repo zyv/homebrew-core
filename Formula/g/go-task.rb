0cfab703d6df87a0f8fbf64bc5c4f54c619e1178
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-05 00:41:01 +0000

    go-task 3.35.1

--- Formula/g/go-task.rb
+++ Formula/g/go-task.rb
@@ -1,8 +1,8 @@
 class GoTask < Formula
   desc "Task is a task runner/build tool that aims to be simpler and easier to use"
   homepage "https://taskfile.dev/"
-  url "https://github.com/go-task/task/archive/refs/tags/v3.35.0.tar.gz"
-  sha256 "ca9a0646c01dcfe414a6244385cae355d092d0fa507ca7fe72efc3b7df03032f"
+  url "https://github.com/go-task/task/archive/refs/tags/v3.35.1.tar.gz"
+  sha256 "09fec550ce19f531ebb94379cea747efff24f6da2465e5a60c53cf8cbae0d9ba"
   license "MIT"
   head "https://github.com/go-task/task.git", branch: "main"
 
