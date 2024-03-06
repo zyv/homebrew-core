a4465c1e24dbf7c24608a9d17636178b1d20a161
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-06 15:57:33 +0000

    steampipe 0.22.0

--- Formula/s/steampipe.rb
+++ Formula/s/steampipe.rb
@@ -1,8 +1,8 @@
 class Steampipe < Formula
   desc "Use SQL to instantly query your cloud services"
   homepage "https://steampipe.io/"
-  url "https://github.com/turbot/steampipe/archive/refs/tags/v0.21.8.tar.gz"
-  sha256 "9e519bed71c73241b2e816481121bd278988ed6d4408dbf4d8d376b08cc39cea"
+  url "https://github.com/turbot/steampipe/archive/refs/tags/v0.22.0.tar.gz"
+  sha256 "c6ddf025d178b8a7495ea943997fec63b41fa4dc346afae816a8b3185352631b"
   license "AGPL-3.0-only"
   head "https://github.com/turbot/steampipe.git", branch: "main"
 
