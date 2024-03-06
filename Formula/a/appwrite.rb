0ea1c6af1b6cb3874b6fd88e559898c2d02e27dc
Patrick Linnane <patrick@linnane.io>
2024-03-06 17:17:19 +0000

    appwrite: add 4.2.2 bottle.

--- Formula/a/appwrite.rb
+++ Formula/a/appwrite.rb
@@ -7,6 +7,16 @@ class Appwrite < Formula
   sha256 "328a2a58166cdd73a2cec41dd9ac88677a58809b37674b5e26270beae8e73f58"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "7484d686cf29dffbdd993d4b8fa1a8569fdaf30040fe36d6734d1320a74c4427"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7484d686cf29dffbdd993d4b8fa1a8569fdaf30040fe36d6734d1320a74c4427"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7484d686cf29dffbdd993d4b8fa1a8569fdaf30040fe36d6734d1320a74c4427"
+    sha256 cellar: :any_skip_relocation, sonoma:         "000ead456d79e1e6370b4767c36d9752c4a11e473c54681493453f5c9e6c0c19"
+    sha256 cellar: :any_skip_relocation, ventura:        "000ead456d79e1e6370b4767c36d9752c4a11e473c54681493453f5c9e6c0c19"
+    sha256 cellar: :any_skip_relocation, monterey:       "000ead456d79e1e6370b4767c36d9752c4a11e473c54681493453f5c9e6c0c19"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7484d686cf29dffbdd993d4b8fa1a8569fdaf30040fe36d6734d1320a74c4427"
+  end
+
   depends_on "node"
 
   def install
