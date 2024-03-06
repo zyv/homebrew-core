0da9de413c4329814b4241bea0e000d14a17f049
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-06 22:19:18 +0000

    tox: update 4.14.1 bottle.

--- Formula/t/tox.rb
+++ Formula/t/tox.rb
@@ -8,13 +8,13 @@ class Tox < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "21c7862184c54c365325337ae62ed8c1537447764748fa7f59e778f718d008e4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0c66c83591dd86417023015038098c7fd7e3ff53935cb435cc98e081f0810ec1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2be14cf9a845faca37933521e33dba32cd976f9006bbeb6085f0ceb5eec525d3"
-    sha256 cellar: :any_skip_relocation, sonoma:         "84121eee797fa18a79ebd2034073b41109f8b1ff1f2eb1114d8d0042804abf29"
-    sha256 cellar: :any_skip_relocation, ventura:        "253a918b13c9816177253afea804804bc7b3299dab325b9a074a1e07d7ffabf6"
-    sha256 cellar: :any_skip_relocation, monterey:       "41551810eedf1e16793cb9fd2265455fc423752454741fc73423153dc853f0fd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bdc504b7a1f3dc549b3b34b6a80f693d5c0dcf8da3a90770217b9eb069ef368c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "34cd91a8e2b4239984323b88c8ec075ce64aef18e840d91bd0cffc5b83a6a8ed"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a2d7b505c4d12feb71ede26cf9c80a0d3d2cecca6fba288d5e3c656fa507785c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4588c6643962ce317f80509dc7aedb50cb5e752b610250439e52f0cafc4ecec2"
+    sha256 cellar: :any_skip_relocation, sonoma:         "b6a5addb42d8e8725dad1b0af5d426c18b7bb9e6d7225b3cd3ae17e3ef11e2eb"
+    sha256 cellar: :any_skip_relocation, ventura:        "4f89f7becc3a8c73735fd58632b086b715fd77dcdbecfee29044ad6975aec200"
+    sha256 cellar: :any_skip_relocation, monterey:       "e61e55803b47cf545b7e3d399e0a0bef36b6981f072e1faedda3d49e888c9974"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9d09c1a8252356b6862e11c6a6eb5a6026c736c371fe73f303b3d512aaceec07"
   end
 
   depends_on "python@3.12"
