040592950761f34bbd6e5d5c39d0ae1cc3849ab0
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-03-07 22:34:29 +0800

    git-imerge: drop `python-setuptools` dependency

--- Formula/g/git-imerge.rb
+++ Formula/g/git-imerge.rb
@@ -1,4 +1,6 @@
 class GitImerge < Formula
+  include Language::Python::Virtualenv
+
   desc "Incremental merge for git"
   homepage "https://github.com/mhagger/git-imerge"
   license "GPL-2.0-or-later"
@@ -25,15 +27,10 @@ class GitImerge < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "96f10ccf33457be8b0e265a79b4f270c9c6c8aa5c2ea51423e2b8f39d4a73847"
   end
 
-  depends_on "python-setuptools" => :build
   depends_on "python@3.12"
 
-  def python3
-    "python3.12"
-  end
-
   def install
-    system python3, "-m", "pip", "install", *std_pip_args, "."
+    virtualenv_install_with_resources
     bash_completion.install "completions/git-imerge"
   end
 
