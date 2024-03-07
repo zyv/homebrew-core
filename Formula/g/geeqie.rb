60c6c893bee36a26905210aaa966a45984af0a15
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-07 12:47:30 +0000

    geeqie 2.3
    geeqie: add upstream build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/geeqie.rb
+++ Formula/g/geeqie.rb
@@ -1,10 +1,9 @@
 class Geeqie < Formula
   desc "Lightweight Gtk+ based image viewer"
   homepage "https://www.geeqie.org/"
-  url "https://github.com/BestImageViewer/geeqie/releases/download/v2.2/geeqie-2.2.tar.xz"
-  sha256 "899ac33b801e0e83380f79e9094bc2615234730ccf6a02d93fd6da3e6f8cfe94"
+  url "https://github.com/BestImageViewer/geeqie/releases/download/v2.3/geeqie-2.3.tar.xz"
+  sha256 "cd5cad97f8d0e8c62025334d62688fcf6f82be73a7716c5ca16b205d59b8519b"
   license "GPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url :stable
@@ -50,6 +49,12 @@ class Geeqie < Formula
 
   uses_from_macos "python" => :build
   uses_from_macos "vim" => :build # for xxd
+
+  # macos build patch
+  patch do
+    url "https://github.com/BestImageViewer/geeqie/commit/4a9376a78d258ff11e9db33985abbfb9a7be614c.patch?full_index=1"
+    sha256 "f9c49cec18cbeb9e764e7ed51cdbeec005f5dbcf99efc26f7e254bb32be1acc9"
+  end
 
   def install
     system "meson", "setup", "build", "-Dlua=disabled", *std_meson_args
