25b1ce4447545bc1355af0b58711acf9b6435c2d
Mike McQuaid <mike@mikemcquaid.com>
2024-03-07 17:33:32 +0000

    ddns-go: use default hash syntax.
    For https://github.com/Homebrew/brew/pull/16848

--- Formula/d/ddns-go.rb
+++ Formula/d/ddns-go.rb
@@ -24,7 +24,7 @@ class DdnsGo < Formula
       -X main.version=v#{version}
       -X main.buildTime=#{time.iso8601}
     ]
-    system "go", "build", *std_go_args(ldflags: ldflags)
+    system "go", "build", *std_go_args(ldflags:)
   end
 
   test do
