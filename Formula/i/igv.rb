d8d8fe204acf3e0eea18b52df679cf2ae4cbbe48
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-07 00:43:46 +0000

    igv 2.17.3

--- Formula/i/igv.rb
+++ Formula/i/igv.rb
@@ -1,8 +1,8 @@
 class Igv < Formula
   desc "Interactive Genomics Viewer"
   homepage "https://igv.org/doc/desktop/"
-  url "https://data.broadinstitute.org/igv/projects/downloads/2.17/IGV_2.17.2.zip"
-  sha256 "b47dd43848dc0ca0bed81c03f55ad2188e42726f451bfdb5caebc8759ff360c0"
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.17/IGV_2.17.3.zip"
+  sha256 "58369ad1e156dc27a7cd2861c238049563396be605a2ab1b172a008a69ad7cb4"
   license "MIT"
 
   livecheck do
