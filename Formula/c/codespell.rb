50cea3f060b1b153c95781d74de2d076cfac6c89
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-07 20:38:38 +0000

    codespell: update 2.2.6 bottle.

--- Formula/c/codespell.rb
+++ Formula/c/codespell.rb
@@ -8,14 +8,14 @@ class Codespell < Formula
   license "GPL-2.0-only"
 
   bottle do
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0d883e3146afd3b3c3d43fb66fcb12b4a466be1ea59e958c8d4cab8081b031cc"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a93eb0e3cbf715721a6ecf29f042724c330b8de6e99262ee9a26d78dae4ff840"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "849e750f5af072fb384b837aa88de95726a3862eeb4d69e44459c986448ecde1"
-    sha256 cellar: :any_skip_relocation, sonoma:         "d3f40a6c36275b652dab12628b52b076b7ee913643b32585437595878eddb99d"
-    sha256 cellar: :any_skip_relocation, ventura:        "5143b890b0acd3dda48e4290dcacd85026f2c19c014500f160238705ae1042b6"
-    sha256 cellar: :any_skip_relocation, monterey:       "27d74d2c680f0bb58eccbb5687659116556574b3b6c1c71e9cc1ccd0437871be"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1c82558ab3dfb54399a1a33137fb37394b7f9c57970f92da0d279777d2213200"
+    rebuild 4
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "56f15d39c3a8a94ae1e97b2ac5bcf319a7599ffc31513276b5cfefc900342266"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9e7bf4d7de602e111662c378d93220fe9407b127020f21c5bee0b9121fcd9d38"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8951acd26d4fd939043bf67c27cd905fe714852851cd64d5986ea98bd9b48cca"
+    sha256 cellar: :any_skip_relocation, sonoma:         "2a8dcc35f274a23a166a14f9d9822095b29be5d5af9a51c9a9d8ac8b2ee6b9a7"
+    sha256 cellar: :any_skip_relocation, ventura:        "ae3d3e3655da690c4a4c0e3b314a7590031d280abf8dfd86a2ab813000662a9b"
+    sha256 cellar: :any_skip_relocation, monterey:       "075ef1d096144f751fda548a2c12b3e24f83b29370d7b5f4085fa7c0191d1eb9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dab6f78d63a1c9759ad658c8bf95ac5b2977eda4e4aeb5c335c86c73c1187930"
   end
 
   depends_on "python@3.12"
