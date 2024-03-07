42cb49c31f3ad63e93e26f05abdc208ddece8cc6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-07 21:50:56 +0000

    repo 2.43

--- Formula/r/repo.rb
+++ Formula/r/repo.rb
@@ -4,8 +4,8 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      tag:      "v2.42",
-      revision: "5554572f02537b8646139d59ab520e59e1d5f7b3"
+      tag:      "v2.43",
+      revision: "edadb25c0270398e9afa3eb0093d6b94aa51c3f4"
   license "Apache-2.0"
   version_scheme 1
 
