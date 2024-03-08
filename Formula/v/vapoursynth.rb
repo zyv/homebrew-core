8eff0cd20657b286c13631f974919cc53651674c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-08 21:59:46 +0000

    vapoursynth 66

--- Formula/v/vapoursynth.rb
+++ Formula/v/vapoursynth.rb
@@ -1,8 +1,8 @@
 class Vapoursynth < Formula
   desc "Video processing framework with simplicity in mind"
   homepage "https://www.vapoursynth.com"
-  url "https://github.com/vapoursynth/vapoursynth/archive/refs/tags/R65.tar.gz"
-  sha256 "2bde5233b82d914b5e985119ed9cc344e3c27c3c068b5c4ab909176cd1751dce"
+  url "https://github.com/vapoursynth/vapoursynth/archive/refs/tags/R66.tar.gz"
+  sha256 "e2c82b1f583adbc33dabbe59c0dc65e6aede70dedebe79be94155cb38d418b2c"
   license "LGPL-2.1-or-later"
   head "https://github.com/vapoursynth/vapoursynth.git", branch: "master"
 
