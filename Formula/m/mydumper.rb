8a256bf30ed87a7703b9e83e6453b1cc84f6f195
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-08 19:03:10 +0000

    mydumper 0.16.1-1

--- Formula/m/mydumper.rb
+++ Formula/m/mydumper.rb
@@ -1,10 +1,9 @@
 class Mydumper < Formula
   desc "How MySQL DBA & support engineer would imagine 'mysqldump' ;-)"
   homepage "https://launchpad.net/mydumper"
-  url "https://github.com/mydumper/mydumper/archive/refs/tags/v0.15.1-3.tar.gz"
-  sha256 "a25bcb283b2eab192640da2e55ab7798d92293693bb717fb1490d59bc35887ee"
+  url "https://github.com/mydumper/mydumper/archive/refs/tags/v0.16.1-1.tar.gz"
+  sha256 "3219b7c2a16f4ce1848315583090a0b05c1dc781b8cebc2948441c247eb28deb"
   license "GPL-3.0-or-later"
-  revision 2
 
   livecheck do
     url :stable
