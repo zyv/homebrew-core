05af2a02fbaca479a13db2d9876df2bebe89929a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-09 12:27:17 +0000

    broot 1.36.0

--- Formula/b/broot.rb
+++ Formula/b/broot.rb
@@ -1,8 +1,8 @@
 class Broot < Formula
   desc "New way to see and navigate directory trees"
   homepage "https://dystroy.org/broot/"
-  url "https://github.com/Canop/broot/archive/refs/tags/v1.35.0.tar.gz"
-  sha256 "1d3c2674a95c8c13ff66d356aab04da4f7e7695e2073c2d3a85842916f4d307e"
+  url "https://github.com/Canop/broot/archive/refs/tags/v1.36.0.tar.gz"
+  sha256 "2929d18c2398afa073f170941a45cd8ab4693d272e1c487c95f7e527569cda99"
   license "MIT"
   head "https://github.com/Canop/broot.git", branch: "master"
 
