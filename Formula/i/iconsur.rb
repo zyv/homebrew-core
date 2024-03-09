e4e018b134b623ed401bde2e7054382423b372c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-09 17:24:30 +0000

    iconsur: update 1.7.0 bottle.

--- Formula/i/iconsur.rb
+++ Formula/i/iconsur.rb
@@ -12,15 +12,13 @@ class Iconsur < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "34926c4bd6b066a9ca422ad09daa4a04c7b3ac40e6e094eec10a74a3f3ffce9c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8c89a0bafa76e65252ca37540e7e80079894ef63150c4d29fdbaa2f92ce04359"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "304d38f9248f8979b2b483280cac76690c14b61ff87f08484c0dc2a9387c21b8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "304d38f9248f8979b2b483280cac76690c14b61ff87f08484c0dc2a9387c21b8"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a5755fcf3326e667794b2cec721d12d5488c34da0ba53b977d7c5431e079ed7b"
-    sha256 cellar: :any_skip_relocation, ventura:        "eaf5ee9a4bc080056cf6ea44e633660b90309b201d17b4b7165eaa3e62656d63"
-    sha256 cellar: :any_skip_relocation, monterey:       "77884a43974a1b6a917d415b12d2b6fe476dee4215e986e7123b9d911b0a95cc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "77884a43974a1b6a917d415b12d2b6fe476dee4215e986e7123b9d911b0a95cc"
-    sha256 cellar: :any_skip_relocation, catalina:       "77884a43974a1b6a917d415b12d2b6fe476dee4215e986e7123b9d911b0a95cc"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "7e67e0fa5d12b5598bb3f6755547a50e20da472da57a7df5c68d2ca3380f7140"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cdfdf024544f90244bfefea1697a7100c7e2adde6a05fd51a4e4e54c5f3ebc98"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "11889c0c42c2a044fe9dc081f50cf332f606e922837b5a765141ef2340e0cdb6"
+    sha256 cellar: :any_skip_relocation, sonoma:         "488565e07e909605382b2ed833216e8b87ac499225443e2b2ace61f1508b6a6b"
+    sha256 cellar: :any_skip_relocation, ventura:        "1a1f49255b7376a223d04e38c2dbd0ef278376e161670b0fca923b94e4d12227"
+    sha256 cellar: :any_skip_relocation, monterey:       "f9a727a3148a71fdda8a537c85028002fb33f7ff9a91db86a4d50971db5b4a42"
   end
 
   depends_on :macos
