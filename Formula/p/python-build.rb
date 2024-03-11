b4dcf89a606877134767798ed171f47c35e10960
Branch Vincent <branchevincent@gmail.com>
2024-03-10 19:57:08 -0700

    python-build: vendor deps

--- Formula/p/python-build.rb
+++ Formula/p/python-build.rb
@@ -1,4 +1,6 @@
 class PythonBuild < Formula
+  include Language::Python::Virtualenv
+
   desc "Simple, correct PEP 517 build frontend"
   homepage "https://github.com/pypa/build"
   url "https://files.pythonhosted.org/packages/55/f7/7bd626bc41b59152248087c1b56dd9f5d09c3f817b96075dc3cbda539dc7/build-1.1.1.tar.gz"
@@ -16,35 +18,23 @@ class PythonBuild < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "31b35abfa8a384e706955fadf27e3abed1e327ce52761d6d4e8ad532a120cb66"
   end
 
-  depends_on "python-flit-core" => :build
-  depends_on "python@3.11" => [:build, :test]
-  depends_on "python@3.12" => [:build, :test]
-  depends_on "python-packaging"
-  depends_on "python-pyproject-hooks"
+  depends_on "python@3.12"
 
-  def pythons
-    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/ee/b5/b43a27ac7472e1818c4bafd44430e69605baefe1f34440593e0332ec8b4d/packaging-24.0.tar.gz"
+    sha256 "eb82c5e3e56209074766e6885bb04b8c38a0c015d0a30036ebe7ece34c9989e9"
+  end
+
+  resource "pyproject-hooks" do
+    url "https://files.pythonhosted.org/packages/25/c1/374304b8407d3818f7025457b7366c8e07768377ce12edfe2aa58aa0f64c/pyproject_hooks-1.0.0.tar.gz"
+    sha256 "f271b298b97f5955d53fb12b72c1fb1948c22c1a6b70b315c54cedaca0264ef5"
   end
 
   def install
-    pythons.each do |python|
-      python_exe = python.opt_libexec/"bin/python"
-      system python_exe, "-m", "pip", "install", *std_pip_args, "."
-    end
-  end
-
-  def caveats
-    <<~EOS
-      To run `pyproject-build`, you may need to `brew install #{pythons.last}`
-    EOS
+    virtualenv_install_with_resources
   end
 
   test do
-    pythons.each do |python|
-      python_exe = python.opt_libexec/"bin/python"
-      system python_exe, "-c", "import build"
-    end
-
     stable.stage do
       system bin/"pyproject-build"
       assert_predicate Pathname.pwd/"dist/build-#{stable.version}.tar.gz", :exist?
