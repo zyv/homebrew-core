cfdce5ca729f062da5dc4213316569ccb60c2ed6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-11 01:44:57 +0000

    katago: update 1.14.1 bottle.

--- Formula/k/katago.rb
+++ Formula/k/katago.rb
@@ -11,13 +11,13 @@ class Katago < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "2db17fe4a7e4faf7b848fe8be9bedb05dccafe6202d574c159f9d43de9aa2f78"
-    sha256 cellar: :any,                 arm64_ventura:  "110dd79c63c3b612b446f5dbb149301581867a4c0b8024bf4894cc2fce2cd5c5"
-    sha256 cellar: :any,                 arm64_monterey: "815c9a37762eb15788c8e7ad6b7da86a0ff63c9137967b07876d6ea313621982"
-    sha256 cellar: :any,                 sonoma:         "69351321bb32aa67da12b70e438c1bbacdc02e516f01610a07df1c89739d285a"
-    sha256 cellar: :any,                 ventura:        "64a1e1d62f824b8015843d52de8272eae02a696c10f8d2f7dac18fd815ba8244"
-    sha256 cellar: :any,                 monterey:       "28da9e121ab022105b61f776be0c002853c0e170c60f3991dc65d90b8db3eb5e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6b8ecc133a5f941cd3b84ee0d2d124d3fb628e595ccbd9a9ecfc4046adaec920"
+    sha256 cellar: :any,                 arm64_sonoma:   "e2b7df163de75c2d1ffc03a2a3cd8a458fc7108e01b6dfcefbdc6517302cd191"
+    sha256 cellar: :any,                 arm64_ventura:  "45ae58e3412cadde15bc46e89225b9b259b44e81f5c0304aa9490872feec5790"
+    sha256 cellar: :any,                 arm64_monterey: "218cd81826671227870bb65317bb42d9b7b1742103582f42e30b9b365b7932b6"
+    sha256 cellar: :any,                 sonoma:         "30d97d287876702216d97d59e32a3df21c1586134458be069ff52aea48253e02"
+    sha256 cellar: :any,                 ventura:        "9df9faaab88d83c002dc0c0733ce4e61d100a40abf72f0f94ce3e03162b09de1"
+    sha256 cellar: :any,                 monterey:       "009315fcc87bd9b85391c3d94d4716b9da8dd1b0f2e88b0b8c34a7a8600bd337"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf856497d4397a8c46c27d44cbf37fe675f031e25aed4ca43e10d386a2e7fde5"
   end
 
   depends_on "cmake" => :build
