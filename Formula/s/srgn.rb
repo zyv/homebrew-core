cf616f861dc849817cc9c62523d5404f0d9ef096
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-11 15:43:15 +0000

    srgn: add 0.11.0 bottle.

--- Formula/s/srgn.rb
+++ Formula/s/srgn.rb
@@ -6,6 +6,16 @@ class Srgn < Formula
   license "MIT"
   head "https://github.com/alexpovel/srgn.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9502974bda8a829cd0f02a488e920aebf220b2b730ae3b3f9139bec6336e9c7d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6c39e18f77523336c1bb8fc05aeb14c2c730731278474ad1146d826758b7cec5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7fd6c628f68d4b9c134e65570c15bf10f7713ee6da12ae88ef2dfa213676f4ec"
+    sha256 cellar: :any_skip_relocation, sonoma:         "4d13fb260523de7976895358288c7ba0874d682d1bd087cecc1c14bc290cea91"
+    sha256 cellar: :any_skip_relocation, ventura:        "2fa8f93f0dab332c957d4cde998c5e09ad207b665c72cc53d0cab48cd3dc3f22"
+    sha256 cellar: :any_skip_relocation, monterey:       "b4a36b438b7f3e3ff86ab6689bc8aca99562d5032ca208d324f2c0bccf8be215"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c815fb0a60589747ffd43c0c3242eb5e74079ade72f0dddae0cd99aa44e24159"
+  end
+
   depends_on "rust" => :build
 
   def install
