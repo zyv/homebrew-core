4d6b11d40b3546400e1a58e27e0043678ba709ae
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2024-03-11 18:05:32 -0400

    runme 3.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/r/runme.rb
+++ Formula/r/runme.rb
@@ -1,8 +1,8 @@
 class Runme < Formula
   desc "Execute commands inside your runbooks, docs, and READMEs"
   homepage "https://runme.dev/"
-  url "https://github.com/stateful/runme/archive/refs/tags/v3.1.1.tar.gz"
-  sha256 "905c9090437398557c8326f67c875f8a415fdcc6b6d2e73582655f12bc4cfa2f"
+  url "https://github.com/stateful/runme/archive/refs/tags/v3.2.0.tar.gz"
+  sha256 "346c9707e043a02f953a55f8bd2c19cdaa773a04cf36925fd179e29fa366cfaa"
   license "Apache-2.0"
   head "https://github.com/stateful/runme.git", branch: "main"
 
