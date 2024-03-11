72e102c4915f538b03044deab115cf975b93d7a3
Branch Vincent <branchevincent@gmail.com>
2024-03-10 22:05:43 -0700

    twoping: use venv

--- Formula/t/twoping.rb
+++ Formula/t/twoping.rb
@@ -1,4 +1,6 @@
 class Twoping < Formula
+  include Language::Python::Virtualenv
+
   desc "Ping utility to determine directional packet loss"
   homepage "https://www.finnie.org/software/2ping/"
   url "https://www.finnie.org/software/2ping/2ping-4.5.1.tar.gz"
@@ -12,17 +14,13 @@ class Twoping < Formula
     sha256 cellar: :any_skip_relocation, all: "75e4fc4ecab8c1c779214c4220d5d7c0c61b3d6a2d9f64774a12950cf22bcfe0"
   end
 
-  depends_on "python-setuptools" => :build
   depends_on "python@3.12"
 
   def install
-    python3 = "python3.12"
-    ENV.prepend_create_path "PYTHONPATH", libexec/Language::Python.site_packages(python3)
-    system python3, *Language::Python.setup_install_args(libexec, python3)
+    virtualenv_install_with_resources
+
     man1.install "doc/2ping.1"
     man1.install_symlink "2ping.1" => "2ping6.1"
-    bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
     bash_completion.install "2ping.bash_completion" => "2ping"
   end
 
