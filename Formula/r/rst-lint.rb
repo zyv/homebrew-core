423e2378aaf7788e14708f47406e5dfa85585ad1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-11 08:54:30 +0000

    rst-lint: update 1.4.0 bottle.

--- Formula/r/rst-lint.rb
+++ Formula/r/rst-lint.rb
@@ -8,14 +8,14 @@ class RstLint < Formula
   license "Unlicense"
 
   bottle do
-    rebuild 5
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "1db6397fe2d4b35e67f124e9582f3197e28b4932ab113ec4cac2f034cef2a654"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "431cbcd662e20a5e4df5738eb6c6dbb71cbabd450f83daeff704a1b98885d936"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6eda922bdc79d0b4aa1cab87d7d1f8575b191ee0dd03a9913b667d024dbdc8cd"
-    sha256 cellar: :any_skip_relocation, sonoma:         "41a6b658d2b234bf2278667d39bea343d47511d9f7f72e4d76505384cc093fec"
-    sha256 cellar: :any_skip_relocation, ventura:        "975a231fd9109436a7797b5dfd2a9fccd30fd84a1c79c80e4167711ff1b14038"
-    sha256 cellar: :any_skip_relocation, monterey:       "daa0ff6cbd83610b8439648ab9648fa6fd818394ca77007f94af04cb9bfe052a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "108a99943b09360f91f3e2f45cd3ae287408ec38f17c806c40a84463349b679e"
+    rebuild 6
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e905e695e089e2c31d67b53fad2b3162e7e1b3c27068535ab6fe9c5eed55d367"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e905e695e089e2c31d67b53fad2b3162e7e1b3c27068535ab6fe9c5eed55d367"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e905e695e089e2c31d67b53fad2b3162e7e1b3c27068535ab6fe9c5eed55d367"
+    sha256 cellar: :any_skip_relocation, sonoma:         "e905e695e089e2c31d67b53fad2b3162e7e1b3c27068535ab6fe9c5eed55d367"
+    sha256 cellar: :any_skip_relocation, ventura:        "e905e695e089e2c31d67b53fad2b3162e7e1b3c27068535ab6fe9c5eed55d367"
+    sha256 cellar: :any_skip_relocation, monterey:       "e905e695e089e2c31d67b53fad2b3162e7e1b3c27068535ab6fe9c5eed55d367"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e0cb2d8ff6a65493086ab426e3391cff943cd74e3521859f3728427cc7472591"
   end
 
   depends_on "python@3.12"
