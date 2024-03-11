dac18f4fed269a5db36df0d3d5af2d5c81221eb0
Rui Chen <rui@chenrui.dev>
2024-03-11 16:47:49 -0400

    odpi 5.2.0
    autobump: add odpi
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/odpi.rb
+++ Formula/o/odpi.rb
@@ -1,8 +1,8 @@
 class Odpi < Formula
   desc "Oracle Database Programming Interface for Drivers and Applications"
   homepage "https://oracle.github.io/odpi/"
-  url "https://github.com/oracle/odpi/archive/refs/tags/v5.1.0.tar.gz"
-  sha256 "b3e25d37dda78ac42c91eecfb48ebba932712189d2072f6d9064283f2093a59d"
+  url "https://github.com/oracle/odpi/archive/refs/tags/v5.2.0.tar.gz"
+  sha256 "9e2eb3a10e4ab89691d847d5106cfcde8c1126b7959d3ee06076ea3f5b5a9d19"
   license any_of: ["Apache-2.0", "UPL-1.0"]
 
   bottle do
