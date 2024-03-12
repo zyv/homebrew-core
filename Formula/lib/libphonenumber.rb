f5cf87ed7eea3c6dafaf91958f7b415aa4fde23f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-12 06:56:51 +0000

    libphonenumber 8.13.32

--- Formula/lib/libphonenumber.rb
+++ Formula/lib/libphonenumber.rb
@@ -1,10 +1,9 @@
 class Libphonenumber < Formula
   desc "C++ Phone Number library by Google"
   homepage "https://github.com/google/libphonenumber"
-  url "https://github.com/google/libphonenumber/archive/refs/tags/v8.13.31.tar.gz"
-  sha256 "6d03446f096259552d34580a42ef6ebb450f2419dde3801a60fffca4240b3227"
+  url "https://github.com/google/libphonenumber/archive/refs/tags/v8.13.32.tar.gz"
+  sha256 "88ba4be45cb95bd01b87952d37416145bd135e203c1f6893a610983463c0cacd"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url :stable
