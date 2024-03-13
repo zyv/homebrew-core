25f9cf3a9996e8f7f5252563e9fdfcc73899122b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-13 03:12:45 +0000

    jenv: update 0.5.7 bottle.

--- Formula/j/jenv.rb
+++ Formula/j/jenv.rb
@@ -7,7 +7,7 @@ class Jenv < Formula
   head "https://github.com/jenv/jenv.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "24cae0d62470b6aaf396b57872ce3afa648a0f7b9124b0571a34cd4abe37a7dc"
+    sha256 cellar: :any_skip_relocation, all: "35224b1400c377abd56e99f5e6caec0b48672a935cd3eb250046cf5ab107948e"
   end
 
   def install
