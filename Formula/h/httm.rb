3d649ca5a7155a3325fdd274d276a235ff372d7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-13 08:42:11 +0000

    httm: update 0.37.2 bottle.

--- Formula/h/httm.rb
+++ Formula/h/httm.rb
@@ -7,13 +7,13 @@ class Httm < Formula
   head "https://github.com/kimono-koans/httm.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "05fdfc68342b97d5ac21482fd8308941a89ab504dca90056f74ca51cc97470c5"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b152e69a659a53acb37c4f61b5007eea468b6375b17cd9dfcecbede7934b4fdb"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0662f12f9e49f4e6ff110ba5ab5215020cbf63461f3f8365d21ef65939742a60"
-    sha256 cellar: :any_skip_relocation, sonoma:         "920a0c4b8593fdd960fe2d4ca26538bf23768907468e1ada096682f806bf4927"
-    sha256 cellar: :any_skip_relocation, ventura:        "a305af5edd1290910422644435ebdb28148dc0967fe6b0e6b73888fd4537d2ea"
-    sha256 cellar: :any_skip_relocation, monterey:       "e1790bf9822991e1d70f38c842c1df6d8d139495b98fb5e2170a4985df991d56"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "057169a8791f26ea278efce4e65762284c4adbc8a1eb4f98ec606fd1ca9bee61"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c9fc8f71198bf4b4904d601afd33ef0669f77410d9bd8b499ea8edb6f8476b1d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "96c13b48672531dcb2944a82224b59118145a590eee49aa218371bdde9f808d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ca4eb35d66153a1ee35bb08bb652ba8216702597d3f89e6173167025075ac08a"
+    sha256 cellar: :any_skip_relocation, sonoma:         "3e96813197d10ac151caff75ccef680046d7412a6da971d8322ff66a03d42292"
+    sha256 cellar: :any_skip_relocation, ventura:        "f9b1bb810a4d6d1b4ce77810eac6251b92ca65210d3cf83ca9e9913bdd89db37"
+    sha256 cellar: :any_skip_relocation, monterey:       "00c3fea8a2f4f1190699432514fc2b0f6b57371189bece18aaef3a738c84e7fd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "55bfe082efb194ea9ead25e240acc71aeea94ff7aba9f7e67a61d5db47babef8"
   end
 
   depends_on "rust" => :build
