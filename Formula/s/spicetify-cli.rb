13473554898fde0b08d8f98091274912f1e5d0e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-13 22:47:04 +0000

    spicetify-cli: update 2.34.1 bottle.

--- Formula/s/spicetify-cli.rb
+++ Formula/s/spicetify-cli.rb
@@ -7,13 +7,13 @@ class SpicetifyCli < Formula
   head "https://github.com/spicetify/spicetify-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "70a491cfbd28e07732ab994eed66ef791d6d0fc305c05543931d86582a70158f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a369631a46830a35cedffaeda91468aba23625189b8d59dd48cf8ccff2d96613"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1a8a8ec92e0b6b20af4ff58efb948a13cfdea3c07ef12d43bb9954b04093657e"
-    sha256 cellar: :any_skip_relocation, sonoma:         "bf32af6721cc7d4f5e6e554449ae0c8ea57fc4d9b6679082f116216e9fb9bf31"
-    sha256 cellar: :any_skip_relocation, ventura:        "b4f71f3ea3ed837f0eeaebe105f92b4b644a6aea5369b46c3e36a14e6b9ca0b6"
-    sha256 cellar: :any_skip_relocation, monterey:       "5cdc29be5182577b04b99fdb6c8b40c0de87515ef770a3e09c3532bad0d23226"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ccf6048cbca168ae2ffbc62f8b252ebb83c73b1136a3e23c35600d6edbb58a89"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "f406c02302f70735772a986f82b292804ac4ca89fe5a380da1e73792fe6503d3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a502415c0070849bad4f25854ea202957b0cefca637a2351776e0d4863736510"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "056e6895793dd57eec4656f3024b90ecb1c147d5ac2a0541490f1f6d7c47367b"
+    sha256 cellar: :any_skip_relocation, sonoma:         "aaed60bd7853057de3b0b12780d3564ddf412027a0d58080a63bb6c22c84b591"
+    sha256 cellar: :any_skip_relocation, ventura:        "da6e65e1e9faca852953c195d9a1cfe39d83a8982fe4b7c3c178eeb99c24d529"
+    sha256 cellar: :any_skip_relocation, monterey:       "6a7ec5b525a8e2f8c1d386f06c79ae22c2af932f4e0a6d8294fadbca2f24f90e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "519176ba41cb99bcc984497d05edc06d85d2904fbea18e2d366a3069b6df8e2d"
   end
 
   depends_on "go" => :build
