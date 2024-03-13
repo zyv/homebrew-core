9eabf3de8ba36104b70603a2b2ac0777e07d9196
Pandemics <lionel.rigoux+pandemics@gmail.com>
2024-03-13 09:30:59 +0000

    pandemics 0.12.0

--- Formula/p/pandemics.rb
+++ Formula/p/pandemics.rb
@@ -3,10 +3,9 @@ require "language/node"
 class Pandemics < Formula
   desc "Converts your markdown document in a simplified framework"
   homepage "https://pandemics.gitlab.io"
-  url "https://registry.npmjs.org/pandemics/-/pandemics-0.11.11.tgz"
-  sha256 "e9e54497f5b32c15cb5635d780566d855d086b82c352be154d5fb01a18f8ff7e"
+  url "https://registry.npmjs.org/pandemics/-/pandemics-0.12.0.tgz"
+  sha256 "8106ae09462a19768b4e74cb0079093b73d30ed9bc6ec22e0dd9d4434d23ea3f"
   license "BSD-3-Clause"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3f661c33867c3c714248b6e3fee9b5b075588cb6227c4730b12e584d1e3c3b44"
