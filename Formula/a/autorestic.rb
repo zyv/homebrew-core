63bb1a82ca933a502c8e265bdc4004d06c02c821
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-13 12:39:07 +0000

    autorestic: update 1.8.1 bottle.

--- Formula/a/autorestic.rb
+++ Formula/a/autorestic.rb
@@ -7,13 +7,13 @@ class Autorestic < Formula
   head "https://github.com/cupcakearmy/autorestic.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4d541e809577fd8a4184b9cbb630d3eeefaadf17ef62294a499f44af05f7e0a4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "631a5753766d572ed9a5bf53bf7858841131ce8727d4e9e46330c7c03c472153"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c7ecf5cabcf3ac802d3f105815eb3039b9c9a5a9a425924df0118cab342b8c96"
-    sha256 cellar: :any_skip_relocation, sonoma:         "2ea531dbc2a68676c83f482c82b30eceb0c4597e35f402668c1c109a27c70993"
-    sha256 cellar: :any_skip_relocation, ventura:        "d03c7487b8d946362b4dafc790ccbd3adccbfd2277391bc52d756e8cd88faa7e"
-    sha256 cellar: :any_skip_relocation, monterey:       "5c192815f29dab34aecad2d087eff9264649ca44065e655b5340751915991f65"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ad47371feb780bed56cbab307fde6719556ee10bf6297e277d45dd860a77203b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "21d84be529b93e0d91ab301d50f3b238f0cbcb337a91d55e778338b25532ec2b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b3c887b89d6e111069bc94d3b77a283e45a634654015c821ec7971f431eed999"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a5efe71592ba1fb6831e84df00e02e4b197a7deaf2318ae50af51e63b6adf039"
+    sha256 cellar: :any_skip_relocation, sonoma:         "1a9dc937b2e51a0fffd2f092223ef8e0983ab92fa6a3fe07f540deaa39bd8dfd"
+    sha256 cellar: :any_skip_relocation, ventura:        "f43e77974777a51ab5f503a55188dc7bafff09c99c4b3648353c1198bb87cc49"
+    sha256 cellar: :any_skip_relocation, monterey:       "fcbaf7a8f4cc55bce7f158c9e6a0df06b8e1d4129b48bfda2b45f683a37feee4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2616c3238463f80d37af128fb763f65bcb0a121850bb5020f2b6174dcede03c7"
   end
 
   depends_on "go" => :build
