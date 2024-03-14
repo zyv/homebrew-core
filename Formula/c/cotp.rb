80cedca0ba86547f77a298e752b0ef8e36e61f6e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-14 17:29:37 +0000

    cotp: update 1.5.0 bottle.

--- Formula/c/cotp.rb
+++ Formula/c/cotp.rb
@@ -7,13 +7,13 @@ class Cotp < Formula
   head "https://github.com/replydev/cotp.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "72479934ebd72ff1ce24c19f7cc9d764145fb2982fdd08eb2c74e36153d33a14"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "909015a4d2655fef31760773898191fdde3dfc8828166a4c1d9efbe830aef8fe"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0ccec62ee3cbd2b30bc45960ec29b237820833566d5a9822dd36df3c9263c25b"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a3a56d5f358f5a463de07e259c099dd538291a07a98f668290540ac2d1fabec5"
-    sha256 cellar: :any_skip_relocation, ventura:        "a9c5789f61a3c8d4f926f0795f17feb8b7122c3d0e26bef7aa409ba80ff24b6a"
-    sha256 cellar: :any_skip_relocation, monterey:       "d96620068676206f74a58b5f12422ac7dab68ee0e079ac0664955e9728f2ae1c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f963948b0dd3c66c40a82b3047a0e8292398c879b9593758fb4955f9b2b86d4b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "1807ac281480ec01eb10bcecfbcfcc400188897b88e887816dfe31009ef7af81"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cbd3500e014dc6fa197e31d68d3c00f33e18c3e6e8208ef9a3f791a4c4cdaaa4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8ea0c4db93925a5b422c8b36ca46e46d62d254c7cbdeee5bbfb4457ca370160a"
+    sha256 cellar: :any_skip_relocation, sonoma:         "cf06881a109d3c620dc841d2a2f25d5ade1f37659dce8149b4f79bcc4161507f"
+    sha256 cellar: :any_skip_relocation, ventura:        "50302e92aca1fb6bb3e8c2c5630a7dc965e62cffc860e394f638654c9bc6b45c"
+    sha256 cellar: :any_skip_relocation, monterey:       "eefd4de121c1770f26c26f43e4961da8a5b197f1398c60fe90f4dba2266477db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d54947061d528219c5f0dda7ced6e8beb5deabafc81e149c495c07d97f5504f3"
   end
 
   depends_on "rust" => :build
