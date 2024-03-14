d2161670679daf197de9595ebcf23ddd4de604aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-14 14:27:49 +0000

    testkube: update 1.17.0 bottle.

--- Formula/t/testkube.rb
+++ Formula/t/testkube.rb
@@ -12,13 +12,13 @@ class Testkube < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5cac2e31c2d23b104360717c1f9815f45451c45fb603ad4228ec257ef99516f7"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "218cf8a54135fa56891320a7ba080affff3ff5d8a5d2bd8ab5695ece85634169"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "eb241c8920187f6865b33e797032ac77337aca7f23842f1e88a8edf06c6b41ef"
-    sha256 cellar: :any_skip_relocation, sonoma:         "5c38db709ac246a72d16bea0054e767b22fdf6e40fe9d7fb91c0a334d239eda6"
-    sha256 cellar: :any_skip_relocation, ventura:        "ceaaddbdfe743b741ec120ca3e2ce4b5e01d2c4ccae9a1f130b8ade45b18ebfb"
-    sha256 cellar: :any_skip_relocation, monterey:       "8b67e7da858250b722074d0c3c49779717d6a7d64b6fe73c769029a671fb525f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6fc1169aa982236f52b32aa1674be80c99af9eff8a6a9470ba681701557ab523"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4835a66c104a53c7f5fcfdf783bda3d2c986f69495b523c1f7f16824301547f3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7ece746e0384b9f81c44ab96068338d6b02d6df7e26402fc216199f064a53861"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3f88998f27d8283e012c92d5d83f66408f5b9e7b07f714c1241fcd9c51d2f27a"
+    sha256 cellar: :any_skip_relocation, sonoma:         "5b7bb2233c772ad44269b0638c08f59a401543a917b5eb63debd51ba21066ccd"
+    sha256 cellar: :any_skip_relocation, ventura:        "ecc1660039d45f8067303f6be67ad87cc0e68a7a49bfa4f6f42b2e4a081c0692"
+    sha256 cellar: :any_skip_relocation, monterey:       "52eceaeda406714ec949b37767379cfb103dadc95833f876e299f7415f334754"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d0cf1aa212af38211b5cdfc106b1f76e5a3d19bc79b13c6a120c08dd30a85ae6"
   end
 
   depends_on "go" => :build
