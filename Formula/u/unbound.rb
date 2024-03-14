188dd96e4a386eb43414c0bdc95df3c18bc894e9
Dustin Rodrigues <dust.rod@gmail.com>
2024-03-14 11:29:31 -0400

    unbound 1.19.3

--- Formula/u/unbound.rb
+++ Formula/u/unbound.rb
@@ -1,8 +1,8 @@
 class Unbound < Formula
   desc "Validating, recursive, caching DNS resolver"
   homepage "https://www.unbound.net"
-  url "https://nlnetlabs.nl/downloads/unbound/unbound-1.19.2.tar.gz"
-  sha256 "cc560d345734226c1b39e71a769797e7fdde2265cbb77ebce542704bba489e55"
+  url "https://nlnetlabs.nl/downloads/unbound/unbound-1.19.3.tar.gz"
+  sha256 "3ae322be7dc2f831603e4b0391435533ad5861c2322e34a76006a9fb65eb56b9"
   license "BSD-3-Clause"
   head "https://github.com/NLnetLabs/unbound.git", branch: "master"
 
