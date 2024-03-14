6c61809db14abc2945ce499eef8ab57d67402872
l5io <47201130+l5io@users.noreply.github.com>
2024-03-14 11:50:47 -0500

    mesheryctl 0.7.32
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/m/mesheryctl.rb
+++ Formula/m/mesheryctl.rb
@@ -2,8 +2,8 @@ class Mesheryctl < Formula
   desc "Command-line utility for Meshery, the cloud native management plane"
   homepage "https://meshery.io"
   url "https://github.com/meshery/meshery.git",
-      tag:      "v0.7.31",
-      revision: "e5d5257ded21bfe07b2ee3f886ee4476651df6e2"
+      tag:      "v0.7.32",
+      revision: "3ae378acd44826ec24a5104f23e2f7bd090a6fef"
   license "Apache-2.0"
   head "https://github.com/meshery/meshery.git", branch: "master"
 
