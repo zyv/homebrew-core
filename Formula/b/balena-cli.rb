82317255c1052f3fd350651e9e86c806a7f3f605
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-15 14:40:21 +0000

    balena-cli: update 18.1.2 bottle.

--- Formula/b/balena-cli.rb
+++ Formula/b/balena-cli.rb
@@ -13,13 +13,13 @@ class BalenaCli < Formula
   end
 
   bottle do
-    sha256                               arm64_sonoma:   "9467087a9da09ed6bbede80b92de12f4ed83f8c7620005c682bc381e434769c2"
-    sha256                               arm64_ventura:  "06561766a645b7b5d42e541d377039ddc917d3a92ae9cf08cef04ffe1749824f"
-    sha256                               arm64_monterey: "5cfe2f2bb027b4763143e7cadab53f9294bf536ed9d5722beda0fe7faf3c3c04"
-    sha256                               sonoma:         "f0305d463361c5c3ab09b442b0964b85e1da9a495789255bd90924dcc73b06ef"
-    sha256                               ventura:        "cfa582e5d2e9d7221bd9ceb7c28614f15b8a3372330b54e6451ef43d28a49c92"
-    sha256                               monterey:       "04c9b8559a861dc64d8ce1ca6505c69c0b4c5e35c0676f433e2fdd38db93a0c8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "be9de0258599b4e6ce876de7e7ae41c55f50a4c6f461b7c0975825d2b5c0812b"
+    sha256                               arm64_sonoma:   "139f8023de855e5232bce736ed8442bc239c21bb29fcf19553de1fb298f95dfc"
+    sha256                               arm64_ventura:  "05956c57b834578c42a59451a6734fbebf674e93f78845d95b3d423ca62c33f7"
+    sha256                               arm64_monterey: "54d4acf02355b0cc599520a49fb399428628a08eb3ffa69ca70fb3e628e7d757"
+    sha256                               sonoma:         "805aad2084857c3123f840bdc02e34d09ffea16dc6930bb56b3fa9935e9c5767"
+    sha256                               ventura:        "7073d2c5de61ddf1ea639b4cff54fcb92697233ff5cedda4376400aff1106d26"
+    sha256                               monterey:       "602b3236af4b25ee9d942e6aa4792fb6e7cb13f0a66a68c5dc4d08a535570c19"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e249eec0efd27c060a6d3f8fa728ccd308a491aa45f134038a18aa790860cd94"
   end
 
   # need node@20, and also align with upstream, https://github.com/balena-io/balena-cli/blob/master/.github/actions/publish/action.yml#L21
