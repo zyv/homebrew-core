34580411a63899512e848e7bf3195bf514648979
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-16 22:18:33 +0000

    maturin: update 1.5.0 bottle.

--- Formula/m/maturin.rb
+++ Formula/m/maturin.rb
@@ -7,13 +7,14 @@ class Maturin < Formula
   head "https://github.com/PyO3/maturin.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ac6f489ca22aa4f6cb411b7ee0fc36d6fbaa6e6b940e0cce811a512d8d29557c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d84c0788904a7553bed9991e24beaa51565dce98b0e2883fbf3ab5c63a829e8d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ebbee243e035f679192422efffc84688b95f2b30793882debe999d6b990a945d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "03e2117430b88ad8219937bed7d08340c6745889dad57933ec9e8a6d0dabb8ba"
-    sha256 cellar: :any_skip_relocation, ventura:        "799978386674d99008a88d45cfa659747b01cd4efe38497d78b0bcf995dd0839"
-    sha256 cellar: :any_skip_relocation, monterey:       "e4a31781cced921fc214d6610778efcf2e31566c2a7569e246fa66370df44ade"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8c6f2f3baa0da92565d5af8102220ec9a711759407ab8435198f2c1b52acf371"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8fc79f12a929026da868cbb73643bf0aae2ab4a4a6af069b6fe67541c0417a35"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ef0781946e83686e56b45b5070b84b4ecd15c330ec605004115bb01cae7d2de9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bfb5abc92c699651e3c4f2c1486d68a49e38e71a94264ab7b2594d9a8b6b634e"
+    sha256 cellar: :any_skip_relocation, sonoma:         "048e3a2794746a71ec0e084fcf75f3ad9592dce289253158df806e2d73aa3d13"
+    sha256 cellar: :any_skip_relocation, ventura:        "3178dfec202bda2d86d599c01cae985e3a1003cb73081d0106636f48086d3e77"
+    sha256 cellar: :any_skip_relocation, monterey:       "fb1502a2ebf260ed013f57750f39f3c2428c28c306cdd9de2d59e1c3f15d33bc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "022e831c7874c173b2030d3f86c4cf727ca91a80e1ca5261dc65d02147b086c3"
   end
 
   depends_on "python@3.12" => :test
