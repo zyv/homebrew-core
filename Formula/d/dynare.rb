f4562579926ffa37599dc297e9a1dcd61a2b3a84
Timothy Sutton <tim@synthist.net>
2024-03-17 15:07:53 -0400

    dynare: revision bump (octave-9.1.0)

--- Formula/d/dynare.rb
+++ Formula/d/dynare.rb
@@ -4,6 +4,7 @@ class Dynare < Formula
   url "https://www.dynare.org/release/source/dynare-6.0.tar.xz"
   sha256 "52460046d44776d936986f52649f9e48966b07e414a864d83531d43e568ab682"
   license "GPL-3.0-or-later"
+  revision 1
   head "https://git.dynare.org/Dynare/dynare.git", branch: "master"
 
   livecheck do
