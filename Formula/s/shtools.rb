d525c878690b8b5788dc79f936c04832509020ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-18 22:30:05 +0000

    shtools: update 4.12.2 bottle.

--- Formula/s/shtools.rb
+++ Formula/s/shtools.rb
@@ -7,13 +7,13 @@ class Shtools < Formula
   head "https://github.com/SHTOOLS/SHTOOLS.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "7d139c07337891fe153bbf18700cb4d45f258177f9a3c8f4cbc4f54cadce446a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "68a1fd186978e7993f4656eac8b2df6f9076e8d4e42565ca67d754aaad9cc7d0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7bf7cbe3cd3ca8e4c7a25f668dc709c26a20b6166e7149aaa34607d6420b4a5f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "ff84c07de9706cbb77e8c7c058c383eb6382edf9bfc5a683ed6ab49eb4e0a566"
-    sha256 cellar: :any_skip_relocation, ventura:        "d8bc6890a51dfce4f141ea650f9635fd47982bbee0352a9e1742b74796ab5fda"
-    sha256 cellar: :any_skip_relocation, monterey:       "b4575c4027ef6086e68e3c14be1bf6577b8f494e7638af3a9e6a29b29fdb9053"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "46e390c6a35f4e23ec07a96421758cd9729ee2c0f14118fcc79006f291bf64e2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b1680f060ecacd1501b699b42aefcb5185a316925298bf8309e8064c5f11036e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "474630875450960b7da6dba70172664558609a5a848577a1ac38c5993b788058"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a7d973f00e4749ce16dd754f570489183ed7623ecb07f9aadbd94d57819e764e"
+    sha256 cellar: :any_skip_relocation, sonoma:         "f6ed69c843a88452a0b5a36845732bea2457f051309ad66643c7e49f803bbbea"
+    sha256 cellar: :any_skip_relocation, ventura:        "851ec5c5a13bd37d3e9d460ce368656be61119a6acf618a33ab23aa78ef5c304"
+    sha256 cellar: :any_skip_relocation, monterey:       "266187fe1e263156428432644610c4b45133127645ee18d41bd461d94ecc948e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ebf6c8a5dbd76a0d8a93985c625bea278d633e9591b365196889db3430fbbd82"
   end
 
   depends_on "fftw"
