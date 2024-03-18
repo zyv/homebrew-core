58732357900764e479dae69dc63d5722a295400b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-18 22:04:18 +0000

    dynaconf: update 3.2.5 bottle.

--- Formula/d/dynaconf.rb
+++ Formula/d/dynaconf.rb
@@ -8,14 +8,13 @@ class Dynaconf < Formula
   license "MIT"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ea284ee8d0e1cd983f62d60ffbbaa64e0bd99584a695ecd4d38b5f0c1605baed"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7e93e5cd441b0512d12c131d036f4b9a063880595e6f1f2a1a18865263e82690"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e309fd6e7922e34feb53d0760362d5eab6b4887f626c05e8dcc0285b1de18a95"
-    sha256 cellar: :any_skip_relocation, sonoma:         "b21eefc850000cfd4af2fc50d37cc6bd1b23515995e13838a98499da2f536a06"
-    sha256 cellar: :any_skip_relocation, ventura:        "b96734f90e5ae1546b56bec927ea2c25cb2fc09db7f61972b26663c3d3537ee4"
-    sha256 cellar: :any_skip_relocation, monterey:       "5d59e2f06e5f26bd59b0ceced8d39f9f36a0bf59835cd644ee769e60805fc2e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aad67495af56194247e98dfbd86c45032be4adf19785db9ab76ed37595a4208d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3de5f9073989529dfebb6da92aeecdba30882c9ced08dd9e92bf12708afa20af"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3de5f9073989529dfebb6da92aeecdba30882c9ced08dd9e92bf12708afa20af"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3de5f9073989529dfebb6da92aeecdba30882c9ced08dd9e92bf12708afa20af"
+    sha256 cellar: :any_skip_relocation, sonoma:         "3de5f9073989529dfebb6da92aeecdba30882c9ced08dd9e92bf12708afa20af"
+    sha256 cellar: :any_skip_relocation, ventura:        "3de5f9073989529dfebb6da92aeecdba30882c9ced08dd9e92bf12708afa20af"
+    sha256 cellar: :any_skip_relocation, monterey:       "3de5f9073989529dfebb6da92aeecdba30882c9ced08dd9e92bf12708afa20af"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6bae742fc1d2b99d307784f348459c20c33b6535f28b737f3df1171a1e6f41a1"
   end
 
   depends_on "python@3.12"
