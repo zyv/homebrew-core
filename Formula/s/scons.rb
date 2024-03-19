8da542355328fde25d97e7a8f4d464c587dff1e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-19 22:39:33 +0000

    scons: update 4.7.0 bottle.

--- Formula/s/scons.rb
+++ Formula/s/scons.rb
@@ -8,14 +8,13 @@ class Scons < Formula
   license "MIT"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4c6b94fba45b0c21d695110fa318c6a9b53b9fe71144f1bae9ee8c38b4c44ae8"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "82b89dd373acc68b7fb2f2a8e23a183dfcceba5582760b7783e0c31440d96a4f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b3dc25bcdcc86857b1b069ccb3194e8b0bceafa974c8711ee456abdca9c7208a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "91acae80219d74656874541767162f83b492ca76790a7f8f246c7fcb5a051039"
-    sha256 cellar: :any_skip_relocation, ventura:        "a367a980297c34131ff5dcb51460e667d0e9f6b5eee65acdc12683d5f82e6bb5"
-    sha256 cellar: :any_skip_relocation, monterey:       "67257e1cfae102ffc07e641d70cf415e89a29cc64e0e832c51ea29af8f7e3b70"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5c548747373fac9ebd731df2c791d7ecaa89ac60188c3aa09d55e0c8273596c4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "95120f2bfed80d992a7f8f8b24e3b30461f5174a6b9a7c24a5722c6dc34eb760"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "95120f2bfed80d992a7f8f8b24e3b30461f5174a6b9a7c24a5722c6dc34eb760"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "95120f2bfed80d992a7f8f8b24e3b30461f5174a6b9a7c24a5722c6dc34eb760"
+    sha256 cellar: :any_skip_relocation, sonoma:         "a896b0c7638102ad279bc7f80b0e533e5fa00bf458f7913dc534d7450b454222"
+    sha256 cellar: :any_skip_relocation, ventura:        "a896b0c7638102ad279bc7f80b0e533e5fa00bf458f7913dc534d7450b454222"
+    sha256 cellar: :any_skip_relocation, monterey:       "a896b0c7638102ad279bc7f80b0e533e5fa00bf458f7913dc534d7450b454222"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "653548364415711953a4bb8283cd09bf087b2d46ed9ed2aa298eeaf3b62d3ae9"
   end
 
   depends_on "python@3.12"
