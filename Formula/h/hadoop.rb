309bea6dae26bf73f16caa726d2fb7b99c1d9328
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-19 17:22:56 +0000

    hadoop: update 3.4.0 bottle.

--- Formula/h/hadoop.rb
+++ Formula/h/hadoop.rb
@@ -12,15 +12,13 @@ class Hadoop < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "babb72b9ea422ae665945db155016e9fda102ec8fd37af97261faf72805ac7ac"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cddc35ec7bb9fa8f6304588d52b62886ea9cb6ede2bd25368565457423258e2f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cddc35ec7bb9fa8f6304588d52b62886ea9cb6ede2bd25368565457423258e2f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cddc35ec7bb9fa8f6304588d52b62886ea9cb6ede2bd25368565457423258e2f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "104b0ece894cfc0b4fac859cbc370117748cbedd21890846bb7ca87a95ee72ec"
-    sha256 cellar: :any_skip_relocation, ventura:        "6191a8773d779d5307e2f3fb7bb0c0d6f797638a8f87f07e849a0a61be3a726d"
-    sha256 cellar: :any_skip_relocation, monterey:       "6191a8773d779d5307e2f3fb7bb0c0d6f797638a8f87f07e849a0a61be3a726d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6191a8773d779d5307e2f3fb7bb0c0d6f797638a8f87f07e849a0a61be3a726d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cddc35ec7bb9fa8f6304588d52b62886ea9cb6ede2bd25368565457423258e2f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3bc4d1af76f0d40746abffa84b42508f1ef53eda3d612b03e5c1f4144bb4826a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3bc4d1af76f0d40746abffa84b42508f1ef53eda3d612b03e5c1f4144bb4826a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3bc4d1af76f0d40746abffa84b42508f1ef53eda3d612b03e5c1f4144bb4826a"
+    sha256 cellar: :any_skip_relocation, sonoma:         "090c88b44a952701c87d753f376e9465571bfdd4fcb4de38c245e217964574da"
+    sha256 cellar: :any_skip_relocation, ventura:        "090c88b44a952701c87d753f376e9465571bfdd4fcb4de38c245e217964574da"
+    sha256 cellar: :any_skip_relocation, monterey:       "090c88b44a952701c87d753f376e9465571bfdd4fcb4de38c245e217964574da"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3bc4d1af76f0d40746abffa84b42508f1ef53eda3d612b03e5c1f4144bb4826a"
   end
 
   # WARNING: Check https://cwiki.apache.org/confluence/display/HADOOP/Hadoop+Java+Versions before updating JDK version
