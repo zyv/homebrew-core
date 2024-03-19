15cd64a272380f53ed8d1f6c4cd0cf1dce3679c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-19 00:54:39 +0000

    pushpin: update 1.39.1 bottle.

--- Formula/p/pushpin.rb
+++ Formula/p/pushpin.rb
@@ -7,10 +7,10 @@ class Pushpin < Formula
   head "https://github.com/fastly/pushpin.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 sonoma:       "ace5ffa91826756133c944eb86bd0cc93f60d5143fd054966ad6a960c2ad02b8"
-    sha256 cellar: :any,                 ventura:      "e03f4034571cf0ecb9f3ca5273581b6092bdbc4b8db6c59df6bd06682c1b2c17"
-    sha256 cellar: :any,                 monterey:     "fe0a1365ca2bf4893497e94a821eae6f757576415f11cb739ffbad4a865624e8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "02a03ff705aa26e807f3de0f58e7c221eb8243091a0d0c680626bb750f8dead4"
+    sha256 cellar: :any,                 sonoma:       "fdad7253f708ceb44826dfc9b906f56d46de3ac57fc1e04ef7b3c80392e64366"
+    sha256 cellar: :any,                 ventura:      "e46403df994c44c67870959789bc73166e287290cb7cc377078ec7f8fec3c6fc"
+    sha256 cellar: :any,                 monterey:     "bc5bd12f2a558f41a7335fc9dec4747d06e198840786cab9621526f279e319ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6ba7ebf19e770baa018030f672e84e068417f907d7f977d109c3f39bdb1430fe"
   end
 
   depends_on "boost" => :build
