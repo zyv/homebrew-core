1eaa9137189b8b651b80443dd7f7379b5ee32ba0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-20 15:43:46 +0000

    jhipster 8.2.0
    jhipster: remove incompatible pre-built binaries
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/j/jhipster.rb
+++ Formula/j/jhipster.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Jhipster < Formula
   desc "Generate, develop and deploy Spring Boot + Angular/React applications"
   homepage "https://www.jhipster.tech/"
-  url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-8.1.0.tgz"
-  sha256 "9e9b7b348845200a12dfdd0fd4d976314a1fe675beb1cff6cbc82984db9e0b71"
+  url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-8.2.0.tgz"
+  sha256 "302a8810d08bfbb0891e899a5bc7e18f42abd55bb4c18199671e472fdd756f10"
   license "Apache-2.0"
 
   bottle do
@@ -24,6 +24,12 @@ class Jhipster < Formula
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install Dir["#{libexec}/bin/*"]
     bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
+
+    # Remove incompatible pre-built binaries
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    node_modules = libexec/"lib/node_modules/generator-jhipster/node_modules"
+    (node_modules/"nice-napi/prebuilds").each_child { |dir| dir.rmtree if dir.basename.to_s != "#{os}-#{arch}" }
   end
 
   test do
