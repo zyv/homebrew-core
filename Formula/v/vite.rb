b9cb9d97cea92af15fd7f60dbf388c2ec0745187
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-20 13:34:53 +0000

    vite: update 5.2.1 bottle.

--- Formula/v/vite.rb
+++ Formula/v/vite.rb
@@ -8,13 +8,13 @@ class Vite < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "795046207a17a022cd7dc4d1f33c9743f2268f2b72d1193ae5e1fabc2ffd74ee"
-    sha256 cellar: :any,                 arm64_ventura:  "795046207a17a022cd7dc4d1f33c9743f2268f2b72d1193ae5e1fabc2ffd74ee"
-    sha256 cellar: :any,                 arm64_monterey: "795046207a17a022cd7dc4d1f33c9743f2268f2b72d1193ae5e1fabc2ffd74ee"
-    sha256 cellar: :any,                 sonoma:         "9398962e22c03a4c292e335fd5e85a5fb414525c98d7b0ff8a129e4151ccf7cd"
-    sha256 cellar: :any,                 ventura:        "9398962e22c03a4c292e335fd5e85a5fb414525c98d7b0ff8a129e4151ccf7cd"
-    sha256 cellar: :any,                 monterey:       "9398962e22c03a4c292e335fd5e85a5fb414525c98d7b0ff8a129e4151ccf7cd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "770c2f68d5f68dac0ec7bd3445de5603f18983ca4046ecd07486c0cb2d8bc875"
+    sha256 cellar: :any,                 arm64_sonoma:   "53c53a1ee34d193aa58a352985f531b67ad33b259b4c3600dc009c45c790abdf"
+    sha256 cellar: :any,                 arm64_ventura:  "53c53a1ee34d193aa58a352985f531b67ad33b259b4c3600dc009c45c790abdf"
+    sha256 cellar: :any,                 arm64_monterey: "53c53a1ee34d193aa58a352985f531b67ad33b259b4c3600dc009c45c790abdf"
+    sha256 cellar: :any,                 sonoma:         "84daab9c55cec755d7f26bfaa7e996d9691202b244ad7070aeb15fd6a2372d6c"
+    sha256 cellar: :any,                 ventura:        "84daab9c55cec755d7f26bfaa7e996d9691202b244ad7070aeb15fd6a2372d6c"
+    sha256 cellar: :any,                 monterey:       "84daab9c55cec755d7f26bfaa7e996d9691202b244ad7070aeb15fd6a2372d6c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "68f38b461264fc1efa82ba1c623b513f1028d9881a0996b06efbdd40ce11a138"
   end
 
   depends_on "node"
