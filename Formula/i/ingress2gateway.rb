27f18ce74193537189dd7d8af9e055607066dc7d
Rui Chen <rui@chenrui.dev>
2024-03-21 17:18:26 +0000

    ingress2gateway: add 0.2.0 bottle.

--- Formula/i/ingress2gateway.rb
+++ Formula/i/ingress2gateway.rb
@@ -6,6 +6,16 @@ class Ingress2gateway < Formula
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/ingress2gateway.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5cd6eedc90347967f37c2be2d5a472f2d59cb1ca4544fb845c82b8024aed5c48"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c37c69b9716141fe19a22b3e659533be4d6c09d161c2255b0c7eaa1e68563323"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a7d6297b75fa0c3aebe810ab8723179d24dd37da3bb65a982dbe3849d49a1c6a"
+    sha256 cellar: :any_skip_relocation, sonoma:         "b642d9684846de5bdac2011b773a11841438c8a59861b85631e0897f1f29ea92"
+    sha256 cellar: :any_skip_relocation, ventura:        "4619cd67314f0584cd5abcd42fbd0625ef5ed747a923eebed0c5320f13a174f6"
+    sha256 cellar: :any_skip_relocation, monterey:       "078352ac874427d894344820ac68d059798e018b386cb2014d3fa6766de29e00"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b11b0d725a6fad9bec4a62bf97ab9ce2121563648adac0fa16d7d4818f9c0719"
+  end
+
   depends_on "go" => :build
 
   def install
