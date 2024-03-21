bf4e5428547b04f2d41d18d96f6be6400ba650b6
Mike McQuaid <mike@mikemcquaid.com>
2024-03-21 12:17:52 +0000

    rtabmap: add 0.21.4 bottle.

--- Formula/r/rtabmap.rb
+++ Formula/r/rtabmap.rb
@@ -6,6 +6,16 @@ class Rtabmap < Formula
   license "BSD-3-Clause"
   head "https://github.com/introlab/rtabmap.git", branch: "master"
 
+  bottle do
+    sha256                               arm64_sonoma:   "2c38fca1ac84b47eba43de32a781d2fd181a263f4925ef15fa280c84efe9a3fa"
+    sha256                               arm64_ventura:  "396c70a3efbdd53ae2605906f2eaedcfb9335ca9082ada6e5691d2aa4c5bf66f"
+    sha256                               arm64_monterey: "86d6a2567d2e28c55f48232a186fa167758bff7772b2294b760ec362a2b81f87"
+    sha256                               sonoma:         "be8665e9d7ae0b1effcfef0c4f82ae86e665ba25dad6016fd37aab5c71800bc7"
+    sha256                               ventura:        "d262fc25ce3f5c1c251b38976317eb969b83caa73a8fb36ab9dc6d7ef6aa1c72"
+    sha256                               monterey:       "3352cfbbda1039bceecaf60d83cafff009f58c6afb3fec80600194cf9c3f62d7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ab72221ba2f0dff302f8999f4d5f32c67c4ed79b62f9c3b7fe3fd6ad7ac7d4c6"
+  end
+
   depends_on "cmake" => [:build, :test]
   depends_on "g2o"
   depends_on "librealsense"
