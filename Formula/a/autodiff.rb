3e2fd96e1cefb4b4169a5a8a4269571edbecf32e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-21 14:20:30 +0000

    autodiff: update 1.1.0 bottle.

--- Formula/a/autodiff.rb
+++ Formula/a/autodiff.rb
@@ -7,14 +7,13 @@ class Autodiff < Formula
   head "https://github.com/autodiff/autodiff.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e18a4195b2b9cac0708f49cc54c039778dcf113370746338092ee999a8739519"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b8c09e5877fd17bebf49215bf059bc33b73866bfa3e49a405d9ce9b973810ca0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f5c2a195565a9dd9da5feb6e55636405ce5089d98204921d29d0350e95050f4f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "bd918b0a4be4a6988aabf5a2eaeaa4c80b9ef455c4be804a01566893bfa12b2a"
-    sha256 cellar: :any_skip_relocation, ventura:        "4e6e331c5f1b2f3fae3aa2ab1404b1b34ca79364d55a8039758645e432572f18"
-    sha256 cellar: :any_skip_relocation, monterey:       "2e13fca1888ccd7946eb0c0f558321069f1b4ea5ea12520bd73d0fe0d047cd28"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "84d5ac57af23463a1cdcefcc44907cf884232d9e8a39c350986cfab1643240bf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "17cf0578327f5896ac1fc151ea1be8055c4bc2fbd0c854ecfee2945ab953979f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8db7eeeb4a5767e42dc5979c1f2c7d08ef176f5b9a8ff750b26ed6fc54f37ce8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8f87e5a030c6a8b988893cf3b245111682b3fdb6655bf02391195350cde4e1a1"
+    sha256 cellar: :any_skip_relocation, sonoma:         "15b5791e62db828b1f54c040d0c0b97b0323adf19e7e645de3fe18c4571bee8c"
+    sha256 cellar: :any_skip_relocation, ventura:        "28ef141acc024bafa3b5c24e7b070a1fb2bd37c9d7f21f8f76e01d7f6dd5f4d5"
+    sha256 cellar: :any_skip_relocation, monterey:       "6a5e0ceed8842ff79d708fae0a925ecf77d3c9458536d6634aab2515c346c6df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bd32f73ba21fa3d0be7977f0e130d4ac2577ea3147e6cc07b5ac99a491dabc12"
   end
 
   depends_on "cmake" => :build
