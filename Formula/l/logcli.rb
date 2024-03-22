75dcea38fb952cf5d8278c656ce517f800457aed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-22 21:27:37 +0000

    logcli: update 2.9.6 bottle.

--- Formula/l/logcli.rb
+++ Formula/l/logcli.rb
@@ -11,13 +11,13 @@ class Logcli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b6b263def59de49b458227c11e4f94131c2d0128b08ac895af4e15938c6df936"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9c1a341eccb6d6cc56fae216c0eedbc8b2fe66711e70f2bed0612618d5d84fea"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cf96f98ba3ef58ea5dae2796b48c65904b0d7095951cd63e622cb9ffb36e6c0a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "720fb924a07d22418a71878652bed1a10cc538ea6ceae1a6feedc7a1db19b93c"
-    sha256 cellar: :any_skip_relocation, ventura:        "512df8660ababcadebeda89f2f8c96bdcce5df7e11dfd7c4c88b4e4c4b525dd9"
-    sha256 cellar: :any_skip_relocation, monterey:       "9f0f38a740ee46cfa7921a5923bcb5346a523426b8bdc05beee2a40284fae061"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fcbed2454dcb028e44729832ea96efee84e3e47f3cc904fec26b21ee5d450d35"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "781d4568608789dcc72e86d71c91edaa29beea45b2293871e0d94183a2c1fadf"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3fb286735c40bc738a685f7fe5cac9bea1fa1d6cf218104c867bb4c9a8dc0bb5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6ed4a552faf94d8208a3aa9ff17d690680ac7f91a7332094770bd503bcfdd772"
+    sha256 cellar: :any_skip_relocation, sonoma:         "5678dcc0352d1543b2426c5c16fefa054d9127990f4d8b5d62bf8c4fe4736a47"
+    sha256 cellar: :any_skip_relocation, ventura:        "4095cedcc0e0e73ea8d8a4065f499fba3d96d5887249c16708efde09402c58dc"
+    sha256 cellar: :any_skip_relocation, monterey:       "718e61e7394c37b76ecbda5e9b01b31356b1ace20a8bce30373f07e0413e4e85"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "28e4407e0bd180a71e8dd3664bd5e868b81c777c075df848257368c37518b677"
   end
 
   depends_on "go" => :build
