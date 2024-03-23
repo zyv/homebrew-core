42b279831e1ccf3ac2ba33fdd5482435251770d1
Michael Cho <michael@michaelcho.dev>
2024-03-23 13:01:14 -0400

    osm-pbf: revision bump (protobuf 26.0)

--- Formula/o/osm-pbf.rb
+++ Formula/o/osm-pbf.rb
@@ -4,6 +4,7 @@ class OsmPbf < Formula
   url "https://github.com/openstreetmap/OSM-binary/archive/refs/tags/v1.5.1.tar.gz"
   sha256 "183ad76c5905c7abd35d938824320ffb82d9ca8987796018f2da8380b51cdac2"
   license "LGPL-3.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_sonoma:   "656693464a84370602be8ebfc7a027510712026fbfd3da69fb27ccbb8c1358ae"
