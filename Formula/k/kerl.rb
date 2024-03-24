d6a22dcbc07f062461298531279aa65bf1a3a546
Porkepix <porkepix@gmail.com>
2024-03-24 11:22:48 +0100

    kerl 4.1.0

--- Formula/k/kerl.rb
+++ Formula/k/kerl.rb
@@ -1,8 +1,8 @@
 class Kerl < Formula
   desc "Easy building and installing of Erlang/OTP instances"
   homepage "https://github.com/kerl/kerl"
-  url "https://github.com/kerl/kerl/archive/refs/tags/4.0.0.tar.gz"
-  sha256 "5731a6d2292aab9e90c83d12a50c064079467126dc335eba6b8654c0306568d1"
+  url "https://github.com/kerl/kerl/archive/refs/tags/4.1.0.tar.gz"
+  sha256 "5b754ba985f04125dc6824dbdab966f734615db6ec57cb544b4eff5d7f4c059d"
   license "MIT"
   head "https://github.com/kerl/kerl.git", branch: "master"
 
