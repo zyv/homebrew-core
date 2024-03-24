71cd84a3d05dfcc46add70e546429f6916497a6b
Rui Chen <rui@chenrui.dev>
2024-03-24 13:27:45 +0000

    c-blosc2: add 2.13.2 bottle.

--- Formula/c/c-blosc2.rb
+++ Formula/c/c-blosc2.rb
@@ -6,6 +6,16 @@ class CBlosc2 < Formula
   license "BSD-3-Clause"
   head "https://github.com/Blosc/c-blosc2.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sonoma:   "8db72646fc1755ee4fa84d1b5bda0d81258b6808159073b9b4dbe15ebbbd97f4"
+    sha256 cellar: :any,                 arm64_ventura:  "81416ed7c32349649139b7de7daae2db24aa3351a2581a031af9645d61e181e9"
+    sha256 cellar: :any,                 arm64_monterey: "6fb12f94c66c031d68eded33b35965abff3ef0c88efeac67374dd0e17ff959a3"
+    sha256 cellar: :any,                 sonoma:         "c6c2fec12055d25412ade617ec5e904cd01ceb76865a765ba6eee64336d184c6"
+    sha256 cellar: :any,                 ventura:        "45f494cf8b93f2c896ac135f6397ce168e5113b705292c4098d218d50e0bb1ba"
+    sha256 cellar: :any,                 monterey:       "d4ad769882e8fdc0b02d1a0cb7bb7fb62e4802726b0cd97996a285444b7f7855"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a4e83e69f6f5ce989d0553a1615ca0afdf258330f0d456b0a8b4fb5cad798dc3"
+  end
+
   depends_on "cmake" => :build
   depends_on "lz4"
   depends_on "zstd"
