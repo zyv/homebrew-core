cfd87810b2b44a51aa89cc78359a63fed534a4a3
Rui Chen <rui@chenrui.dev>
2024-03-25 19:40:37 -0400

    roapi 0.11.1
    autobump: add roapi
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r/roapi.rb
+++ Formula/r/roapi.rb
@@ -1,8 +1,8 @@
 class Roapi < Formula
   desc "Full-fledged APIs for static datasets without writing a single line of code"
   homepage "https://roapi.github.io/docs"
-  url "https://github.com/roapi/roapi/archive/refs/tags/roapi-v0.10.0.tar.gz"
-  sha256 "85d194b78dfef8aaab5da0c609d8c6d126ee0d474e6b1455b6ca3c7f1b8afd06"
+  url "https://github.com/roapi/roapi/archive/refs/tags/roapi-v0.11.1.tar.gz"
+  sha256 "0bc41ff939596b10a85cf1c81364d13d8c26ee32b5718c87bc39d9907d5be037"
   license "Apache-2.0"
 
   bottle do
