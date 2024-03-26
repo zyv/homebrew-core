28d7f407a138d6f1ce8d36459f7ce11cbe385b8d
Rui Chen <rui@chenrui.dev>
2024-03-26 11:15:35 +0000

    jnv: add 0.2.0 bottle.

--- Formula/j/jnv.rb
+++ Formula/j/jnv.rb
@@ -6,6 +6,16 @@ class Jnv < Formula
   license "MIT"
   head "https://github.com/ynqa/jnv.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5c2af8626ff805bb08739b066dfa6f4411935b2b45a6c97f797609cc828fc663"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fc8410f97add619d0a875be0d6c724a2115071c7c94f575b600c772e40ba345e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c6e89328a6a81cf58ac8425fc1eff44738de7425eb41a7a5ac08efe20f7d5875"
+    sha256 cellar: :any_skip_relocation, sonoma:         "8b208430040fa188f1e6548a04ceda5f43e2e9ea0368e861173ae054c810ee58"
+    sha256 cellar: :any_skip_relocation, ventura:        "8edc2451a86b287d5c43fe2546ff3c94f8eb2ccb740e27e58ade4b90d28ce98e"
+    sha256 cellar: :any_skip_relocation, monterey:       "72fa2e797790d3b4c18c0b41f6814ea212bc87288f242eac8980f7a167a55307"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bc47f97b0e34a2ce29df201979a5ee505bde2cce6256724db61368633b4e9ba0"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
