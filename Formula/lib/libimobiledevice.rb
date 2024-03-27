5cc3c0ef4f6773b4fd2e5846047523f99ab3fdca
Rui Chen <rui@chenrui.dev>
2024-03-27 13:45:13 -0400

    libimobiledevice: revision bump (libusbmuxd 2.1.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libimobiledevice.rb
+++ Formula/lib/libimobiledevice.rb
@@ -4,7 +4,7 @@ class Libimobiledevice < Formula
   url "https://github.com/libimobiledevice/libimobiledevice/releases/download/1.3.0/libimobiledevice-1.3.0.tar.bz2"
   sha256 "53f2640c6365cd9f302a6248f531822dc94a6cced3f17128d4479a77bd75b0f6"
   license "LGPL-2.1-or-later"
-  revision 2
+  revision 3
 
   bottle do
     rebuild 1
