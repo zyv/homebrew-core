00667e7249af58cebfdba8d37715f921c1fe4952
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-27 15:44:05 +0000

    pint 0.57.3

--- Formula/p/pint.rb
+++ Formula/p/pint.rb
@@ -1,8 +1,8 @@
 class Pint < Formula
   desc "Prometheus rule linter/validator"
   homepage "https://cloudflare.github.io/pint/"
-  url "https://github.com/cloudflare/pint/archive/refs/tags/v0.57.2.tar.gz"
-  sha256 "5faf1a0b8a13f1ed4754aa9fa45249de228bf2cc2cc6ab833076a34c42796bc3"
+  url "https://github.com/cloudflare/pint/archive/refs/tags/v0.57.3.tar.gz"
+  sha256 "6d245ce3583f9ca5c149099296061a0284816bc11572fa4befcc70b4f0fac791"
   license "Apache-2.0"
 
   bottle do
