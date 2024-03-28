48b50fa0e03b5f52bd6264ab1804d1938e673bd4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-28 15:34:42 +0000

    liquibase: update 4.27.0 bottle.

--- Formula/l/liquibase.rb
+++ Formula/l/liquibase.rb
@@ -11,13 +11,13 @@ class Liquibase < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "edb971b0d93ae1c3a7887d7e3b9aa00421a8b9b5e31f90b9e3dde197dafd287a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "edb971b0d93ae1c3a7887d7e3b9aa00421a8b9b5e31f90b9e3dde197dafd287a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "edb971b0d93ae1c3a7887d7e3b9aa00421a8b9b5e31f90b9e3dde197dafd287a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "1d4c18d97e7dfc7a22642c7c089ad92bda22c7c7da9c25b2b7800157f80c558a"
-    sha256 cellar: :any_skip_relocation, ventura:        "1d4c18d97e7dfc7a22642c7c089ad92bda22c7c7da9c25b2b7800157f80c558a"
-    sha256 cellar: :any_skip_relocation, monterey:       "1d4c18d97e7dfc7a22642c7c089ad92bda22c7c7da9c25b2b7800157f80c558a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "edb971b0d93ae1c3a7887d7e3b9aa00421a8b9b5e31f90b9e3dde197dafd287a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c2c3a070f0c481323ec824ad1e8adfbf55a26ad1da5b4e8e44b872718ba2bbfb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c2c3a070f0c481323ec824ad1e8adfbf55a26ad1da5b4e8e44b872718ba2bbfb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c2c3a070f0c481323ec824ad1e8adfbf55a26ad1da5b4e8e44b872718ba2bbfb"
+    sha256 cellar: :any_skip_relocation, sonoma:         "c73888c4128174716d1cc686cce880f0ce09cd383b1baee31455b247e6231d29"
+    sha256 cellar: :any_skip_relocation, ventura:        "c73888c4128174716d1cc686cce880f0ce09cd383b1baee31455b247e6231d29"
+    sha256 cellar: :any_skip_relocation, monterey:       "c73888c4128174716d1cc686cce880f0ce09cd383b1baee31455b247e6231d29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c2c3a070f0c481323ec824ad1e8adfbf55a26ad1da5b4e8e44b872718ba2bbfb"
   end
 
   depends_on "openjdk"
