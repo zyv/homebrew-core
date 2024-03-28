ec17606d1aadd5204773122e698492d2e71dba8e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-28 21:41:38 +0000

    ollama 0.1.30

--- Formula/o/ollama.rb
+++ Formula/o/ollama.rb
@@ -2,8 +2,8 @@ class Ollama < Formula
   desc "Create, run, and share large language models (LLMs)"
   homepage "https://ollama.com/"
   url "https://github.com/ollama/ollama.git",
-      tag:      "v0.1.29",
-      revision: "e87c780ff97673d9f906980d68b4557032bc4618"
+      tag:      "v0.1.30",
+      revision: "756c2575535641f1b96d94b4214941b90f4c30c7"
   license "MIT"
   head "https://github.com/ollama/ollama.git", branch: "main"
 
