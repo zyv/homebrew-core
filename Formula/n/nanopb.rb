5729a3783be1e183fcdd8d5ab9468481b55ee8c7
Michael Cho <michael@michaelcho.dev>
2024-03-28 08:14:30 -0400

    nanopb: vendor python resources
    Signed-off-by: Michael Cho <michael@michaelcho.dev>

--- Formula/n/nanopb.rb
+++ Formula/n/nanopb.rb
@@ -1,11 +1,13 @@
 class Nanopb < Formula
   include Language::Python::Shebang
+  include Language::Python::Virtualenv
 
   desc "C library for encoding and decoding Protocol Buffer messages"
   homepage "https://jpa.kapsi.fi/nanopb/docs/index.html"
   url "https://jpa.kapsi.fi/nanopb/download/nanopb-0.4.8.tar.gz"
   sha256 "d685e05fc6e56fd7e4e3cacc71f45bd91d90c0455257603ed98a39d2b0f1dd4b"
   license "Zlib"
+  revision 1
 
   livecheck do
     url "https://jpa.kapsi.fi/nanopb/download/"
@@ -24,20 +26,30 @@ class Nanopb < Formula
 
   depends_on "cmake" => :build
   depends_on "protobuf"
-  depends_on "python-setuptools" # import pkg_resources
   depends_on "python@3.12"
+
+  resource "protobuf" do
+    url "https://files.pythonhosted.org/packages/d2/e5/7e22ca7201a6b1040aae7787d0fe6cd970311da376a86fdafa5182be1d1b/protobuf-5.26.1.tar.gz"
+    sha256 "8ca2a1d97c290ec7b16e4e5dff2e5ae150cc1582f55b5ab300d45cb0dfa90e51"
+  end
+
+  resource "setuptools" do
+    url "https://files.pythonhosted.org/packages/4d/5b/dc575711b6b8f2f866131a40d053e30e962e633b332acf7cd2c24843d83d/setuptools-69.2.0.tar.gz"
+    sha256 "0ff4183f8f42cd8fa3acea16c45205521a4ef28f73c6391d8a25e92893134f2e"
+  end
 
   def install
     ENV.append_to_cflags "-DPB_ENABLE_MALLOC=1"
-    site_packages = Language::Python.site_packages("python3.12")
+    venv = virtualenv_create(libexec, "python3.12")
+    venv.pip_install resources
 
     system "cmake", "-S", ".", "-B", "build",
                     "-DBUILD_SHARED_LIBS=ON",
-                    "-Dnanopb_PYTHON_INSTDIR_OVERRIDE=#{prefix/site_packages}",
+                    "-Dnanopb_PYTHON_INSTDIR_OVERRIDE=#{venv.site_packages}",
                     *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-    rewrite_shebang detected_python_shebang, *bin.children
+    rewrite_shebang python_shebang_rewrite_info(venv.root/"bin/python"), *bin.children
   end
 
   test do
