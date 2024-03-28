e4be6bc5c6ceb46c10d320de9309f13f2684f889
Rui Chen <rui@chenrui.dev>
2024-03-28 14:25:17 -0400

    coreutils 9.5
    autobump: add coreutils
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/coreutils.rb
+++ Formula/c/coreutils.rb
@@ -1,9 +1,9 @@
 class Coreutils < Formula
   desc "GNU File, Shell, and Text utilities"
   homepage "https://www.gnu.org/software/coreutils"
-  url "https://ftp.gnu.org/gnu/coreutils/coreutils-9.4.tar.xz"
-  mirror "https://ftpmirror.gnu.org/coreutils/coreutils-9.4.tar.xz"
-  sha256 "ea613a4cf44612326e917201bbbcdfbd301de21ffc3b59b6e5c07e040b275e52"
+  url "https://ftp.gnu.org/gnu/coreutils/coreutils-9.5.tar.xz"
+  mirror "https://ftpmirror.gnu.org/coreutils/coreutils-9.5.tar.xz"
+  sha256 "cd328edeac92f6a665de9f323c93b712af1858bc2e0d88f3f7100469470a1b8a"
   license "GPL-3.0-or-later"
 
   bottle do
