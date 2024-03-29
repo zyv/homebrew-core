792ceb8b7605e34bf02c4faad5c04f9a7bf26541
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-29 14:31:50 +0000

    moarvm: update 2024.03 bottle.

--- Formula/m/moarvm.rb
+++ Formula/m/moarvm.rb
@@ -11,13 +11,13 @@ class Moarvm < Formula
   end
 
   bottle do
-    sha256 arm64_sonoma:   "6bb36eb66ea6a36dbd184c99ab98f048307ef92c35201bee6731bbe82bb6256f"
-    sha256 arm64_ventura:  "034b2ac16b88ec73db132d06dfa190b2cfbe539b10fce2ff986781f0754b3e86"
-    sha256 arm64_monterey: "66cd1004d350f839a1f7ca4c23692778676837a6aa9fa0994399bedbc73ed5de"
-    sha256 sonoma:         "2a9ea35810b80540c09d4f89505f71ed8966d1b52353ea47b83cce4fb48b73fd"
-    sha256 ventura:        "2a38d7c1be98e5c0b1c5cb2b94b409db894ce446de7acb4dc00ce66dfa70e660"
-    sha256 monterey:       "c7593ac596c73f9de993f8ad451de638edbe14718205d3e1d192b3848f914a39"
-    sha256 x86_64_linux:   "40fa644a86901f6b4e31775f3150ad89b931b98d4a114f42ca11905671f53a22"
+    sha256 arm64_sonoma:   "e1ffbec8e95c4ec48f4b45d5ba1cd39ccb1ce9ab2f78b8798b4dbc4538b4955f"
+    sha256 arm64_ventura:  "0fab08d25bdb859c10897ae8206e132b79904a386715e9200ebfa92e95293628"
+    sha256 arm64_monterey: "0652964e290512d8f26661e6f5931227ae4b1c292e283ad44350b17aeb40e0e2"
+    sha256 sonoma:         "ed38b852f13c40e0ed31e8782f57b8d6f9fd5760656df58d3c8ca12c2bb76d7c"
+    sha256 ventura:        "cbb74bc61d9cd398c797039dba613f889e7088a0bf283b2c708efe9945c3b0cf"
+    sha256 monterey:       "abdf8b5bcaaec7ffb6d94fbec1dd049ef893d958cbb0b6ac0ca93acf0689afea"
+    sha256 x86_64_linux:   "0dab05f7b7170f4510c9a99a58e3eb8ab73258267ef5f178254e6e4f45fbcbb5"
   end
 
   depends_on "pkg-config" => :build
