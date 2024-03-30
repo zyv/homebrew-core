d4a23c758b326b2351ce257b5d955be4acc01f61
Rui Chen <rui@chenrui.dev>
2024-03-30 15:36:20 -0400

    makensis 3.10
    autobump: add makensis
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/makensis.rb
+++ Formula/m/makensis.rb
@@ -1,8 +1,8 @@
 class Makensis < Formula
   desc "System to create Windows installers"
   homepage "https://nsis.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/nsis/NSIS%203/3.09/nsis-3.09-src.tar.bz2"
-  sha256 "0cd846c6e9c59068020a87bfca556d4c630f2c5d554c1098024425242ddc56e2"
+  url "https://downloads.sourceforge.net/project/nsis/NSIS%203/3.10/nsis-3.10-src.tar.bz2"
+  sha256 "11b54a6307ab46fef505b2700aaf6f62847c25aa6eebaf2ae0aab2f17f0cb297"
   license "Zlib"
 
   bottle do
