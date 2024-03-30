2ddd3a863bbc21be66d8b8cfb9e1de30ca81affc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-30 12:39:15 +0000

    imap-backup 14.6.1

--- Formula/i/imap-backup.rb
+++ Formula/i/imap-backup.rb
@@ -1,8 +1,8 @@
 class ImapBackup < Formula
   desc "Backup GMail (or other IMAP) accounts to disk"
   homepage "https://github.com/joeyates/imap-backup"
-  url "https://github.com/joeyates/imap-backup/archive/refs/tags/v14.6.0.tar.gz"
-  sha256 "69878f5f9533b8167c4d9c3588f791709f81ed2e626f6ef35b3bf29d6f24fcb5"
+  url "https://github.com/joeyates/imap-backup/archive/refs/tags/v14.6.1.tar.gz"
+  sha256 "6e9b7f2ef081d7792864b04edd724386a9605a7fb9aa18194895af55e1be0970"
   license "MIT"
 
   livecheck do
