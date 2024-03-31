ff9c5e8dcabca9733936b278fc5e46610839a07e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-31 02:13:09 +0000

    cython: update 3.0.10 bottle.

--- Formula/c/cython.rb
+++ Formula/c/cython.rb
@@ -6,13 +6,13 @@ class Cython < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "11be1cb7c3864e7ff277eae101795f495c55890a1c086ce143ec56f5271d41a8"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e951f308796e0ae7790cdc9aca3203f3647750a5809e658a89897178b688d0f6"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ff439fccb4f55f829764f7bf281888ce28f6e7c3c142d88293ac7b3981ce0911"
-    sha256 cellar: :any_skip_relocation, sonoma:         "e78797ce1d585d36f0b884118f77d8fb2c15d11daefd758e1b1e13410b97c89e"
-    sha256 cellar: :any_skip_relocation, ventura:        "3520ebf21e90dcbc74b80c504df141889aaa9ffc3e324345694b5caa66c58632"
-    sha256 cellar: :any_skip_relocation, monterey:       "5ac8e66f0a982d6430565878bc3be0dbb9a96957a325a737aa64a2e297c0bfc4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c189e9b63b6722448779b1ba667f44519a9d93dfa1594c3bee5634f15dd26e4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "ca2588052ba86d0e310f26b8acd8116e7346306483446b7b61e83014fae0bcf3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "57fbc8fef50d02de7e8807119aebd4ba3d645f1527f24c5ad755132fc0267c97"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2f5a6277b1e740eb1efc9e48d19abb23bf79695abe58288e82436b825d63c013"
+    sha256 cellar: :any_skip_relocation, sonoma:         "313a30f3440adc4f013a8d313a6e1c237457cd3ac05a4a75070b32f7a9ea0907"
+    sha256 cellar: :any_skip_relocation, ventura:        "24dde71147d7b8e7e61559a67e7b177580770b812974e33e9f5148879c5b5614"
+    sha256 cellar: :any_skip_relocation, monterey:       "3745bbc021c2c2292c37e5bd0bdb4a4ee473751cea89e60c0420f21a0563cbf1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f54316fa17664dce2527fe4ba8f6f8427e9288fece9c8c0d9ae7e43e5884f4b5"
   end
 
   keg_only <<~EOS
