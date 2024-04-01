d9244429e2094587d4e72b96e29f38c0f08deb60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-01 19:36:55 +0000

    typos-cli: update 1.20.0 bottle.

--- Formula/t/typos-cli.rb
+++ Formula/t/typos-cli.rb
@@ -11,13 +11,13 @@ class TyposCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "f1e4bd24d51c015f1b28d7d72486904e3e280693d04a0edc5eddbca9431ab6fb"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dbd353f776201f04360f1a838eeb8cd2ead378e4324258da299b5aac84c53671"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8a5689253c90a22353f29402b97e9a22a2c44d09ec04728f42487e594e1c630d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "1a01ecd099aa487435221da1abdfc59f00d0a33435325f772296623ab54422ad"
-    sha256 cellar: :any_skip_relocation, ventura:        "d829f091610e6ee254658993897c7b031be77f8ad0fbeb0ee707b37f1ecb04dd"
-    sha256 cellar: :any_skip_relocation, monterey:       "fbe54af469fc187e0ed63c153cceb747454abe0ab77cbda541fa04c7f97c7494"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0494ae8149da9b9b681df4efe57a65065d2fc67fa4c0706b36933dd8a0ed3703"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "50341967af7ff87ac4b2c0b884033c720df0dfbf95e0d663474daa096fdc3074"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8c278ade32379e5bfbcb27419287a460d40888ed18ada1be97d83ac035a7fac0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c13b2caad7e64217d0b7cb21411bd9e8bfb231a09e2418092ae64026c1b6420c"
+    sha256 cellar: :any_skip_relocation, sonoma:         "4056620ab800aa9dbc1b39fe53ed0bbe864b931b41064daa3660498d436568f6"
+    sha256 cellar: :any_skip_relocation, ventura:        "db6af19bf5b219b9ac1322c1eeaff51720c24c7e4d868ffa96fd6e9b7008da88"
+    sha256 cellar: :any_skip_relocation, monterey:       "b047e5d675427b6f2d6188d5b496012278b5af9356a0c65107aa90efbd405a5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a41040210e0ffb5935fd8639f67f113059c7d781aa3753ab488f72989b3d3db8"
   end
 
   depends_on "rust" => :build
