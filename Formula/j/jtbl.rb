4e2afcf629b33726f62c71d3917b9c086b4615b6
Rui Chen <rui@chenrui.dev>
2024-04-01 22:11:56 +0000

    jtbl: add 1.6.0 bottle.

--- Formula/j/jtbl.rb
+++ Formula/j/jtbl.rb
@@ -7,6 +7,16 @@ class Jtbl < Formula
   sha256 "7de0cb08ebb2b3a0658229a8edd4204c6944cbd9e3e04724a9ea235a61c115a5"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "21a81be0445de58f0e890b2b6b45fe9acefc6e4aaf2843874cbcfc143f6e82e6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "21a81be0445de58f0e890b2b6b45fe9acefc6e4aaf2843874cbcfc143f6e82e6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "21a81be0445de58f0e890b2b6b45fe9acefc6e4aaf2843874cbcfc143f6e82e6"
+    sha256 cellar: :any_skip_relocation, sonoma:         "e0848f48a0387422d6ca1c9fc68e9b5a741f7d0072bf69e2fa96c4dc040bcbb8"
+    sha256 cellar: :any_skip_relocation, ventura:        "e0848f48a0387422d6ca1c9fc68e9b5a741f7d0072bf69e2fa96c4dc040bcbb8"
+    sha256 cellar: :any_skip_relocation, monterey:       "e0848f48a0387422d6ca1c9fc68e9b5a741f7d0072bf69e2fa96c4dc040bcbb8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5812058f8e11da8ef073f6aafbcff218424383be387f5ba14ab542198e57014b"
+  end
+
   depends_on "python@3.12"
 
   resource "tabulate" do
