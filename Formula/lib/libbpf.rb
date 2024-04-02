7207ff3d7a1d2a8193b2c21b8afe9236bab1be22
Rui Chen <rui@chenrui.dev>
2024-04-02 09:44:18 -0400

    libbpf 1.4.0
    autobump: add libbpf
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libbpf.rb
+++ Formula/lib/libbpf.rb
@@ -1,8 +1,8 @@
 class Libbpf < Formula
   desc "Berkeley Packet Filter library"
   homepage "https://github.com/libbpf/libbpf"
-  url "https://github.com/libbpf/libbpf/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "11db86acd627e468bc48b7258c1130aba41a12c4d364f78e184fd2f5a913d861"
+  url "https://github.com/libbpf/libbpf/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "4271dfd51f59d23c03d9ae62d8a92622af0f5216a620a7a666c4975c3c7cda6e"
   license "BSD-2-Clause"
 
   bottle do
