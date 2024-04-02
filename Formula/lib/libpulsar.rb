d8fcbf67526a1984c79c97e2bcab262bc0e43a72
Rui Chen <rui@chenrui.dev>
2024-04-02 14:10:19 +0000

    libpulsar: update 3.5.1 bottle.

--- Formula/lib/libpulsar.rb
+++ Formula/lib/libpulsar.rb
@@ -7,13 +7,13 @@ class Libpulsar < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "d430f202dc9c85789de0678d8ca1dabed3c65b4c258eca82c052e1b7b023404d"
-    sha256 cellar: :any,                 arm64_ventura:  "03c0139e9d075f34ab6a4e5001324c97d1702fec27011a3ab745ac33a4c4f095"
-    sha256 cellar: :any,                 arm64_monterey: "5b5616eba6449d1d403a33ba73fd471284d2116cd356fba80279ce2c7a46360a"
-    sha256 cellar: :any,                 sonoma:         "72f75853ce8b2114acefe6d8aa7469f89a4f12a35227ddaee5840e1da21083b6"
-    sha256 cellar: :any,                 ventura:        "511125e0dcf5a6592827647219da1f0aa8d12f3902b34c064706f95953f76b6e"
-    sha256 cellar: :any,                 monterey:       "8b62e6cd52c84b2d2f6a9d97fa80c8036f3605a1c45b3c0d2beb5d0f2a40d049"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d7981e99cb2f11265ffe299577de9499d5a7167c06f9a5c5a73936931e74a66d"
+    sha256 cellar: :any,                 arm64_sonoma:   "a08b3483b74be83bc5ccecb061825df782d4f034e39c9fb4cb7c57a6d9a6a1c9"
+    sha256 cellar: :any,                 arm64_ventura:  "21515bff2543c94ae83043cbc4aa8ad0a81adf27cd5a48a0dfb89e42c0e4f94b"
+    sha256 cellar: :any,                 arm64_monterey: "f683513f15b32eddcabc1f7189f996d2e1b529eba88b9d852f765c323754ae6a"
+    sha256 cellar: :any,                 sonoma:         "9de9907d629344d85aea0db2dc90eed47bab34fe7562a4e6e60d8b760db88b20"
+    sha256 cellar: :any,                 ventura:        "2dfbdae8e6b3130179a2306025006d37bab8899906544abbb0da1522cc1a65d1"
+    sha256 cellar: :any,                 monterey:       "dd274f1f6042234cbbe75b40c2f6dded59d38ffdc8e8737a38c26bf93cf1f07d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3447063797b661017795f2a6bc02af991e687614234a0ad864870b5808f83ddc"
   end
 
   depends_on "cmake" => :build
