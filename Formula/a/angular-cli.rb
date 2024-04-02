64c6697b2007d4daf3f00088732a41d0fa9d95df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-02 21:43:40 +0000

    angular-cli: update 17.3.3 bottle.

--- Formula/a/angular-cli.rb
+++ Formula/a/angular-cli.rb
@@ -8,13 +8,13 @@ class AngularCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "50292a637ade873372bf639b0417dd6abcf0577636c8bf6fddea0ebe4ca27c37"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "50292a637ade873372bf639b0417dd6abcf0577636c8bf6fddea0ebe4ca27c37"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "50292a637ade873372bf639b0417dd6abcf0577636c8bf6fddea0ebe4ca27c37"
-    sha256 cellar: :any_skip_relocation, sonoma:         "2e37b05977b42636be85d3db5f2ba68fe5c02b3efe2ee981f901b0035b6d68b9"
-    sha256 cellar: :any_skip_relocation, ventura:        "2e37b05977b42636be85d3db5f2ba68fe5c02b3efe2ee981f901b0035b6d68b9"
-    sha256 cellar: :any_skip_relocation, monterey:       "2e37b05977b42636be85d3db5f2ba68fe5c02b3efe2ee981f901b0035b6d68b9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50292a637ade873372bf639b0417dd6abcf0577636c8bf6fddea0ebe4ca27c37"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4f574d5ad107ea9bf6ae25362a31ec0a1b5968141df70c2b14a3284d46624944"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4f574d5ad107ea9bf6ae25362a31ec0a1b5968141df70c2b14a3284d46624944"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4f574d5ad107ea9bf6ae25362a31ec0a1b5968141df70c2b14a3284d46624944"
+    sha256 cellar: :any_skip_relocation, sonoma:         "c3c721655a4c4e6b96e132a1fb0669a1fcdf3ad56e762b81849afa5e747ba6f5"
+    sha256 cellar: :any_skip_relocation, ventura:        "c3c721655a4c4e6b96e132a1fb0669a1fcdf3ad56e762b81849afa5e747ba6f5"
+    sha256 cellar: :any_skip_relocation, monterey:       "c3c721655a4c4e6b96e132a1fb0669a1fcdf3ad56e762b81849afa5e747ba6f5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4f574d5ad107ea9bf6ae25362a31ec0a1b5968141df70c2b14a3284d46624944"
   end
 
   depends_on "node"
