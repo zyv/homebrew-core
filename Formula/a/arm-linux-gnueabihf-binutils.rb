430aa97ec34f3b1536f8bec8d9a80c033926e688
Patrick Linnane <patrick@linnane.io>
2024-04-02 16:29:22 +0000

    arm-linux-gnueabihf-binutils: update 2.42 bottle.

--- Formula/a/arm-linux-gnueabihf-binutils.rb
+++ Formula/a/arm-linux-gnueabihf-binutils.rb
@@ -11,13 +11,11 @@ class ArmLinuxGnueabihfBinutils < Formula
   end
 
   bottle do
-    sha256 arm64_sonoma:   "ae60dae148c0c903a5ebc57282c6df9e9aa76161bb9311a73a3f943bb0f4806b"
-    sha256 arm64_ventura:  "e9f62b0f3c7aa06bf858df99dd38eabeae6bfba9234b360dfb58f7c3118abb7c"
-    sha256 arm64_monterey: "e5a90266cf3b3e347a8b5242e6b4bde64b443bbe168f7c0798e7d920d7fcc522"
-    sha256 sonoma:         "9afcd8cb32f113be993b15702f753e676bfb21f9c48aed4bea4362e686a83dfa"
-    sha256 ventura:        "995f49001c59c1a31a69921a4a727d5ca91de826b76f2c11987f5fe4d979abdd"
-    sha256 monterey:       "55080e306990e4fac4832b7ad39e6de6b56703ddddb44cc298c9f1d813c2be2d"
-    sha256 x86_64_linux:   "2e2a57cfe8267debac6a4b28022fa6329f64232b5b47797dd1e49e0ff539a9b6"
+    sha256 arm64_sonoma:  "74a5a3cb193d05c8f2917106655e1e1090499c26fab6bdc45d2133a741201e75"
+    sha256 arm64_ventura: "80e4ed787df40fdcacd6554b88847ad04323fa1579fc59b8600282adffbeb98b"
+    sha256 sonoma:        "dad357ce82254a1be31dfc2b4e9655e444c78505d346004d13b73881c1c267f3"
+    sha256 ventura:       "d5df81db688b1cfdaa4ad4d14ffacaf17483aa0ede6894928371f4ccf09ab53e"
+    sha256 x86_64_linux:  "31d10ab761e6f69d2ed8bd21fc94264c2bb515eb3ab99cb6fff4046c1e37c243"
   end
 
   depends_on "pkg-config" => :build
