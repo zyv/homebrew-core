7a5304b2c10debad18e7155b81ec2c1112091605
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-03 15:27:47 +0000

    django-completion 5.0.4

--- Formula/d/django-completion.rb
+++ Formula/d/django-completion.rb
@@ -1,8 +1,8 @@
 class DjangoCompletion < Formula
   desc "Bash completion for Django"
   homepage "https://www.djangoproject.com/"
-  url "https://github.com/django/django/archive/refs/tags/5.0.3.tar.gz"
-  sha256 "b166eef2fe3f24d2ba9e7f2416f3d6db661fe9328b3f3da9c465d8e6cf8bf464"
+  url "https://github.com/django/django/archive/refs/tags/5.0.4.tar.gz"
+  sha256 "0484848137b22138bac47ad13c25f4064b6e1fad33db6094ca508c2399390e67"
   license "BSD-3-Clause"
   head "https://github.com/django/django.git", branch: "main"
 
