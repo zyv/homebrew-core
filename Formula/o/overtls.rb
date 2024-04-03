15a0c53dd2aeb984bdca8c5fcd18da1d463c34c7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-03 15:42:09 +0000

    overtls 0.2.20
    overtls: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/overtls.rb
+++ Formula/o/overtls.rb
@@ -1,8 +1,8 @@
 class Overtls < Formula
   desc "Simple proxy tunnel for bypassing the GFW"
   homepage "https://github.com/ShadowsocksR-Live/overtls"
-  url "https://github.com/ShadowsocksR-Live/overtls/archive/refs/tags/v0.2.19.tar.gz"
-  sha256 "d7975311dcccc38fd19e53311f94d465fa95d4b0fe19e9fa190e3f335faec170"
+  url "https://github.com/ShadowsocksR-Live/overtls/archive/refs/tags/v0.2.20.tar.gz"
+  sha256 "1be367e43ea8e58cd92c800488e70eaffb2d647aeb9ac8beed5c6f395a5c1a82"
   license "MIT"
   head "https://github.com/ShadowsocksR-Live/overtls.git", branch: "master"
 
@@ -28,6 +28,6 @@ class Overtls < Formula
     assert_match version.to_s, shell_output("#{bin}/overtls -V")
 
     output = shell_output(bin/"overtls -r client -c #{pkgshare}/config.json 2>&1", 1)
-    assert_match "kind: TimedOut, message: \"connection timed out\"", output
+    assert_match "Error: Io(Kind(TimedOut))", output
   end
 end
