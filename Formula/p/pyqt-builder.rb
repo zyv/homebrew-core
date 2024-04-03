9d2f5c4b455d5300b9c1aa98e6c8b4580b5790a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-03 22:49:32 +0000

    pyqt-builder: update 1.16.0 bottle.

--- Formula/p/pyqt-builder.rb
+++ Formula/p/pyqt-builder.rb
@@ -7,13 +7,7 @@ class PyqtBuilder < Formula
   head "https://www.riverbankcomputing.com/hg/PyQt-builder", using: :hg
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "fb9cb2df20660795abfd6ea2e89f23bf798d3f18431f50dfc7c1572def2f3511"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "744a018fc9c00e74ffbb29ddcd3d037f15523c0c4c49414605570045cf5e18f8"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bf53c0b52cfff8b06c2f678f3434fccc0f2e84014cf3778b22b4222c94d5849a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "ca71b526c655474b71220c684372d27ef64db07bf0d8728379b4ed4566996cf0"
-    sha256 cellar: :any_skip_relocation, ventura:        "339f953ca98cb45b6857d62e3c5a019841851c6a7fa1532c5e5035a99284916f"
-    sha256 cellar: :any_skip_relocation, monterey:       "5331dc9bbb738ff55b5c1d3e339170e6f82d458bfe3d5abf48f323a24a26c99e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7aeb1c3c1861d60289011c9b1dc52cce5fd8cc6c68bd7b977fcbc2c80787be87"
+    sha256 cellar: :any_skip_relocation, all: "3b07ba04d94369bb4bb119a9a15274f97f05ee4c0a2fb45b76becd8aa83891f9"
   end
 
   depends_on "python@3.12"
