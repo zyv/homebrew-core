29c3633cbce7e7d7df97a5c7705725892af057c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-04 15:22:04 +0000

    atlas 0.21.1

--- Formula/a/atlas.rb
+++ Formula/a/atlas.rb
@@ -3,8 +3,8 @@ class Atlas < Formula
   homepage "https://atlasgo.io/"
   # Upstream may not mark patch releases as latest on GitHub; it is fine to ship them.
   # See https://github.com/ariga/atlas/issues/1090#issuecomment-1225258408
-  url "https://github.com/ariga/atlas/archive/refs/tags/v0.21.0.tar.gz"
-  sha256 "34c7429f8cd1e7fa6a26f15996c92d56cae779231f8d837c670cca0daac9f330"
+  url "https://github.com/ariga/atlas/archive/refs/tags/v0.21.1.tar.gz"
+  sha256 "eb35bf79613d1bba10df482cf688a0c5a139e8834f4ab65747cd2b63cbe610ae"
   license "Apache-2.0"
   head "https://github.com/ariga/atlas.git", branch: "master"
 
