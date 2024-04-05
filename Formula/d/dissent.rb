73ceaff57c0e1e96b83bde4189e533c5e7ac350f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-05 03:29:32 +0000

    dissent 0.0.23

--- Formula/d/dissent.rb
+++ Formula/d/dissent.rb
@@ -1,8 +1,8 @@
 class Dissent < Formula
   desc "GTK4 Discord client in Go"
   homepage "https://github.com/diamondburned/dissent"
-  url "https://github.com/diamondburned/dissent/archive/refs/tags/v0.0.22.tar.gz"
-  sha256 "756f7bf983e98e035655a66388a3409f02939c972d6bd024a5177a389cbde5c4"
+  url "https://github.com/diamondburned/dissent/archive/refs/tags/v0.0.23.tar.gz"
+  sha256 "7c5e10a6111cd8912c052337c56cb04cbb92b237453d560c85409f1c8f8cc01b"
   license "GPL-3.0-or-later"
   head "https://github.com/diamondburned/dissent.git", branch: "main"
 
