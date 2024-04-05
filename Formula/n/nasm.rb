ff2d469916e6507584336e0be6fa9f890d204fa0
Rui Chen <rui@chenrui.dev>
2024-04-05 15:08:42 -0400

    nasm 2.16.02
    autobump: add nasm
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/n/nasm.rb
+++ Formula/n/nasm.rb
@@ -1,8 +1,8 @@
 class Nasm < Formula
   desc "Netwide Assembler (NASM) is an 80x86 assembler"
   homepage "https://www.nasm.us/"
-  url "https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.xz"
-  sha256 "c77745f4802375efeee2ec5c0ad6b7f037ea9c87c92b149a9637ff099f162558"
+  url "https://www.nasm.us/pub/nasm/releasebuilds/2.16.02/nasm-2.16.02.tar.xz"
+  sha256 "1e1b942ea88f22edae89659e15be26fa027eae0747f51413540f52d4eac4790d"
   license "BSD-2-Clause"
 
   livecheck do
