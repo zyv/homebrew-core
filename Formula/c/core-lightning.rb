e909bc0415cc3222bc621614c04d576e2f9b6fda
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-06 09:27:19 +0000

    core-lightning 24.02.2

--- Formula/c/core-lightning.rb
+++ Formula/c/core-lightning.rb
@@ -1,8 +1,8 @@
 class CoreLightning < Formula
   desc "Lightning Network implementation focusing on spec compliance and performance"
   homepage "https://github.com/ElementsProject/lightning"
-  url "https://github.com/ElementsProject/lightning/releases/download/v24.02.1/clightning-v24.02.1.zip"
-  sha256 "733e2b41411a5882d93319883f8575e6959fe33a30e4f0de589ce7e4511a512b"
+  url "https://github.com/ElementsProject/lightning/releases/download/v24.02.2/clightning-v24.02.2.zip"
+  sha256 "2904bfe15994c4990da6f3dcc4be54a7d7f6e657a23f3be2d8088abb3450983a"
   license "MIT"
 
   livecheck do
