e9b6890ed0afd7a25b51e1854dd624e2baea1d4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-08 21:35:57 +0000

    heartbeat 8.13.2

--- Formula/h/heartbeat.rb
+++ Formula/h/heartbeat.rb
@@ -2,8 +2,8 @@ class Heartbeat < Formula
   desc "Lightweight Shipper for Uptime Monitoring"
   homepage "https://www.elastic.co/beats/heartbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.13.1",
-      revision: "e9e462d71bdcd33a84d7f51753a116b5d418938f"
+      tag:      "v8.13.2",
+      revision: "d41b4978ea7b4d7c6020b47ffd8a3b8642531fe3"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "master"
 
