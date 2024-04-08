0ae1ae2f5b71c32671006f05e9638fda7558032f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-08 06:38:57 +0000

    proto 0.34.0

--- Formula/p/proto.rb
+++ Formula/p/proto.rb
@@ -1,8 +1,8 @@
 class Proto < Formula
   desc "Pluggable multi-language version manager"
   homepage "https://moonrepo.dev/proto"
-  url "https://github.com/moonrepo/proto/archive/refs/tags/v0.32.2.tar.gz"
-  sha256 "81582f84a25c3aaf09718b5570e449b39c1e894d447281f6014cb084efe9d28c"
+  url "https://github.com/moonrepo/proto/archive/refs/tags/v0.34.0.tar.gz"
+  sha256 "ac2359deb384a8f62d3730339dad4e5f2443ab4090769536357dbe831907bb83"
   license "MIT"
   head "https://github.com/moonrepo/proto.git", branch: "master"
 
