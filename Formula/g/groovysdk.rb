add0c04f573406f3e4e898546298273d42100960
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-09 06:35:15 +0000

    groovysdk 4.0.21

--- Formula/g/groovysdk.rb
+++ Formula/g/groovysdk.rb
@@ -2,8 +2,8 @@ class Groovysdk < Formula
   desc "SDK for Groovy: a Java-based scripting language"
   # TODO: remove `groovy-raw-#{version}-raw.jar` workaround when bump
   homepage "https://www.groovy-lang.org/"
-  url "https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/apache-groovy-sdk-4.0.20.zip"
-  sha256 "a1680827e7fea8f2d6bf7be495b5b4238f3b6c6d1784e9e1c3bda5cba9927184"
+  url "https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/apache-groovy-sdk-4.0.21.zip"
+  sha256 "983dd01aae4380a3d9fb28a691755418a39b4763d71cbe8447c9d253cb5f1134"
   license "Apache-2.0"
 
   livecheck do
