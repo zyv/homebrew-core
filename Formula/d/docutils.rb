4163c3f1e89e0010674fb4768d0d0ac68a491613
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-04-09 23:35:38 +0000

    docutils: update 0.21 bottle.

--- Formula/d/docutils.rb
+++ Formula/d/docutils.rb
@@ -8,14 +8,13 @@ class Docutils < Formula
   license all_of: [:public_domain, "BSD-2-Clause", "GPL-3.0-or-later", "Python-2.0"]
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "fa9cbcc947ce1576e508a8dfe24ba0ef19141ee8e068f8ae2055179aa303b24d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cba04ed0253c9a19fd605e2f8366797f612d3744d7899467307eac1394cf11ed"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "767da48c2d42fca594ee7684e0defa880d1449e0e3e97d53890442d6641e5984"
-    sha256 cellar: :any_skip_relocation, sonoma:         "5cf8ababce162145e3bc7ac9332787d4f16dc19666970555937585e03c5fe8b7"
-    sha256 cellar: :any_skip_relocation, ventura:        "88b0625f0e9f7f87254ee88e97de0248f6c21faac2de980cd00b33ab262f38d3"
-    sha256 cellar: :any_skip_relocation, monterey:       "99112fc17faff857416bb41fe2c5d14c7e1b3d0dd86d9f86bffb7ab34aefd01f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "480c0b72303e68588d67dd37efc0b55d3971f67561c765f3947d1af96103d790"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "09c0234bff094ff3cdf3f0080f0fa536e3856f333437c2f3833d912358f3ec33"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "09c0234bff094ff3cdf3f0080f0fa536e3856f333437c2f3833d912358f3ec33"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "09c0234bff094ff3cdf3f0080f0fa536e3856f333437c2f3833d912358f3ec33"
+    sha256 cellar: :any_skip_relocation, sonoma:         "09c0234bff094ff3cdf3f0080f0fa536e3856f333437c2f3833d912358f3ec33"
+    sha256 cellar: :any_skip_relocation, ventura:        "09c0234bff094ff3cdf3f0080f0fa536e3856f333437c2f3833d912358f3ec33"
+    sha256 cellar: :any_skip_relocation, monterey:       "09c0234bff094ff3cdf3f0080f0fa536e3856f333437c2f3833d912358f3ec33"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "644296dd3ea5be965ad4e2d904517181fd6d513aab4063ae3e58046b38aa9fd7"
   end
 
   depends_on "python@3.12"
