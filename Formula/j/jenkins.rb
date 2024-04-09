908477db4e61802be7323011eeb50b14aeddf2b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-09 16:13:58 +0000

    jenkins: update 2.453 bottle.

--- Formula/j/jenkins.rb
+++ Formula/j/jenkins.rb
@@ -11,7 +11,7 @@ class Jenkins < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e87582c269c9324adaba445976dff1f4a323e7a656ae78e4206775bcd2cbdfdb"
+    sha256 cellar: :any_skip_relocation, all: "7e69149a137f373a0c9d06f357beabcf4c301abd92bc0947e36bef0acde3110e"
   end
 
   head do
