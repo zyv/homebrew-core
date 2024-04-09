9dfd0eeac9f6e6e7b112ea3bdad99730bb78eaa7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-09 08:07:38 +0000

    jhipster: update 8.3.0 bottle.

--- Formula/j/jhipster.rb
+++ Formula/j/jhipster.rb
@@ -8,13 +8,13 @@ class Jhipster < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256                               arm64_sonoma:   "0e17bad03f85df6836fc6df3511358255d53e5dfe0bf078e51b0b7894585b8a4"
-    sha256                               arm64_ventura:  "a4422cb587db92fd53854d205285c1b16031a7977e20f1dc68ef8543a6eeb838"
-    sha256                               arm64_monterey: "85d8aba14f7da7f8e463c3e9abc5549a04e50275df0f04a8dd4cb62431519cf6"
-    sha256                               sonoma:         "e672aaa5fd475aaee9475c841ae271cdabbafda452075cd51c1cf20d34efcc8b"
-    sha256                               ventura:        "d17984363c0dc7aad3f0f3bd506a3c6ded40540409524a8fe7b5f12acaed7b87"
-    sha256                               monterey:       "d2fe7b05f711a499e8b7c983ecddbf8ea8a446338e7548ebc6b1c8aa300a095c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6fd2d70e939bf83f778b210210c56e6e259f20104d2f6e098d8986c1b91dd202"
+    sha256                               arm64_sonoma:   "9fb434833b28fd8f6ccee122b01f3e2dde6505c39fe2814da8918156d96ee145"
+    sha256                               arm64_ventura:  "69b4657bd15ab89f707c682aa5b0349a17ab3e5a6ae28fc0788440ea471535cc"
+    sha256                               arm64_monterey: "e585c6ab8ad5b71de9610ca257c1d920ee26564d7227c2d83203bbbaf6b6c957"
+    sha256                               sonoma:         "024039e03a8111589e8ca245edcbcc184485326867fdb2324a5790d0206ebd32"
+    sha256                               ventura:        "0207611b8a8f5190a4d06c01121741d89f697ba77fd0f31439a6af66033456ba"
+    sha256                               monterey:       "b39ee692cd47f4181371ebe56de1764e47e844fad1ff5519365f0ad89ee1c6ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5e7fe58e0b9325d21812460001579feb827654d55429d3532b25f599e5fa0028"
   end
 
   depends_on "node"
