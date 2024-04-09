4104385195aeff299751f4ee72f594546fce6a54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-09 00:36:18 +0000

    i2p 2.5.0

--- Formula/i/i2p.rb
+++ Formula/i/i2p.rb
@@ -1,8 +1,8 @@
 class I2p < Formula
   desc "Anonymous overlay network - a network within a network"
   homepage "https://geti2p.net"
-  url "https://files.i2p-projekt.de/2.4.0/i2psource_2.4.0.tar.bz2"
-  sha256 "30ef8afcad0fffafd94d30ac307f86b5a6b318e2c1f44a023005841a1fcd077c"
+  url "https://files.i2p-projekt.de/2.5.0/i2psource_2.5.0.tar.bz2"
+  sha256 "6bda9aff7daa468cbf6ddf141c670140de4d1db145329645a90c22c1e5c7bc01"
   license :cannot_represent
 
   livecheck do
