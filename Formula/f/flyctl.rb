83de8ad4c2676d46eab2c2848055c3fbea2a7ae7
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-04-09 08:01:16 +0000

    flyctl: update 0.2.30 bottle.

--- Formula/f/flyctl.rb
+++ Formula/f/flyctl.rb
@@ -18,13 +18,13 @@ class Flyctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "eb1a6c8c85694ac5bca85a1977cbbf888723203954d89c01e40f4b8ae8ca6854"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "eb1a6c8c85694ac5bca85a1977cbbf888723203954d89c01e40f4b8ae8ca6854"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "eb1a6c8c85694ac5bca85a1977cbbf888723203954d89c01e40f4b8ae8ca6854"
-    sha256 cellar: :any_skip_relocation, sonoma:         "341839163e6da0bfc24e9196720b327b8e82838f8e20e9ac0bba717cf07d5376"
-    sha256 cellar: :any_skip_relocation, ventura:        "341839163e6da0bfc24e9196720b327b8e82838f8e20e9ac0bba717cf07d5376"
-    sha256 cellar: :any_skip_relocation, monterey:       "341839163e6da0bfc24e9196720b327b8e82838f8e20e9ac0bba717cf07d5376"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b1fe235334ae625200c3a1dfcf7524920737948c30c0af0dfe625e33e3e4e46"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4d6677b10af77fce36cc7b4de831acfeb9e616d64a69992a6a3b61050c2ccd38"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4d6677b10af77fce36cc7b4de831acfeb9e616d64a69992a6a3b61050c2ccd38"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4d6677b10af77fce36cc7b4de831acfeb9e616d64a69992a6a3b61050c2ccd38"
+    sha256 cellar: :any_skip_relocation, sonoma:         "eaef9939d9a58333cadad5c2be3f696faccc6a2bb6bebc60e3b8282aa54b8d01"
+    sha256 cellar: :any_skip_relocation, ventura:        "eaef9939d9a58333cadad5c2be3f696faccc6a2bb6bebc60e3b8282aa54b8d01"
+    sha256 cellar: :any_skip_relocation, monterey:       "eaef9939d9a58333cadad5c2be3f696faccc6a2bb6bebc60e3b8282aa54b8d01"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3cf284530226365147d69a8b837683507d011bf20ed4d87d957fb660d6a36c43"
   end
 
   depends_on "go" => :build
