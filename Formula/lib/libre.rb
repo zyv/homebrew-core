ccd7a2424533db92376fc7076cef0dad7ebac43e
Rui Chen <rui@chenrui.dev>
2024-04-09 08:49:11 -0400

    libre 3.11.0

--- Formula/lib/libre.rb
+++ Formula/lib/libre.rb
@@ -1,8 +1,8 @@
 class Libre < Formula
   desc "Toolkit library for asynchronous network I/O with protocol stacks"
   homepage "https://github.com/baresip/re"
-  url "https://github.com/baresip/re/archive/refs/tags/v3.10.0.tar.gz"
-  sha256 "275f1eb074a517ee1116266e59e1ef3400e1f4b8b81ad9807dc4504e94002b4d"
+  url "https://github.com/baresip/re/archive/refs/tags/v3.11.0.tar.gz"
+  sha256 "a29dbdbbacd27461b9c8e94b0e52773f3b1396a64e31e258635f18cf5f27e44e"
   license "BSD-3-Clause"
 
   bottle do
