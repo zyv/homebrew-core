0bcb71d413535b161e58345f7bfd6873597d7559
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-04-10 12:33:08 +0000

    vgmstream: update r1896_1 bottle.

--- Formula/v/vgmstream.rb
+++ Formula/v/vgmstream.rb
@@ -17,13 +17,13 @@ class Vgmstream < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "c0af31a7a46c5516fc5e1c3bbe867849089789fc628224c6cc3cd39ef87bb77e"
-    sha256 cellar: :any,                 arm64_ventura:  "a6d0f22450506ea7a0f506ae7cf8000733fcf6b3484573bcfa57a157740893d0"
-    sha256 cellar: :any,                 arm64_monterey: "25de05664a5f8d75cb24381b979ac3f2a7cd204ae0730b8d54147b4a68df2966"
-    sha256 cellar: :any,                 sonoma:         "017573c62b35ff8d33e21b24e92cf4e15d6af1dbd03ba03c7ca1a32e6cd019f6"
-    sha256 cellar: :any,                 ventura:        "c76243e533cfb1e6cafc618a2a920ac9bd82d438fd60c940fc74dd45d654bfb6"
-    sha256 cellar: :any,                 monterey:       "ee29b4ff0ebcab165ecbb0ae8d4e7b26d940dbf1285a3b537a2e0c52a89adccd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fdc64a6ac20461be95dc805d9f700387fa2405b4ea622b442183a725ad02051d"
+    sha256 cellar: :any,                 arm64_sonoma:   "c9d866183fd14d8cc60672a66bc2951e5ba6fef088f0c1f18364b43e37dd3e82"
+    sha256 cellar: :any,                 arm64_ventura:  "a456abf3e5b6aed9c998b55e80741930cf34905cfd7bbb706cd7c0d35eef8fbb"
+    sha256 cellar: :any,                 arm64_monterey: "f34ae184eaf0fddec4322078b53b9f177fa95ce7e1e38bb2679bdec508ebf489"
+    sha256 cellar: :any,                 sonoma:         "68c38a3569afe428b6c94184537a876c5fe8c4e76c77cba24b1e6ef41a3b7ece"
+    sha256 cellar: :any,                 ventura:        "53197ef2b9b776f81a735c199ecef7c5495fb97e2cd987aab7340a1659a4cb21"
+    sha256 cellar: :any,                 monterey:       "75d00ad77223cdb095f8e4e0052df23dce5579cbd02461f5c608431b74ac01b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9daa41b3c2e228c150a9873e737c8315bc7392dd9f5f945c0841634acd92d648"
   end
 
   depends_on "autoconf" => :build
