62319fb0ac2ac7263fb959327a77d9681e7da542
Rui Chen <rui@chenrui.dev>
2024-04-11 12:08:16 -0400

    docker 26.0.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/docker.rb
+++ Formula/d/docker.rb
@@ -2,8 +2,8 @@ class Docker < Formula
   desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/cli.git",
-      tag:      "v26.0.0",
-      revision: "2ae903e86cab51f694c819721cdfdf5eec693720"
+      tag:      "v26.0.1",
+      revision: "d260a54c81efcc3f00fe67dee78c94b16c2f8692"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git", branch: "master"
 
