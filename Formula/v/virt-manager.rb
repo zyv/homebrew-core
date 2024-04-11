c89666fd87cfb711df2074b8e16e2a0cabc99d90
Branch Vincent <branchevincent@gmail.com>
2024-04-10 21:50:50 -0700

    virt-manager: replace `setup_install_args`

--- Formula/v/virt-manager.rb
+++ Formula/v/virt-manager.rb
@@ -20,7 +20,7 @@ class VirtManager < Formula
   depends_on "docutils" => :build
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
-
+  depends_on "python-setuptools" => :build
   depends_on "adwaita-icon-theme"
   depends_on "certifi"
   depends_on "cpio"
@@ -37,21 +37,19 @@ class VirtManager < Formula
   depends_on "spice-gtk"
   depends_on "vte3"
 
-  # Resources are for Python `libvirt-python` and `requests` packages
-
   resource "charset-normalizer" do
     url "https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz"
     sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
   end
 
   resource "idna" do
-    url "https://files.pythonhosted.org/packages/8b/e1/43beb3d38dba6cb420cefa297822eac205a277ab43e5ba5d5c46faf96438/idna-3.4.tar.gz"
-    sha256 "814f528e8dead7d329833b91c5faa87d60bf71824cd12a7530b5526063d02cb4"
+    url "https://files.pythonhosted.org/packages/21/ed/f86a79a07470cb07819390452f178b3bef1d375f2ec021ecfc709fc7cf07/idna-3.7.tar.gz"
+    sha256 "028ff3aadf0609c1fd278d8ea3089299412a7a8b9bd005dd08b9f8285bcb5cfc"
   end
 
   resource "libvirt-python" do
-    url "https://files.pythonhosted.org/packages/92/92/315e66f442147369a592d2afe07df1321602c5c5ef93ed15cf4eecf90ffb/libvirt-python-9.9.0.tar.gz"
-    sha256 "a97327417851b72aa24e24cdfe10765df0037b9775564cd19bcee25e22b341ea"
+    url "https://files.pythonhosted.org/packages/47/f7/5c5112f79761616bf0388b97bb4d0ea1de1d015fb46a40672fe56fdc8ef0/libvirt-python-10.2.0.tar.gz"
+    sha256 "483a2e38ffc2e65f743e4c819ccb45135dbe50b594a0a2cd60b73843dcfde694"
   end
 
   resource "requests" do
@@ -59,22 +57,23 @@ class VirtManager < Formula
     sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
   end
 
-  # requests require "urllib3>=1.21.1,<1.27"
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/0c/39/64487bf07df2ed854cc06078c27c0d0abc59bd27b32232876e403c333a08/urllib3-1.26.18.tar.gz"
-    sha256 "f8ecc1bba5667413457c529ab955bf8c67b45db799d159066261719e328580a0"
+    url "https://files.pythonhosted.org/packages/7a/50/7fd50a27caa0652cd4caf224aa87741ea41d3265ad13f010886167cfcc79/urllib3-2.2.1.tar.gz"
+    sha256 "d0570876c61ab9e520d776c38acbbb5b05a776d3f9ff98a5c8fd5162a444cf19"
   end
 
   def install
-    python = "python3.12"
-    venv = virtualenv_create(libexec, python)
+    python3 = "python3.12"
+    venv = virtualenv_create(libexec, python3)
     venv.pip_install resources
 
-    args = Language::Python.setup_install_args(prefix, python)
-    args.insert((args.index "install"), "--no-update-icon-cache", "--no-compile-schemas")
-
+    # Restore disabled egg_info command
+    inreplace "setup.py", "'install_egg_info': my_egg_info,", ""
     system libexec/"bin/python", "setup.py", "configure", "--prefix=#{prefix}"
-    system libexec/"bin/python", *args
+    ENV["PIP_CONFIG_SETTINGS"] = "--global-option=--no-update-icon-cache --no-compile-schemas"
+    venv.pip_install_and_link buildpath
+
+    prefix.install libexec/"share"
   end
 
   def post_install
