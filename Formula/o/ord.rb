c43f22030d8e644624e3b89693b7782af5e8a953
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-11 21:44:40 +0000

    ord 0.18.1

--- Formula/o/ord.rb
+++ Formula/o/ord.rb
@@ -1,8 +1,8 @@
 class Ord < Formula
   desc "Index, block explorer, and command-line wallet"
   homepage "https://ordinals.com/"
-  url "https://github.com/ordinals/ord/archive/refs/tags/0.18.0.tar.gz"
-  sha256 "6914fe2dca3be4e173c0057262d3ead4b2e506a291f8805c18024272989f2679"
+  url "https://github.com/ordinals/ord/archive/refs/tags/0.18.1.tar.gz"
+  sha256 "cef87229084a25b94dc730ec5bcac755a9917ef0c41ba440e16ae9cc0185259e"
   license "CC0-1.0"
   head "https://github.com/ordinals/ord.git", branch: "master"
 
