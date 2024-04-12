e4e4adaa2f73bf04e4b5e85cbd2d529ecb220a36
Rui Chen <rui@chenrui.dev>
2024-04-11 21:25:02 -0400

    ninja 1.12.0
    autobump: add ninja
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    ninja: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/n/ninja.rb
+++ Formula/n/ninja.rb
@@ -1,8 +1,8 @@
 class Ninja < Formula
   desc "Small build system for use with gyp or CMake"
   homepage "https://ninja-build.org/"
-  url "https://github.com/ninja-build/ninja/archive/refs/tags/v1.11.1.tar.gz"
-  sha256 "31747ae633213f1eda3842686f83c2aa1412e0f5691d1c14dbbcc67fe7400cea"
+  url "https://github.com/ninja-build/ninja/archive/refs/tags/v1.12.0.tar.gz"
+  sha256 "8b2c86cd483dc7fcb7975c5ec7329135d210099a89bc7db0590a07b0bbfe49a5"
   license "Apache-2.0"
   head "https://github.com/ninja-build/ninja.git", branch: "master"
 
@@ -25,13 +25,6 @@ class Ninja < Formula
   end
 
   uses_from_macos "python" => [:build, :test], since: :catalina
-
-  # Fix `source code cannot contain null bytes` for Python 3.11.4+
-  # https://github.com/ninja-build/ninja/pull/2311
-  patch do
-    url "https://github.com/ninja-build/ninja/commit/67834978a6abdfb790dac165b8b1f1c93648e624.patch?full_index=1"
-    sha256 "078c7d08278aebff346b0e7490d98f3d147db88ebfa6abf34be615b5f12bdf42"
-  end
 
   def install
     system "python3", "configure.py", "--bootstrap", "--verbose", "--with-python=python3"
