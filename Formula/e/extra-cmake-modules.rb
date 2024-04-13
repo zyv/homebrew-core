e436cc1eb34e233be62af1b0d8856dc5ca3dc0da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-13 02:09:13 +0000

    extra-cmake-modules: update 6.1.0 bottle.

--- Formula/e/extra-cmake-modules.rb
+++ Formula/e/extra-cmake-modules.rb
@@ -12,13 +12,13 @@ class ExtraCmakeModules < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "25365d192ade720851fe1fd5c597e0cba2b1ff2c01da2e672e623ef2051412e1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "25365d192ade720851fe1fd5c597e0cba2b1ff2c01da2e672e623ef2051412e1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "25365d192ade720851fe1fd5c597e0cba2b1ff2c01da2e672e623ef2051412e1"
-    sha256 cellar: :any_skip_relocation, sonoma:         "2044e06efc724680757efafece7b2bb8e426c7ce38b65e6d1c910a87b9b6d64c"
-    sha256 cellar: :any_skip_relocation, ventura:        "2044e06efc724680757efafece7b2bb8e426c7ce38b65e6d1c910a87b9b6d64c"
-    sha256 cellar: :any_skip_relocation, monterey:       "2044e06efc724680757efafece7b2bb8e426c7ce38b65e6d1c910a87b9b6d64c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "25365d192ade720851fe1fd5c597e0cba2b1ff2c01da2e672e623ef2051412e1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "eb39f06cc5e01aed45c8214f6106c54226bf9fa1132bdc000bf656a422c01822"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "eb39f06cc5e01aed45c8214f6106c54226bf9fa1132bdc000bf656a422c01822"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "eb39f06cc5e01aed45c8214f6106c54226bf9fa1132bdc000bf656a422c01822"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ef28b4c2e2f8ea787b934f2d2b71caacb407c4136250020b148cd0d32e751ea6"
+    sha256 cellar: :any_skip_relocation, ventura:        "ef28b4c2e2f8ea787b934f2d2b71caacb407c4136250020b148cd0d32e751ea6"
+    sha256 cellar: :any_skip_relocation, monterey:       "ef28b4c2e2f8ea787b934f2d2b71caacb407c4136250020b148cd0d32e751ea6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eb39f06cc5e01aed45c8214f6106c54226bf9fa1132bdc000bf656a422c01822"
   end
 
   depends_on "cmake" => [:build, :test]
