d85a8617f2d8558e2dbbde76e2a1d039bee3acde
Rui Chen <rui@chenrui.dev>
2024-04-12 20:12:57 -0400

    aws-console 1.8.5
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/aws-console.rb
+++ Formula/a/aws-console.rb
@@ -1,8 +1,8 @@
 class AwsConsole < Formula
   desc "Command-line to use AWS CLI credentials to launch the AWS console in a browser"
   homepage "https://github.com/aws-cloudformation/rain"
-  url "https://github.com/aws-cloudformation/rain/archive/refs/tags/v1.8.4.tar.gz"
-  sha256 "90310e75341ea74acccaa201c04df928976e919e57deb51767682faccb991588"
+  url "https://github.com/aws-cloudformation/rain/archive/refs/tags/v1.8.5.tar.gz"
+  sha256 "59b3f60572ef108e5f651560c53be9d1ab509cbfcdc40a26cc1e1dd0cd3ce634"
   license "Apache-2.0"
 
   livecheck do
