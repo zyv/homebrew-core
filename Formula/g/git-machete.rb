64677b900911569b3c13b752353827cd6bc732f5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-14 22:06:46 +0000

    git-machete: update 3.25.0 bottle.

--- Formula/g/git-machete.rb
+++ Formula/g/git-machete.rb
@@ -8,13 +8,13 @@ class GitMachete < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e855de81a2fb5d723a29eb50506d48ff0a17c0fe7b3e2fefc14d2a13adc93bbe"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e855de81a2fb5d723a29eb50506d48ff0a17c0fe7b3e2fefc14d2a13adc93bbe"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e855de81a2fb5d723a29eb50506d48ff0a17c0fe7b3e2fefc14d2a13adc93bbe"
-    sha256 cellar: :any_skip_relocation, sonoma:         "b4c51d4a81a6ab3cee5aebff1db2d16476ceb05f39c61ab6927a8e56a2c613b3"
-    sha256 cellar: :any_skip_relocation, ventura:        "b4c51d4a81a6ab3cee5aebff1db2d16476ceb05f39c61ab6927a8e56a2c613b3"
-    sha256 cellar: :any_skip_relocation, monterey:       "b4c51d4a81a6ab3cee5aebff1db2d16476ceb05f39c61ab6927a8e56a2c613b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e33b442b21ee06cffff73abbdc9222f5cbcec3516aeab3518ad0bcff562ae568"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "359f4094718752d4c937c3a6a766002749c59b00aa4805881d4a320450379e58"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "359f4094718752d4c937c3a6a766002749c59b00aa4805881d4a320450379e58"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "359f4094718752d4c937c3a6a766002749c59b00aa4805881d4a320450379e58"
+    sha256 cellar: :any_skip_relocation, sonoma:         "359f4094718752d4c937c3a6a766002749c59b00aa4805881d4a320450379e58"
+    sha256 cellar: :any_skip_relocation, ventura:        "359f4094718752d4c937c3a6a766002749c59b00aa4805881d4a320450379e58"
+    sha256 cellar: :any_skip_relocation, monterey:       "359f4094718752d4c937c3a6a766002749c59b00aa4805881d4a320450379e58"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8d6124e2a53a96c793bb38801699a2c71aac26af778fcb6f56d352a3163cddfb"
   end
 
   depends_on "python@3.12"
