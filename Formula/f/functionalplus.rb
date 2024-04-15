f0af30fe3bbe854e4e974a11d469775f6fec844c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-15 18:32:04 +0000

    functionalplus 0.2.24

--- Formula/f/functionalplus.rb
+++ Formula/f/functionalplus.rb
@@ -1,8 +1,8 @@
 class Functionalplus < Formula
   desc "Functional Programming Library for C++"
   homepage "https://github.com/Dobiasd/FunctionalPlus"
-  url "https://github.com/Dobiasd/FunctionalPlus/archive/refs/tags/v0.2.23.tar.gz"
-  sha256 "5c2d28d2ba7d0cdeab9e31bbf2e7f8a9d6f2ff6111a54bfc11d1b05422096f19"
+  url "https://github.com/Dobiasd/FunctionalPlus/archive/refs/tags/v0.2.24.tar.gz"
+  sha256 "446c63ac3f2045e7587f694501882a3d7c7b962b70bcc08deacf5777bdaaff8c"
   license "BSL-1.0"
   head "https://github.com/Dobiasd/FunctionalPlus.git", branch: "master"
 
