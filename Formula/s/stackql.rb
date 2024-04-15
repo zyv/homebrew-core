91521e9588623552bf0745e79bf9250743286317
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-15 03:02:09 +0000

    stackql 0.5.612
    stackql: update build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/stackql.rb
+++ Formula/s/stackql.rb
@@ -1,9 +1,8 @@
 class Stackql < Formula
   desc "SQL interface for arbitrary resources with full CRUD support"
   homepage "https://stackql.io/"
-  url "https://github.com/stackql/stackql.git",
-      tag:      "v0.5.604",
-      revision: "39293f5ea1b1f69ab276fa149bf30d9a7ae643e2"
+  url "https://github.com/stackql/stackql/archive/refs/tags/v0.5.612.tar.gz"
+  sha256 "f35e7c00e024896dd1480b8f1f53847beaf64b49a2b6c522fdc054d0c7787e61"
   license "MIT"
 
   livecheck do
@@ -24,17 +23,17 @@ class Stackql < Formula
   depends_on "go" => :build
 
   def install
-    ENV["CGO_ENABLED"] = "1"
-    ldflags = [
-      "-s",
-      "-w",
-      "-X github.com/stackql/stackql/internal/stackql/cmd.BuildMajorVersion=#{version.major}",
-      "-X github.com/stackql/stackql/internal/stackql/cmd.BuildMinorVersion=#{version.minor}",
-      "-X github.com/stackql/stackql/internal/stackql/cmd.BuildPatchVersion=#{version.patch}",
-      "-X github.com/stackql/stackql/internal/stackql/cmd.BuildCommitSHA=#{Utils.git_head}",
-      "-X github.com/stackql/stackql/internal/stackql/cmd.BuildShortCommitSHA=#{Utils.git_short_head}",
-      "-X stackql/internal/stackql/planbuilder.PlanCacheEnabled=true",
+    ldflags = %W[
+      -s -w
+      -X github.com/stackql/stackql/internal/stackql/cmd.BuildMajorVersion=#{version.major}
+      -X github.com/stackql/stackql/internal/stackql/cmd.BuildMinorVersion=#{version.minor}
+      -X github.com/stackql/stackql/internal/stackql/cmd.BuildPatchVersion=#{version.patch}
+      -X github.com/stackql/stackql/internal/stackql/cmd.BuildCommitSHA=#{tap.user}
+      -X github.com/stackql/stackql/internal/stackql/cmd.BuildShortCommitSHA=#{tap.user}
+      -X github.com/stackql/stackql/internal/stackql/cmd.BuildDate=#{time.iso8601}
+      -X stackql/internal/stackql/planbuilder.PlanCacheEnabled=true
     ]
+
     system "go", "build", *std_go_args(ldflags:), "--tags", "json1 sqleanall", "./stackql"
   end
 
