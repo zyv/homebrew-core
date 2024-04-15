869f7b8752c4720936d620f87c3d870ea648e404
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-04-16 00:02:50 +0800

    fb303 2024.04.15.00

--- Formula/f/fb303.rb
+++ Formula/f/fb303.rb
@@ -1,8 +1,8 @@
 class Fb303 < Formula
   desc "Thrift functions for querying information from a service"
   homepage "https://github.com/facebook/fb303"
-  url "https://github.com/facebook/fb303/archive/refs/tags/v2024.04.08.00.tar.gz"
-  sha256 "d3913610db26312e4c212dd8f7eeb6ee6780a2176152834156642105aa740669"
+  url "https://github.com/facebook/fb303/archive/refs/tags/v2024.04.15.00.tar.gz"
+  sha256 "993de95412962d4883437ef5a4bc94eda6b6927ffe2e083274132e3e862d10ae"
   license "Apache-2.0"
   head "https://github.com/facebook/fb303.git", branch: "main"
 
