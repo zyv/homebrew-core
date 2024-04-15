a498a4fac1352dd22dbab694d556dccfbfcfe351
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-15 18:33:08 +0000

    gopls 0.15.3

--- Formula/g/gopls.rb
+++ Formula/g/gopls.rb
@@ -1,8 +1,8 @@
 class Gopls < Formula
   desc "Language server for the Go language"
   homepage "https://github.com/golang/tools/tree/master/gopls"
-  url "https://github.com/golang/tools/archive/refs/tags/gopls/v0.15.2.tar.gz"
-  sha256 "cf5b8246b36967eb8fbbed518ea941110cc6bbcc7f42a44bc5a4fe0d7ee61652"
+  url "https://github.com/golang/tools/archive/refs/tags/gopls/v0.15.3.tar.gz"
+  sha256 "7fb0a43ff5e562089ca0ec3e8e7819dd205977758c02ea9bded05f56d5080a54"
   license "BSD-3-Clause"
 
   livecheck do
