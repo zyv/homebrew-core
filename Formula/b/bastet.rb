d8598d04b5d8e48622ff1ad79bd855356c67a2f4
Michael Cho <michael@michaelcho.dev>
2024-04-16 17:26:21 -0400

    bastet: revision bump (boost 1.85.0)

--- Formula/b/bastet.rb
+++ Formula/b/bastet.rb
@@ -4,7 +4,7 @@ class Bastet < Formula
   url "https://github.com/fph/bastet/archive/refs/tags/0.43.2.tar.gz"
   sha256 "f219510afc1d83e4651fbffd5921b1e0b926d5311da4f8fa7df103dc7f2c403f"
   license "GPL-3.0-or-later"
-  revision 7
+  revision 8
 
   bottle do
     sha256 arm64_sonoma:   "e3b58e16c8b3462ad7f3fe67a4f10810718e81dc6498d64e685bac37375372c9"
