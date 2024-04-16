126c3717cf78d4e4903f144e875b7f4e2705b401
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-16 18:47:42 +0000

    spidermonkey 115.10.0

--- Formula/s/spidermonkey.rb
+++ Formula/s/spidermonkey.rb
@@ -1,9 +1,9 @@
 class Spidermonkey < Formula
   desc "JavaScript-C Engine"
   homepage "https://spidermonkey.dev"
-  url "https://archive.mozilla.org/pub/firefox/releases/115.9.1esr/source/firefox-115.9.1esr.source.tar.xz"
-  version "115.9.1"
-  sha256 "23657808bfefb8ba33a191645d4df776d5b8d99d453edde32b785d2a8846f929"
+  url "https://archive.mozilla.org/pub/firefox/releases/115.10.0esr/source/firefox-115.10.0esr.source.tar.xz"
+  version "115.10.0"
+  sha256 "0afd3c733d95f7047f258d1a9768d06d856217fe736d85bfb370db9dd926eef2"
   license "MPL-2.0"
   head "https://hg.mozilla.org/mozilla-central", using: :hg
 
