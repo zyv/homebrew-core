88b0cbbf061c1e4a9d46bc9ea2fb67ab4236a876
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-16 21:26:14 +0000

    envoy 1.30.1
    Co-authored-by: Michael Cho <michael@michaelcho.dev>
    Signed-off-by: Michael Cho <michael@michaelcho.dev>

--- Formula/e/envoy.rb
+++ Formula/e/envoy.rb
@@ -1,8 +1,8 @@
 class Envoy < Formula
   desc "Cloud-native high-performance edge/middle/service proxy"
   homepage "https://www.envoyproxy.io/index.html"
-  url "https://github.com/envoyproxy/envoy/archive/refs/tags/v1.29.3.tar.gz"
-  sha256 "892ca7702b6b751665e35fbe9c8d2be0bcf52c4aa2aa4714581f5c5544c248b3"
+  url "https://github.com/envoyproxy/envoy/archive/refs/tags/v1.30.1.tar.gz"
+  sha256 "8f0f34d4a2b2f07ffcd898d62773dd644a5944859e0ed2cdf20cd381d6ea7f9d"
   license "Apache-2.0"
   head "https://github.com/envoyproxy/envoy.git", branch: "main"
 
@@ -43,10 +43,6 @@ class Envoy < Formula
   fails_with :gcc
 
   def install
-    # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
-    # is not set then it's forced to 10.4, which breaks compile on Mojave.
-    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version.to_s if OS.mac?
-
     env_path = "#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin"
     args = %W[
       --compilation_mode=opt
@@ -56,31 +52,13 @@ class Envoy < Formula
       --host_action_env=PATH=#{env_path}
     ]
 
-    if OS.linux?
-      # Build fails with GCC 10+ at external/com_google_absl/absl/container/internal/inlined_vector.h:448:5:
-      # error: '<anonymous>.absl::inlined_vector_internal::Storage<char, 128, std::allocator<char> >::data_'
-      # is used uninitialized in this function [-Werror=uninitialized]
-      # Try to remove in a release that uses a newer abseil
-      args << "--cxxopt=-Wno-uninitialized"
-      args << "--host_cxxopt=-Wno-uninitialized"
+    # Work around build issue with Xcode 15.3 / LLVM 16+
+    # https://github.com/envoyproxy/envoy/issues/33225
+    # https://gitlab.freedesktop.org/pkg-config/pkg-config/-/issues/81
+    args << "--host_action_env=CFLAGS=-Wno-int-conversion"
 
-      # Work around build failure using clang with libstdc++: https://github.com/envoyproxy/envoy/issues/31856
-      args << "--cxxopt=-fsized-deallocation"
-      args << "--config=clang"
-    else
-      # The clang available on macOS catalina has a warning that isn't clean on v8 code.
-      # The warning doesn't show up with more recent clangs, so disable it for now.
-      args << "--cxxopt=-Wno-range-loop-analysis"
-      args << "--host_cxxopt=-Wno-range-loop-analysis"
-
-      # To suppress warning on deprecated declaration on v8 code. For example:
-      # external/v8/src/base/platform/platform-darwin.cc:56:22: 'getsectdatafromheader_64'
-      # is deprecated: first deprecated in macOS 13.0.
-      # https://bugs.chromium.org/p/v8/issues/detail?id=13428.
-      # Reference: https://github.com/envoyproxy/envoy/pull/23707.
-      args << "--cxxopt=-Wno-deprecated-declarations"
-      args << "--host_cxxopt=-Wno-deprecated-declarations"
-    end
+    # GCC/ld.gold had some issues while building envoy so use clang/lld instead
+    args << "--config=clang" if OS.linux?
 
     # Write the current version SOURCE_VERSION.
     system "python3", "tools/github/write_current_source_version.py", "--skip_error_in_git"
