e43f3c0568124964599e4838764e97fe4ed4c5fe
Patrick Linnane <patrick@linnane.io>
2024-04-17 09:50:18 -0700

    valkey 7.2.5
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/v/valkey.rb
+++ Formula/v/valkey.rb
@@ -1,8 +1,8 @@
 class Valkey < Formula
   desc "High-performance data structure server that primarily serves key/value workloads"
   homepage "https://valkey.io"
-  url "https://github.com/valkey-io/valkey/archive/refs/tags/redis-7.2.4.tar.gz"
-  sha256 "275bd332d1013c288915469466aae165d349a599494d91a46cf22798910be327"
+  url "https://github.com/valkey-io/valkey/archive/refs/tags/7.2.5.tar.gz"
+  sha256 "c7c7a758edabe7693b3692db58fe5328130036b06224df64ab1f0c12fe265a76"
   license "BSD-3-Clause"
   head "https://github.com/valkey-io/valkey.git", branch: "unstable"
 
@@ -23,29 +23,29 @@ class Valkey < Formula
   def install
     system "make", "install", "PREFIX=#{prefix}", "CC=#{ENV.cc}", "BUILD_TLS=yes"
 
-    %w[run db/redis log].each { |p| (var/p).mkpath }
+    %w[run db/valkey log].each { |p| (var/p).mkpath }
 
     # Fix up default conf file to match our paths
-    inreplace "redis.conf" do |s|
-      s.gsub! "/var/run/redis_6379.pid", var/"run/redis.pid"
-      s.gsub! "dir ./", "dir #{var}/db/redis/"
+    inreplace "valkey.conf" do |s|
+      s.gsub! "/var/run/valkey_6379.pid", var/"run/valkey.pid"
+      s.gsub! "dir ./", "dir #{var}/db/valkey/"
       s.sub!(/^bind .*$/, "bind 127.0.0.1 ::1")
     end
 
-    etc.install "redis.conf"
-    etc.install "sentinel.conf" => "redis-sentinel.conf"
+    etc.install "valkey.conf"
+    etc.install "sentinel.conf" => "valkey-sentinel.conf"
   end
 
   service do
-    run [opt_bin/"redis-server", etc/"redis.conf"]
+    run [opt_bin/"valkey-server", etc/"valkey.conf"]
     keep_alive true
-    error_log_path var/"log/redis.log"
-    log_path var/"log/redis.log"
+    error_log_path var/"log/valkey.log"
+    log_path var/"log/valkey.log"
     working_dir var
   end
 
   test do
-    system bin/"redis-server", "--test-memory", "2"
-    %w[run db/redis log].each { |p| assert_predicate var/p, :exist?, "#{var/p} doesn't exist!" }
+    system bin/"valkey-server", "--test-memory", "2"
+    %w[run db/valkey log].each { |p| assert_predicate var/p, :exist?, "#{var/p} doesn't exist!" }
   end
 end
