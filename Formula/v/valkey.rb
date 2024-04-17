6e632717fefe3aa7d90c5f653cde80901029f336
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-17 17:18:15 +0000

    valkey: update 7.2.5 bottle.

--- Formula/v/valkey.rb
+++ Formula/v/valkey.rb
@@ -7,13 +7,13 @@ class Valkey < Formula
   head "https://github.com/valkey-io/valkey.git", branch: "unstable"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "379bf80808797ec3c9885baf86e5426360171c7c9663db664fe4f73575bd7dce"
-    sha256 cellar: :any,                 arm64_ventura:  "cff11b3943ce50a90fa31f75fe062191c95acd1b0fcd4361fa4b2857b2333105"
-    sha256 cellar: :any,                 arm64_monterey: "f6ccd387476c12611ea90f45e55e0cb31cf21db04489b37be1a06f71814fec55"
-    sha256 cellar: :any,                 sonoma:         "fbfd66eafd961bb2c5af5de4dac9fa92cd75ad9f55e0410c49b1c6b95606b121"
-    sha256 cellar: :any,                 ventura:        "4e11ec6456fcfeaeb69ce7b2c6b5e74d25c412ca8612cb61b83605f0ca8e48a7"
-    sha256 cellar: :any,                 monterey:       "75b05936f7419e56de73eb410e9a112bd65a30a308bbe6e60a1c61cf718666a1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "be464c47d5f879ea10eda3ffac294134394c10c6f44a33c8295a4da7668c9e73"
+    sha256 cellar: :any,                 arm64_sonoma:   "a40c337f18bad2ae274199b35ee1e261b18ed3521a67bc61a0a4cf4169eaa893"
+    sha256 cellar: :any,                 arm64_ventura:  "cb62ce022ad11d6c12424a028df11ca21b4c8479c7731124536ab62f9a055522"
+    sha256 cellar: :any,                 arm64_monterey: "fffd8f00b86e2c5cf2140c3d178c5834857a77416a043f3427ce9a9b35acf8cc"
+    sha256 cellar: :any,                 sonoma:         "9e1451cda196df3bc62b82832ba613af06bca50d3ef4c93450b0ca320244d699"
+    sha256 cellar: :any,                 ventura:        "3aec084610e710f07c16a6861a45b0bc539e02ef3b815cc8da9267f5b02a5bfe"
+    sha256 cellar: :any,                 monterey:       "4a55fb259562e56eb8df25e92c0fa1fe33eead6ca06c6908e61969173924f62b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "508f9fbd61b282abc3278a4dc015b69c6bcc1c3cf84d0969a3281cab820e6e15"
   end
 
   depends_on "openssl@3"
