e949c4c93ba9d1ab6e3002ae8d93713bc6f1ec71
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-17 03:20:35 +0000

    naturaldocs: update 2.3 bottle.

--- Formula/n/naturaldocs.rb
+++ Formula/n/naturaldocs.rb
@@ -12,13 +12,14 @@ class Naturaldocs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "46d41e17b53b336d1db203a705164e735af1db6021cc973d126de1091fb8ce0c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "46d41e17b53b336d1db203a705164e735af1db6021cc973d126de1091fb8ce0c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "46d41e17b53b336d1db203a705164e735af1db6021cc973d126de1091fb8ce0c"
-    sha256 cellar: :any_skip_relocation, ventura:        "46d41e17b53b336d1db203a705164e735af1db6021cc973d126de1091fb8ce0c"
-    sha256 cellar: :any_skip_relocation, monterey:       "46d41e17b53b336d1db203a705164e735af1db6021cc973d126de1091fb8ce0c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "46d41e17b53b336d1db203a705164e735af1db6021cc973d126de1091fb8ce0c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "44e058e2339a3d6113ac8564fa1b9557c1b93783f541a82a463d03b82f45fb8e"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c8381f2df948395879acd6d145586b1f94f94cb99d5f7fb5a60560b2584934bf"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c8381f2df948395879acd6d145586b1f94f94cb99d5f7fb5a60560b2584934bf"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c8381f2df948395879acd6d145586b1f94f94cb99d5f7fb5a60560b2584934bf"
+    sha256 cellar: :any_skip_relocation, sonoma:         "c8381f2df948395879acd6d145586b1f94f94cb99d5f7fb5a60560b2584934bf"
+    sha256 cellar: :any_skip_relocation, ventura:        "c8381f2df948395879acd6d145586b1f94f94cb99d5f7fb5a60560b2584934bf"
+    sha256 cellar: :any_skip_relocation, monterey:       "c8381f2df948395879acd6d145586b1f94f94cb99d5f7fb5a60560b2584934bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5c39b957abe3a6a51190a7ad008dc9bd8c7ef2014b8c03ff5f0b258c1293735e"
   end
 
   depends_on "mono"
