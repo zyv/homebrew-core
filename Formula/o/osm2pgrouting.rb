b8ee135f89d14eaebbd9353c9d3bda74f4e99707
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-04-18 10:15:31 +0800

    osm2pgrouting: remove LLVM from library search paths

--- Formula/o/osm2pgrouting.rb
+++ Formula/o/osm2pgrouting.rb
@@ -4,7 +4,7 @@ class Osm2pgrouting < Formula
   url "https://github.com/pgRouting/osm2pgrouting/archive/refs/tags/v2.3.8.tar.gz"
   sha256 "e3a58bcacf0c8811e0dcf3cf3791a4a7cc5ea2a901276133eacf227b30fd8355"
   license "GPL-2.0-or-later"
-  revision 11
+  revision 12
   head "https://github.com/pgRouting/osm2pgrouting.git", branch: "main"
 
   bottle do
@@ -28,10 +28,18 @@ class Osm2pgrouting < Formula
   fails_with gcc: "5"
 
   def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args
-      system "make", "install"
+    # Work around an Xcode 15 linker issue which causes linkage against LLVM's
+    # libunwind due to it being present in a library search path.
+    if DevelopmentTools.clang_build_version >= 1500
+      recursive_dependencies
+        .select { |d| d.name.match?(/^llvm(@\d+)?$/) }
+        .map { |llvm_dep| llvm_dep.to_formula.opt_lib }
+        .each { |llvm_lib| ENV.remove "HOMEBREW_LIBRARY_PATHS", llvm_lib }
     end
+
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
