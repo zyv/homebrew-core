f665b4e834724b6369b4a09b28497caf6e1369c4
Rui Chen <rui@chenrui.dev>
2024-04-17 23:11:11 -0400

    miniupnpc 2.2.7
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    miniupnpc: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/miniupnpc.rb
+++ Formula/m/miniupnpc.rb
@@ -1,9 +1,9 @@
 class Miniupnpc < Formula
   desc "UPnP IGD client library and daemon"
   homepage "https://miniupnp.tuxfamily.org"
-  url "https://miniupnp.tuxfamily.org/files/download.php?file=miniupnpc-2.2.6.tar.gz"
-  mirror "https://deb.debian.org/debian/pool/main/m/miniupnpc/miniupnpc_2.2.6.orig.tar.gz"
-  sha256 "37fcd91953508c3e62d6964bb8ffbc5d47f3e13481fa54e6214fcc68704c66f1"
+  url "https://miniupnp.tuxfamily.org/files/download.php?file=miniupnpc-2.2.7.tar.gz"
+  mirror "https://deb.debian.org/debian/pool/main/m/miniupnpc/miniupnpc_2.2.7.orig.tar.gz"
+  sha256 "b0c3a27056840fd0ec9328a5a9bac3dc5e0ec6d2e8733349cf577b0aa1e70ac1"
   license "BSD-3-Clause"
 
   # We only match versions with only a major/minor since versions like 2.1 are
@@ -28,7 +28,11 @@ class Miniupnpc < Formula
   end
 
   test do
-    output = shell_output("#{bin}/upnpc --help 2>&1", 1)
+    # `No IGD UPnP Device` on CI
+    output = shell_output("#{bin}/upnpc -l 2>&1", 1)
+    assert_match "No IGD UPnP Device found on the network !", output
+
+    output = shell_output("#{bin}/upnpc --help 2>&1")
     assert_match version.to_s, output
   end
 end
