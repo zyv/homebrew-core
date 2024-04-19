20092b08eb6e32c0f473c033e66d51db59b5d9cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-19 08:42:52 +0000

    renovate: update 37.308.0 bottle.

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -19,13 +19,13 @@ class Renovate < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "43f26db4f9f93984d30e146211b4245a54befc5604222cff0c19653cbfaff7de"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2fa0edf7a5dc034b9bab8e0209cef32a9af61f5e7d515d8eadd09caca010ed9d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8fff59f22d839b8f016befa0ec2391827ec9ac60b5d33f174f3665b5e37a4c13"
-    sha256 cellar: :any_skip_relocation, sonoma:         "4669ab6b29bf711b5fa7d0b33314e2933206ee7b84d42706c518b2df1d611d5a"
-    sha256 cellar: :any_skip_relocation, ventura:        "ff51130735644198e1add0c0ee4d0509b34cb632d92a19e480324afc885ffaa8"
-    sha256 cellar: :any_skip_relocation, monterey:       "e63ba8f013bf1b6f8f935aa943276a673979d17d13c3390d12739a6baaa5d3a2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6e687746b6723cabfbd8fc6e82960cf4c4b8b5860c0dcfd9620531061dceda5f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bb58da5b02a79dfa14cbe29ea0392fbcfd254b001b8dee11feacb57c9ced6ee6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "25463b5349db666dfbbcb02b64711fd0118750aeae73d4b463117c5d1d4256fd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7d48ab2d06d377b1727a30909593c245bf30eeb36735006048191a50d94a9ce0"
+    sha256 cellar: :any_skip_relocation, sonoma:         "421585350179b9c6a88b3076dceb3869174feb5c01a9ce4f72c076650be05523"
+    sha256 cellar: :any_skip_relocation, ventura:        "83beb4f716b7858a0b0797c01a0bc7c3255ef30235b30c80159ae83903ba8d1d"
+    sha256 cellar: :any_skip_relocation, monterey:       "bf5342b3224de81c799709b97abde69988eec0f8c868f58409b14101f3d38984"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "67aed7ca911683020af012e7306f1e6566e29b926545eb8d93f28d22e39177ad"
   end
 
   depends_on "node"
