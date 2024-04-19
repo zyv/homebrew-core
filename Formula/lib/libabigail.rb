901f4d0d0e9d6514517b9456199151df49f4278b
Rui Chen <rui@chenrui.dev>
2024-04-19 09:07:57 -0400

    libabigail 2.5
    autobump: add libabigail
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    libabigail: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    libabigail: simplify test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libabigail.rb
+++ Formula/lib/libabigail.rb
@@ -1,8 +1,8 @@
 class Libabigail < Formula
   desc "ABI Generic Analysis and Instrumentation Library"
   homepage "https://sourceware.org/libabigail/"
-  url "https://mirrors.kernel.org/sourceware/libabigail/libabigail-2.4.tar.xz"
-  sha256 "5fe76b6344188a95f693b84e1b8731443d274a4c4b0ebee18fc00d9aedac8509"
+  url "https://mirrors.kernel.org/sourceware/libabigail/libabigail-2.5.tar.xz"
+  sha256 "7cfc4e9b00ae38d87fb0c63beabb32b9cbf9ce410e52ceeb5ad5b3c5beb111f3"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   bottle do
@@ -29,14 +29,6 @@ class Libabigail < Formula
   end
 
   test do
-    expected_output = <<~EOS
-      <abi-corpus version='2.2' path='#{test_fixtures("elf/hello")}' architecture='elf-amd-x86_64'>
-        <elf-needed>
-          <dependency name='libc.so.6'/>
-        </elf-needed>
-      </abi-corpus>
-    EOS
-
-    assert_equal expected_output, shell_output("#{bin}/abilint #{test_fixtures("elf/hello")}")
+    assert_match "<dependency name='libc.so.6'/>", shell_output("#{bin}/abilint #{test_fixtures("elf/hello")}")
   end
 end
