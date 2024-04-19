8839d0df5e500720a24e5f6921a7ee64afcbf611
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-19 06:27:29 +0000

    copilot 1.33.3

--- Formula/c/copilot.rb
+++ Formula/c/copilot.rb
@@ -4,8 +4,8 @@ class Copilot < Formula
   desc "CLI tool for Amazon ECS and AWS Fargate"
   homepage "https://aws.github.io/copilot-cli/"
   url "https://github.com/aws/copilot-cli.git",
-      tag:      "v1.33.2",
-      revision: "8da516ae3af52de374b007d86acf6872f8765b25"
+      tag:      "v1.33.3",
+      revision: "037462229d3ac8b60372e1340771a0d776389a3d"
   license "Apache-2.0"
   head "https://github.com/aws/copilot-cli.git", branch: "mainline"
 
