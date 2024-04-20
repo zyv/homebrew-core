74e7950f89bc608ea35604b8cf9bdf12e8948f20
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-20 19:46:40 +0000

    iredis: update 1.15.0 bottle.

--- Formula/i/iredis.rb
+++ Formula/i/iredis.rb
@@ -9,13 +9,14 @@ class Iredis < Formula
   head "https://github.com/laixintao/iredis.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "202198f2b992839fbeeff865504fe9399495867d4191d2e1c41945e47c823326"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "202198f2b992839fbeeff865504fe9399495867d4191d2e1c41945e47c823326"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "202198f2b992839fbeeff865504fe9399495867d4191d2e1c41945e47c823326"
-    sha256 cellar: :any_skip_relocation, sonoma:         "95b9921cbe9ec9d8bd0d3bbb66e584413deb20dda1a69bc37d77ccf5bcab0a6f"
-    sha256 cellar: :any_skip_relocation, ventura:        "95b9921cbe9ec9d8bd0d3bbb66e584413deb20dda1a69bc37d77ccf5bcab0a6f"
-    sha256 cellar: :any_skip_relocation, monterey:       "95b9921cbe9ec9d8bd0d3bbb66e584413deb20dda1a69bc37d77ccf5bcab0a6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6df3eda00748f5311b1fe24e6fd7cb37af9826eb531503342ca408d6dbcc3c3e"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d30f9e8ea40f326932c1e37082366a595dc8ef317af746260987477ee01af756"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d30f9e8ea40f326932c1e37082366a595dc8ef317af746260987477ee01af756"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d30f9e8ea40f326932c1e37082366a595dc8ef317af746260987477ee01af756"
+    sha256 cellar: :any_skip_relocation, sonoma:         "d30f9e8ea40f326932c1e37082366a595dc8ef317af746260987477ee01af756"
+    sha256 cellar: :any_skip_relocation, ventura:        "d30f9e8ea40f326932c1e37082366a595dc8ef317af746260987477ee01af756"
+    sha256 cellar: :any_skip_relocation, monterey:       "d30f9e8ea40f326932c1e37082366a595dc8ef317af746260987477ee01af756"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7f3d0c7ef651d8a7197611927049062a54c0c39b44f71402cdd1c2f12c6d6d92"
   end
 
   depends_on "python@3.12"
