50e6737a55e3e9c390ee51c2da807e84cc26451a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-20 15:01:05 +0000

    giflib: update 5.2.2 bottle.

--- Formula/g/giflib.rb
+++ Formula/g/giflib.rb
@@ -11,18 +11,13 @@ class Giflib < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "0810045224fbfa1ae8c5bbdfc44d454efcaf76606e865f8be4a46606369c44b1"
-    sha256 cellar: :any,                 arm64_ventura:  "ced5a24b12f7057504aa8821a81c03c4d83ff6ba861487e25eba34b863237c20"
-    sha256 cellar: :any,                 arm64_monterey: "6a1194d7b2d991583e3b5d46782ac8d0cecfc35bc28a5b4daf86ec4311cc7cdc"
-    sha256 cellar: :any,                 arm64_big_sur:  "e9a78b55a43f68f2552f845fff27d1c247ed865b1dd653f4c8ab259594411f86"
-    sha256 cellar: :any,                 sonoma:         "84a39bf9c63c3a0fd781c994921f012b5d34bcb8ab39105909453d8635488337"
-    sha256 cellar: :any,                 ventura:        "7b542ce4281136276979dfbe45cea1a84060f624ee307917c24499398b210103"
-    sha256 cellar: :any,                 monterey:       "fa6adb4afc1abd76f8a80afd8c25572f7c990cbfc88a43496350e8c363048217"
-    sha256 cellar: :any,                 big_sur:        "dc23500f50d599c4dbfcea0107b643bef41538c2f5fd162b049f82d21e3d32d5"
-    sha256 cellar: :any,                 catalina:       "ad97d175fa77f7afb4a1c215538d8ae9eff30435de7feaa6a5d2e29fca7fef4d"
-    sha256 cellar: :any,                 mojave:         "42d2f8a6e9dbf9d4c22a2e64581c7170cc7dcb2a0e66df383efc67b7bc96238d"
-    sha256 cellar: :any,                 high_sierra:    "e1a30a20ad93cd9ec003027d7fba43a7e04ced0bff4156614818cccfc9dec6c9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d753208ed3a4bbd60b59e3ca4466196e4b935d4f434935b540fc6bfb5f3e0385"
+    sha256 cellar: :any,                 arm64_sonoma:   "c6b05aecad00588daf749dbde717fb6a03ce83fb9723b15f5786e7b974ef4c02"
+    sha256 cellar: :any,                 arm64_ventura:  "f0c469da58fa4384fb67b249b4869f3daced90a8326e520aeb2a030af54ccc48"
+    sha256 cellar: :any,                 arm64_monterey: "3089db3525957dedba2e0297997fe5bc6f3add879464102e48257ac12775cff7"
+    sha256 cellar: :any,                 sonoma:         "40d390aab5bc396eb3efa0ae00987efd8c9eb8049239f709f486a879577a41ef"
+    sha256 cellar: :any,                 ventura:        "b3d5cfa490fb61890dceb4b49510171783ab0e4dfc6f64f2f5f8ee1cecc08013"
+    sha256 cellar: :any,                 monterey:       "1b8828d26eeaccc1f3cefdbc41bd55551045d1ae55a18a1c96f4d27bd214df17"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "db5d2754722a81e5a842a66236aeebe889ebae26b08329dbd506007b9e63339c"
   end
 
   # Move logo resizing to be a prereq for giflib website only, so that imagemagick is not required to build package
