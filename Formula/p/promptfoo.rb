138fadfa0db0e56983f69a360bf564aa44855e13
Rui Chen <rui@chenrui.dev>
2024-04-22 20:55:28 -0400

    promptfoo 0.54.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/promptfoo.rb
@@ -0,0 +1,24 @@
+require "language/node"
+
+class Promptfoo < Formula
+  desc "Test your LLM app locally"
+  homepage "https://promptfoo.dev/"
+  url "https://registry.npmjs.org/promptfoo/-/promptfoo-0.54.1.tgz"
+  sha256 "d1d42348a0262c225248ea1c550b6a29a4164c09c84ab8c66db2602eaea21210"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"promptfoo", "init"
+    assert_predicate testpath/".promptfoo", :exist?
+    assert_match "description: 'My first eval'", (testpath/"promptfooconfig.yaml").read
+
+    assert_match version.to_s, shell_output("#{bin}/promptfoo --version", 1)
+  end
+end
