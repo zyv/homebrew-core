99c887634b625ccdd09cfa03e286271680f2b284
Rui Chen <rui@chenrui.dev>
2024-04-23 02:42:04 +0000

    ssh-audit: update 3.2.0 bottle.

--- Formula/s/ssh-audit.rb
+++ Formula/s/ssh-audit.rb
@@ -9,14 +9,13 @@ class SshAudit < Formula
   head "https://github.com/jtesta/ssh-audit.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4a619cb1938808b9f8287a0c6a06bb034731e636d4202840327ab21e891ce13f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a2a0584a0b1ed329429ded79816b8b6492eb11fd38881d7636f9f4b13a7b6346"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "26d24f6071a846e1da5add64d256d4aa5b89c11b929a39cbf9435dee271dc425"
-    sha256 cellar: :any_skip_relocation, sonoma:         "758b38d1c6c3d27bb11495b9d056d6054637ae2cceb7e1fbcc941e1fd238e1a9"
-    sha256 cellar: :any_skip_relocation, ventura:        "3b90b39d7d581ef474943fe79f902228f2b2af567e65fb7938a57afd85163d9a"
-    sha256 cellar: :any_skip_relocation, monterey:       "97a49136ac35cd9f2dc88354223437bf22e3ffe35a4366f226a6585b82c91a63"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2986f208ca79dad832abdfdc430e808c23e0507ea443c18711f2bfd3be148bbb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e52d6192b5ca6851ead47cd60f94820beaf7e8daa570100fb29cfa434bea2764"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e52d6192b5ca6851ead47cd60f94820beaf7e8daa570100fb29cfa434bea2764"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e52d6192b5ca6851ead47cd60f94820beaf7e8daa570100fb29cfa434bea2764"
+    sha256 cellar: :any_skip_relocation, sonoma:         "e52d6192b5ca6851ead47cd60f94820beaf7e8daa570100fb29cfa434bea2764"
+    sha256 cellar: :any_skip_relocation, ventura:        "e52d6192b5ca6851ead47cd60f94820beaf7e8daa570100fb29cfa434bea2764"
+    sha256 cellar: :any_skip_relocation, monterey:       "e52d6192b5ca6851ead47cd60f94820beaf7e8daa570100fb29cfa434bea2764"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50405aed6aa6e102ad1bce2902670ff28b47b9c02d2c9adcff3c0497fbd11271"
   end
 
   depends_on "python@3.12"
