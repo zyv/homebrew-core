7bf552613cd1e537a398cd2f6862253e28a2cb7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-23 11:29:10 +0000

    edgevpn: update 0.25.2 bottle.

--- Formula/e/edgevpn.rb
+++ Formula/e/edgevpn.rb
@@ -6,13 +6,13 @@ class Edgevpn < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2d4ac3066b10354a85585f58ba4f452ab66dee0a04181f249a0edcc5d0d2a876"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2d4ac3066b10354a85585f58ba4f452ab66dee0a04181f249a0edcc5d0d2a876"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2d4ac3066b10354a85585f58ba4f452ab66dee0a04181f249a0edcc5d0d2a876"
-    sha256 cellar: :any_skip_relocation, sonoma:         "072d8c7f189feb197f9556130d4d6a28e09a22ced6df64e30b74d4d42f241a5b"
-    sha256 cellar: :any_skip_relocation, ventura:        "072d8c7f189feb197f9556130d4d6a28e09a22ced6df64e30b74d4d42f241a5b"
-    sha256 cellar: :any_skip_relocation, monterey:       "072d8c7f189feb197f9556130d4d6a28e09a22ced6df64e30b74d4d42f241a5b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ce5feffc0561d24e640c6b2e3294d6bfebda494b8c32c7f8891a6d34ad43431b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "25c04c8908aedcd15c87d36a8930898df8f8e6d2afb5c38891e0f860b4421a52"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "25c04c8908aedcd15c87d36a8930898df8f8e6d2afb5c38891e0f860b4421a52"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "25c04c8908aedcd15c87d36a8930898df8f8e6d2afb5c38891e0f860b4421a52"
+    sha256 cellar: :any_skip_relocation, sonoma:         "a77c50dcecef83d06fe5c08753f541785df81a29902c89ae08e0a43b2c9d61e9"
+    sha256 cellar: :any_skip_relocation, ventura:        "a77c50dcecef83d06fe5c08753f541785df81a29902c89ae08e0a43b2c9d61e9"
+    sha256 cellar: :any_skip_relocation, monterey:       "a77c50dcecef83d06fe5c08753f541785df81a29902c89ae08e0a43b2c9d61e9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5fd0aeb50ca2652852f54785ef9831aa90a485335ae4d603add08a0b638f0191"
   end
 
   depends_on "go" => :build
