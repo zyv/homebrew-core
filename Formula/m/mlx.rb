1651c68c513202d49380e45ab64e4fe3f78fb97f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-25 21:48:50 +0000

    mlx: update 0.12.0 bottle.

--- Formula/m/mlx.rb
+++ Formula/m/mlx.rb
@@ -7,8 +7,8 @@ class Mlx < Formula
   head "https://github.com/ml-explore/mlx.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_sonoma:  "0e185609327d3f593e7d98217d5aae908549a196be5bb95078a34c78e5a00f8b"
-    sha256 cellar: :any, arm64_ventura: "80338f07f7f6fd1d0f5a261607f1a08dcc1908e0c604e0af08625f2c7f09ba42"
+    sha256 cellar: :any, arm64_sonoma:  "378ecb43f9c7b7c254b0d1700a2421613dd5d500838278a28c9afbe2ee180609"
+    sha256 cellar: :any, arm64_ventura: "55d0b34cbd71ae10537fbc1b213aabc484fe480c95b8fd1889a27dd62c61cee3"
   end
 
   depends_on "cmake" => :build
