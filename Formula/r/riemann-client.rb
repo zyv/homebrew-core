5a39fd64f9594ca9945320c33f8243de745b58cb
Rui Chen <rui@chenrui.dev>
2024-04-26 21:34:07 +0000

    riemann-client: update 2.2.1 bottle.

--- Formula/r/riemann-client.rb
+++ Formula/r/riemann-client.rb
@@ -7,13 +7,13 @@ class RiemannClient < Formula
   head "https://git.madhouse-project.org/algernon/riemann-c-client.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "250573eb7623272bd39a641f8425c54e25166c9d6f00536411199b3519b35d8a"
-    sha256 cellar: :any,                 arm64_ventura:  "c9e0c1d3a35bfe73bc5bc680c907519f23cc10de654c6b9f1020af02133ae834"
-    sha256 cellar: :any,                 arm64_monterey: "b5b3a981e301f9b3a5ff8db896bdb2052b1125a515c7cdcb4d51fd6a2af2829a"
-    sha256 cellar: :any,                 sonoma:         "952af9423fa3e8d590128d983107d5d209717f835f3c27fa9e4575fafccea41f"
-    sha256 cellar: :any,                 ventura:        "27ab072b295b38bebaa72d9cc080739728104df3d1daf591bfeabf6b6f118ba4"
-    sha256 cellar: :any,                 monterey:       "f4a2f5ac6aaf235950f83439d477423bdcb7f0985f9bba8cc58922a4ab59cc5f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "08985d27aec02a506bf4aa2b6e59cfad8412d445cd8d91d59b67b87c017c406f"
+    sha256 cellar: :any,                 arm64_sonoma:   "7854bc14bd566d2dde42dc063099cfc65f32e76b19753d707c6b2d3b9c25c7cd"
+    sha256 cellar: :any,                 arm64_ventura:  "6c8e5b2f98803a91ff957f0052c2f2ba4bbc276fb4116bb244c8179b0f3ac835"
+    sha256 cellar: :any,                 arm64_monterey: "10c6ded8a26a5b556881b2dc045175c0c02fd5221c8fb79c58805f30c01c150d"
+    sha256 cellar: :any,                 sonoma:         "7392770dda4b7a8a85ad01c6fff29755ebba6bf454569a452e0b179031a2ed20"
+    sha256 cellar: :any,                 ventura:        "3cc09e98b21351ff7e33c35664467ffc1c2f7ddec512a3f4fed9aaa2b44b3717"
+    sha256 cellar: :any,                 monterey:       "76fd317852de35fce96522105df18ef93ce9d58105f764c7d25715ebd14fadc3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5aedd6872a9056fc17d3defe1f0602b61653c0b6ebf698df90da0297be259dd4"
   end
 
   depends_on "autoconf" => :build
