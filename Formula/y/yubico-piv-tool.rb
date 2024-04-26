6eba1de66d0ede64b7667747ab2836916dfe390a
Brendan Shanks <bshanks@codeweavers.com>
2024-04-25 22:35:46 -0700

    yubico-piv-tool: Remove pcsc-lite dependency.
    yubico-piv-tool detects macOS and builds against the system
    PCSC.framework, not pcsc-lite.

--- Formula/y/yubico-piv-tool.rb
+++ Formula/y/yubico-piv-tool.rb
@@ -4,6 +4,7 @@ class YubicoPivTool < Formula
   url "https://developers.yubico.com/yubico-piv-tool/Releases/yubico-piv-tool-2.5.1.tar.gz"
   sha256 "4262df01eec5c5ef942be9694db5bceac79f457e94879298a4934f6b5e44ff5f"
   license "BSD-2-Clause"
+  revision 1
 
   livecheck do
     url "https://developers.yubico.com/yubico-piv-tool/Releases/"
@@ -28,11 +29,13 @@ class YubicoPivTool < Formula
   depends_on "pkg-config" => :build
   depends_on "check"
   depends_on "openssl@3"
-  depends_on "pcsc-lite"
+  uses_from_macos "pcsc-lite"
 
   def install
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args, "-DCMAKE_C_FLAGS=-I#{Formula["pcsc-lite"].opt_include}/PCSC"
+      args = []
+      args << "-DCMAKE_C_FLAGS=-I#{Formula["pcsc-lite"].opt_include}/PCSC" unless OS.mac?
+      system "cmake", "..", *std_cmake_args, *args
       system "make", "install"
     end
   end
