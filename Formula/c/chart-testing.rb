c60cabcc577bd0d1b9c93d8c991def82a08475ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-27 12:28:03 +0000

    chart-testing 3.11.0

--- Formula/c/chart-testing.rb
+++ Formula/c/chart-testing.rb
@@ -2,8 +2,8 @@ class ChartTesting < Formula
   desc "Testing and linting Helm charts"
   homepage "https://github.com/helm/chart-testing"
   url "https://github.com/helm/chart-testing.git",
-      tag:      "v3.10.1",
-      revision: "c35d32b568ba7901e00d3386a231ae2b6e1c2efc"
+      tag:      "v3.11.0",
+      revision: "a2ecd82b650c223a8d264920fd0bab40de16b915"
   license "Apache-2.0"
   head "https://github.com/helm/chart-testing.git", branch: "main"
 
