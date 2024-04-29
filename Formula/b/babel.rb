02833b7ac34a35018cdeadad049ff7d918064d81
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-29 21:21:25 +0000

    babel 7.24.5
    babel: babel-cli 7.24.5
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/b/babel.rb
+++ Formula/b/babel.rb
@@ -4,8 +4,8 @@ require "json"
 class Babel < Formula
   desc "Compiler for writing next generation JavaScript"
   homepage "https://babeljs.io/"
-  url "https://registry.npmjs.org/@babel/core/-/core-7.24.4.tgz"
-  sha256 "382298f7bd5af14f1f1edc0abbfe958b09ec6dedc14453303de2e0c4a5d59bf6"
+  url "https://registry.npmjs.org/@babel/core/-/core-7.24.5.tgz"
+  sha256 "61d6fa7b2633d5c6c39aaddd7ea62e7e80e0d34b78633385294f3c6b7234012d"
   license "MIT"
 
   bottle do
@@ -15,8 +15,8 @@ class Babel < Formula
   depends_on "node"
 
   resource "babel-cli" do
-    url "https://registry.npmjs.org/@babel/cli/-/cli-7.24.1.tgz"
-    sha256 "462d441a9ddce9ab79e233647197d151e72a063e7fd2a323cba50c2d2f514884"
+    url "https://registry.npmjs.org/@babel/cli/-/cli-7.24.5.tgz"
+    sha256 "bf1baf99b310455a79108f0beb774ae1c9d900b9f128be6475fd71e35359489e"
   end
 
   def install
