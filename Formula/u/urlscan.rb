cc761ca775d291b9ce8c243f246d1d272cbfb57a
Dustin Rodrigues <dust.rod@gmail.com>
2024-04-29 20:56:01 -0400

    urlscan 1.0.2
    autobump: add urlscan
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/u/urlscan.rb
+++ Formula/u/urlscan.rb
@@ -3,8 +3,8 @@ class Urlscan < Formula
 
   desc "View/select the URLs in an email message or file"
   homepage "https://github.com/firecat53/urlscan"
-  url "https://files.pythonhosted.org/packages/b7/7d/984994a32b261cc3a72d3bfb0b8c0de4f786682128dc659a5c5e02dfc48c/urlscan-1.0.1.tar.gz"
-  sha256 "e0ec986e5aa2da57dd2face8692116d80af173d4eb56a78e4fd881731113307f"
+  url "https://files.pythonhosted.org/packages/27/23/57d2d0a002a77638c2f3196d24d966209f51c498413ac1758d1680a0f96a/urlscan-1.0.2.tar.gz"
+  sha256 "d909ff180588008faba8a6491e3e0821ad3c8a3b6574b94fd73b8fb11ff302f2"
   license "GPL-2.0-or-later"
 
   bottle do
@@ -19,9 +19,19 @@ class Urlscan < Formula
 
   depends_on "python@3.12"
 
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/f6/f3/b827b3ab53b4e3d8513914586dcca61c355fa2ce8252dea4da56e67bf8f2/typing_extensions-4.11.0.tar.gz"
+    sha256 "83f085bd5ca59c80295fc2a82ab5dac679cbe02b9f33f7d83af68e241bea51b0"
+  end
+
   resource "urwid" do
-    url "https://files.pythonhosted.org/packages/58/7e/4191aa9a1c4a7b2f73a7548002754863189217464fbf76045526c7c97be5/urwid-2.4.1.tar.gz"
-    sha256 "6207cfa8ac911f251bbebf4d454a00e622f68bd5cd2c9e55b53c6eac85bb4a6f"
+    url "https://files.pythonhosted.org/packages/ef/fd/77d351caa11c438c7536bba12ea26bb1f22fe7fd0d9aa65849d4625c3e2d/urwid-2.6.11.tar.gz"
+    sha256 "52770007d734d7387ae0421e7b7769c4c5ec67e91a5f4df54e858e314062e475"
+  end
+
+  resource "wcwidth" do
+    url "https://files.pythonhosted.org/packages/6c/63/53559446a878410fc5a5974feb13d31d78d752eb18aeba59c7fef1af7598/wcwidth-0.2.13.tar.gz"
+    sha256 "72ea0c06399eb286d978fdedb6923a9eb47e1c486ce63e9b4e64fc18303972b5"
   end
 
   def install
