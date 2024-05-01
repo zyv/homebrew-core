4d9e0102a34050a17c235368a36919ca02433c99
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-01 18:45:38 +0000

    sdl2 2.30.3

--- Formula/s/sdl2.rb
+++ Formula/s/sdl2.rb
@@ -1,8 +1,8 @@
 class Sdl2 < Formula
   desc "Low-level access to audio, keyboard, mouse, joystick, and graphics"
   homepage "https://www.libsdl.org/"
-  url "https://github.com/libsdl-org/SDL/releases/download/release-2.30.2/SDL2-2.30.2.tar.gz"
-  sha256 "891d66ac8cae51361d3229e3336ebec1c407a8a2a063b61df14f5fdf3ab5ac31"
+  url "https://github.com/libsdl-org/SDL/releases/download/release-2.30.3/SDL2-2.30.3.tar.gz"
+  sha256 "820440072f8f5b50188c1dae104f2ad25984de268785be40c41a099a510f0aec"
   license "Zlib"
 
   livecheck do
