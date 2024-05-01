da69ed6b315a4229f30db3980b816385b6903abd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-01 04:02:01 +0000

    pyright: update 1.1.361 bottle.

--- Formula/p/pyright.rb
+++ Formula/p/pyright.rb
@@ -9,13 +9,13 @@ class Pyright < Formula
   head "https://github.com/microsoft/pyright.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3aa25e4423c9297ddde5c938e6673c8df46f9c70cb853862d56c8037d015f964"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3aa25e4423c9297ddde5c938e6673c8df46f9c70cb853862d56c8037d015f964"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3aa25e4423c9297ddde5c938e6673c8df46f9c70cb853862d56c8037d015f964"
-    sha256 cellar: :any_skip_relocation, sonoma:         "ef830a1bc324f470297ea613b75b08c60955cabd9e3f64e0a857f06d5222e618"
-    sha256 cellar: :any_skip_relocation, ventura:        "ef830a1bc324f470297ea613b75b08c60955cabd9e3f64e0a857f06d5222e618"
-    sha256 cellar: :any_skip_relocation, monterey:       "ef830a1bc324f470297ea613b75b08c60955cabd9e3f64e0a857f06d5222e618"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3aa25e4423c9297ddde5c938e6673c8df46f9c70cb853862d56c8037d015f964"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3a591c6ba7b0a873c9208860a66914a18513363fae1a3857c072b301e872f0fc"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3a591c6ba7b0a873c9208860a66914a18513363fae1a3857c072b301e872f0fc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3a591c6ba7b0a873c9208860a66914a18513363fae1a3857c072b301e872f0fc"
+    sha256 cellar: :any_skip_relocation, sonoma:         "5ccaee2f05684d4c596e7042edf0d0e5bdd8b33c45fb2658f87ea5fd4d1c776b"
+    sha256 cellar: :any_skip_relocation, ventura:        "5ccaee2f05684d4c596e7042edf0d0e5bdd8b33c45fb2658f87ea5fd4d1c776b"
+    sha256 cellar: :any_skip_relocation, monterey:       "5ccaee2f05684d4c596e7042edf0d0e5bdd8b33c45fb2658f87ea5fd4d1c776b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3a591c6ba7b0a873c9208860a66914a18513363fae1a3857c072b301e872f0fc"
   end
 
   depends_on "node"
