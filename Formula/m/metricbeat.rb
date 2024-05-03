1f0cd96c1135d081b183ab1bc739ccb03277966c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-03 09:43:27 +0000

    metricbeat 8.13.3

--- Formula/m/metricbeat.rb
+++ Formula/m/metricbeat.rb
@@ -2,8 +2,8 @@ class Metricbeat < Formula
   desc "Collect metrics from your systems and services"
   homepage "https://www.elastic.co/beats/metricbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.13.2",
-      revision: "d41b4978ea7b4d7c6020b47ffd8a3b8642531fe3"
+      tag:      "v8.13.3",
+      revision: "79b1528b7bfbf5152041db8f4ab497af6afa06e2"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "master"
 
