77e63983469e1abb8f46e3a2a995ad4545821d37
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-04 20:38:14 +0000

    moto: update 5.0.6_1 bottle.

--- Formula/m/moto.rb
+++ Formula/m/moto.rb
@@ -9,13 +9,13 @@ class Moto < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "23b13a80b3e5abd201cea9efb4ad2315f8ef60cc856b612d0d74286f01fbf440"
-    sha256 cellar: :any,                 arm64_ventura:  "e76e3af447c88c1563295c580439cdfa6650fa7bacb06aba8a37b5f24a6d435f"
-    sha256 cellar: :any,                 arm64_monterey: "15a3741fbfa12b47df8e3a220714bc211b305b2192b1d7b3569758f363495306"
-    sha256 cellar: :any,                 sonoma:         "8fa15c2003613a647e8a4b8e8acd98d7994455c067f6f4b272c9ec6bc74c7918"
-    sha256 cellar: :any,                 ventura:        "57fd94764675d5cb792c69ba9d32b4f458358b75ecfcd7fee50547fbc59e0fda"
-    sha256 cellar: :any,                 monterey:       "41b132d202b334519d14e5f56bb7bab1b8dfddfd9ec6c4185f8428c1f8e39ff4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b03f32323d8286a349e9afea5162b3cd7824f66606652df2e3e59a76e64e614"
+    sha256 cellar: :any,                 arm64_sonoma:   "10182beeeae6df4a54ed60d2b21bd6b9760fdfa105d9fd3b1f2e660f972e116b"
+    sha256 cellar: :any,                 arm64_ventura:  "7c1398cc6d02369d8f8dd727adb2534b976d180d2a3bfd40c4e0baef00a2afcf"
+    sha256 cellar: :any,                 arm64_monterey: "74868e3f8e2dc5d2f47c8e983b86fce7bd30620d75e1d32f4d08216b97145e62"
+    sha256 cellar: :any,                 sonoma:         "4ae7174edb816829d5ebfabe57ff75b69a7c46f140902117ebca07c6232ced10"
+    sha256 cellar: :any,                 ventura:        "73520034c695f6815c4701d07c3e8e23c2144f1f7c0876b142be4e3bcb41a69c"
+    sha256 cellar: :any,                 monterey:       "51a7fc36397343eb6549f081b02065e36311fcede17bd5af05afac4dad95d049"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d0c469b673cf9f69c906a57db228f784b060d75f2fb1a85554d47318e64f9921"
   end
 
   depends_on "rust" => :build # for pydantic_core
