518773047fe6e1c869f8ed6a018df7961108405b
Clémentine <clementine@meilisearch.com>
2024-05-06 09:30:53 +0200

    meilisearch 1.8.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/m/meilisearch.rb
+++ Formula/m/meilisearch.rb
@@ -1,8 +1,8 @@
 class Meilisearch < Formula
   desc "Ultra relevant, instant and typo-tolerant full-text search API"
   homepage "https://docs.meilisearch.com/"
-  url "https://github.com/meilisearch/meilisearch/archive/refs/tags/v1.7.6.tar.gz"
-  sha256 "4e3b2ffe10ee4dfadedc61de877c5cc8ff388f87b98ee6d491dd7193011a7e74"
+  url "https://github.com/meilisearch/meilisearch/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "2b2b49d37ace92e6d4c36ffe548c7417c4a563d028b3452cdb42b646af98218e"
   license "MIT"
 
   # There can be a notable gap between when a version is tagged and a
