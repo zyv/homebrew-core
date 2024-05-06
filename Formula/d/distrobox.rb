5594681328c9f56090ca134ff22e49e45fd2a27b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-06 09:27:28 +0000

    distrobox 1.7.2.1

--- Formula/d/distrobox.rb
+++ Formula/d/distrobox.rb
@@ -1,8 +1,8 @@
 class Distrobox < Formula
   desc "Use any Linux distribution inside your terminal"
   homepage "https://distrobox.privatedns.org/"
-  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.7.2.0.tar.gz"
-  sha256 "10d040863ada39f03173210f3f5ca26c405727088e0417ec311c0eca7db82596"
+  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.7.2.1.tar.gz"
+  sha256 "ff2cca0c6334fff6ed577d23f68a6746ad4009f42d8a45eef5ca3850c895a4bb"
   license "GPL-3.0-only"
   head "https://github.com/89luca89/distrobox.git", branch: "main"
 
