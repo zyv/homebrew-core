0ba4cd95a40279867ebc00598d5e39cf7c436e3c
Branch Vincent <branchevincent@gmail.com>
2024-05-05 17:06:55 -0700

    uriparser 0.9.8
    autobump: add uriparser
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/u/uriparser.rb
+++ Formula/u/uriparser.rb
@@ -1,8 +1,8 @@
 class Uriparser < Formula
   desc "URI parsing library (strictly RFC 3986 compliant)"
   homepage "https://uriparser.github.io/"
-  url "https://github.com/uriparser/uriparser/releases/download/uriparser-0.9.7/uriparser-0.9.7.tar.bz2"
-  sha256 "d27dea0c8b6f6fb9798f07caedef1cd96a6e3fc5c6189596774e19afa7ddded7"
+  url "https://github.com/uriparser/uriparser/releases/download/uriparser-0.9.8/uriparser-0.9.8.tar.bz2"
+  sha256 "72d1b559be3a1806f788a3d9be34a1b063d42aa238b29ba4ee633d6effcd33bd"
   license "BSD-3-Clause"
   head "https://github.com/uriparser/uriparser.git", branch: "master"
 
