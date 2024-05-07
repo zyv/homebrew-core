ab2cd6c864c4d966d019d5770d1f0bb36878fb4b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-07 09:05:45 +0000

    adr-viewer: update 1.4.0_1 bottle.

--- Formula/a/adr-viewer.rb
+++ Formula/a/adr-viewer.rb
@@ -9,13 +9,13 @@ class AdrViewer < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e392e7fc0df34aec1d1963699148c10fcfdde6ffa774a21332a4e368dd42aa2c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "49c198fab0064a72a50a9514ce940137db29bdadb9968642bc9cf17e8634409d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "03ac0a603f17c6726880d1bf3d6e4122ed267c87a2e79b47a442650c187dd454"
-    sha256 cellar: :any_skip_relocation, sonoma:         "5b1549d8b0eb3d3488c58ae488fd362ba10e2c561498e2eea37425922e123c27"
-    sha256 cellar: :any_skip_relocation, ventura:        "76f25ebfe50d0cb2d0eccd6fa492439c7e35ac00033a9e92ea8282c97ffdbe4f"
-    sha256 cellar: :any_skip_relocation, monterey:       "7ab7ceed694fa6bd0fd4f36d89cd8beb3efad47c56dfe278b5fa335b43c5cee3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c2acb8fe3eba8f0134da8a08a85dc89392dc393c54a3c1b5f5a51124fdac2fd6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5318d78eabe31c1b114ca32615317b488f6d586c2bcef64af8fc420cd7fbe58e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c61f5cedde30afad943d4e11fed05a6bcd8eef1b8e8aa5b2e68e8810ad185fc5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fba7ede4d8f7dbdccd2712460e79fe364e54c4d5c45c58d364094f1958dcd1f6"
+    sha256 cellar: :any_skip_relocation, sonoma:         "8ee0cf976f32eb79be1b9c81912327e4c56e0ab1f66254ae7ae87069e8c273cf"
+    sha256 cellar: :any_skip_relocation, ventura:        "ebc4aa912ff8f326f96c4802c75687c4eb7406dabf0ace81d4976eda508c75ae"
+    sha256 cellar: :any_skip_relocation, monterey:       "de080d53d892580f4456233e69f165ca715d681fd89b9fcec88cfc192285bece"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "282d480f5200e1be5e355fbd64c28baca9bf64432f09ce04a8a407eb3bd78072"
   end
 
   depends_on "python@3.12"
