72dfee7334a358ceb4c60a11f1e639deb6ba4627
Tiago Krebs <krebstiago@gmail.com>
2024-05-08 16:56:31 +0000

    azion 1.25.0

--- Formula/a/azion.rb
+++ Formula/a/azion.rb
@@ -1,8 +1,8 @@
 class Azion < Formula
   desc "CLI for the Azion service"
   homepage "https://github.com/aziontech/azion"
-  url "https://github.com/aziontech/azion/archive/refs/tags/1.24.0.tar.gz"
-  sha256 "e25152162139740f7ad4cce82d9666c677b9a0c57c68212ee9a8acf0df277225"
+  url "https://github.com/aziontech/azion/archive/refs/tags/1.25.0.tar.gz"
+  sha256 "c81b9937dac9b478032f17d9fe238ec7bf92865bfe885cbf343ef5c1e54cd26d"
   license "MIT"
 
   bottle do
