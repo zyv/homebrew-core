3f4d5fd1010dd9308313b31d0d365a46f53b9699
Francois-Xavier Coudert <fxcoudert@gmail.com>
2024-05-09 12:01:40 +0200

    riscv64-elf-gcc 14.1.0

--- Formula/r/riscv64-elf-gcc.rb
+++ Formula/r/riscv64-elf-gcc.rb
@@ -1,9 +1,9 @@
 class Riscv64ElfGcc < Formula
   desc "GNU compiler collection for riscv64-elf"
   homepage "https://gcc.gnu.org"
-  url "https://ftp.gnu.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gcc/gcc-13.2.0/gcc-13.2.0.tar.xz"
-  sha256 "e275e76442a6067341a27f04c5c6b83d8613144004c0413528863dc6b5c743da"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-14.1.0/gcc-14.1.0.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-14.1.0/gcc-14.1.0.tar.xz"
+  sha256 "e283c654987afe3de9d8080bc0bd79534b5ca0d681a73a11ff2b5d3767426840"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
 
   livecheck do
@@ -26,14 +26,6 @@ class Riscv64ElfGcc < Formula
   depends_on "libmpc"
   depends_on "mpfr"
   depends_on "riscv64-elf-binutils"
-
-  # Fixes std::log2 import from math.h on Big Sur and Monterey.
-  # Already included upstream for next release. Remove on next release.
-  # gcc/config/riscv/genrvv-type-indexer.cc:118:30: error: no member named 'log2' in namespace 'std';
-  patch do
-    url "https://gcc.gnu.org/git/?p=gcc.git;a=patch;h=87c347c2897537a6aa391efbfc5ed00c625434fe"
-    sha256 "470f9cd51f0ad5d6b7b8dc080f3d4830a8ae640257ed6fccc61bd46287798eb0"
-  end
 
   def install
     target = "riscv64-elf"
