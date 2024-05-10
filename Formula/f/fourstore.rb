c33529f8c63a8e7b14a3cfc8ca8e62f8690d4d58
Rui Chen <rui@chenrui.dev>
2024-05-10 10:24:36 -0400

    fourstore 1.1.7
    fourstore: remove build patch and update license
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    autobump: add fourstore
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/fourstore.rb
+++ Formula/f/fourstore.rb
@@ -2,10 +2,9 @@ class Fourstore < Formula
   desc "Efficient, stable RDF database"
   homepage "https://github.com/4store/4store"
   # NOTE: Try building without `avahi` at version bump.
-  url "https://github.com/4store/4store/archive/refs/tags/v1.1.6.tar.gz"
-  sha256 "a0c8143fcceeb2f1c7f266425bb6b0581279129b86fdd10383bf1c1e1cab8e00"
-  license "GPL-3.0"
-  revision 2
+  url "https://github.com/4store/4store/archive/refs/tags/v1.1.7.tar.gz"
+  sha256 "e511f1adb094e2506545d4773a6005a462f6b4532731e91f1115b038ab25a8f0"
+  license "GPL-3.0-or-later"
 
   bottle do
     sha256 arm64_sonoma:   "b5595483be41d447472e43bedb18b3ca3a04ce88e42f50ee0841be09b27a11b3"
@@ -32,34 +31,23 @@ class Fourstore < Formula
   depends_on "rasqal"
   depends_on "readline"
 
-  # TODO: Check if this dependency can be removed at version bump.
-  on_linux do
-    depends_on "avahi"
-  end
-
-  # Fix build on Linux. Remove on the next release
-  # sigval_t.h:13:3: error: #error "sigval_t defined for standard not including union sigval"
-  patch do
-    on_linux do
-      url "https://github.com/4store/4store/commit/c5a56d7c7504551a1f2fff6c16c4d9a440e4a317.patch?full_index=1"
-      sha256 "f476aa38098aaaebf970603a5003aae40fa43f3243890e404f799be8bdbf0aad"
-    end
-  end
-
   def install
     # Work around failure from GCC 10+ using default of `-fno-common`
     # /usr/bin/ld: query.o:(.bss+0x0): multiple definition of `rasqal_mutex'
     ENV.append_to_cflags "-fcommon" if OS.linux?
-    # Upstream issue garlik/4store#138
+    # Upstream issue https://github.com/4store/4store/issues/138
     # Otherwise .git directory is needed
-    (buildpath/".version").write("v1.1.6")
+    (buildpath/".version").write version.to_s
 
     system "./autogen.sh"
-    (var/"fourstore").mkpath
     system "./configure", "--prefix=#{prefix}",
                           "--with-storage-path=#{var}/fourstore",
                           "--sysconfdir=#{etc}/fourstore"
     system "make", "install"
+  end
+
+  def post_install
+    (var/"fourstore").mkpath
   end
 
   def caveats
