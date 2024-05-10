a2b7fa3ffc7b835bfbc67ece673d6e187f42f315
Rui Chen <rui@chenrui.dev>
2024-05-10 10:23:08 -0400

    threadweaver 6.2
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/threadweaver.rb
+++ Formula/t/threadweaver.rb
@@ -1,8 +1,8 @@
 class Threadweaver < Formula
   desc "Helper for multithreaded programming"
   homepage "https://api.kde.org/frameworks/threadweaver/html/index.html"
-  url "https://download.kde.org/stable/frameworks/6.1/threadweaver-6.1.0.tar.xz"
-  sha256 "dda5d5508d61707eb4cbd044f371e34480ff2f44381adc4cd1b703cf4e458dc8"
+  url "https://download.kde.org/stable/frameworks/6.2/threadweaver-6.2.0.tar.xz"
+  sha256 "e74de2df0bb50148acc8a6161f9809991ae4b2334b58a9ee092ba1d827044337"
   license "LGPL-2.0-or-later"
   head "https://invent.kde.org/frameworks/threadweaver.git", branch: "master"
 
