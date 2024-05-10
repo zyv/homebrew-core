1342443d8cdc367283b44a060c182350b67cb7b8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-10 14:15:21 +0000

    riscv64-elf-gcc: update 14.1.0 bottle.

--- Formula/r/riscv64-elf-gcc.rb
+++ Formula/r/riscv64-elf-gcc.rb
@@ -11,15 +11,13 @@ class Riscv64ElfGcc < Formula
   end
 
   bottle do
-    sha256 arm64_sonoma:   "df674d0488ecc229a6acf4f6dac6a60c89699fb9569515c5cec6c7d3aea2b3c3"
-    sha256 arm64_ventura:  "010cebe94fb852041b6525bb26ecf18da50a2b31ecb2ceea10fa98227268840e"
-    sha256 arm64_monterey: "9c4199cbf3a55f3d188ab4ec3d5db6ce928290aabc4f5f53989194b7b8572c0d"
-    sha256 arm64_big_sur:  "a291ee8500e3560ac26e38fafb8308aceddcbcf1eaf65b7e8eee887878d4eccc"
-    sha256 sonoma:         "529f87bec9d1aa5ff2e47cdae009eec581acb298f0e2ac2dcc5697196d60b6ca"
-    sha256 ventura:        "06f5dc90759c05d5bc014a21db08baede43cbb692432a62dfa2fe20f21497b33"
-    sha256 monterey:       "73bef350abe7f078fe0d4b63f8ae5638d39e195087f42ca5ddb07ad416273b79"
-    sha256 big_sur:        "4adfdb6b0912649db5994bae962666899d7dcfaa22e0def62df966d9b54d24a4"
-    sha256 x86_64_linux:   "d4da1645f7d46091e033e896b50287702deb160a41378cb115d50d0fbe8e7e61"
+    sha256 arm64_sonoma:   "1818ab9fff8bdb3433f8a14e8837eeb53c51d210caaf1225c8a070c00d4efd8f"
+    sha256 arm64_ventura:  "92c258a63f964eda0477351f83960b13a28aa6a8fdd64368c7dc399971c09a18"
+    sha256 arm64_monterey: "e97c6dbd93fd8ddd1212ca992a481a34ed9593c2c41564a4480fa257a43ddd2d"
+    sha256 sonoma:         "7d8fadfac69d5fc0a9eb3118a6ab26eda4dfb3a1764a481b68f3425f7038dbaf"
+    sha256 ventura:        "f48e9911238997a077b4ed18fea24d8f66121fac6974169546244c702d9a8d19"
+    sha256 monterey:       "9539f61abd5e7984623bf62194f769dc3734ec68b27d12c89b70458107971c0a"
+    sha256 x86_64_linux:   "1dc95e8418bd63f2736cf0b3a47b83ac98f2c993fc8567b1cda47b157ad0e309"
   end
 
   depends_on "gmp"
