172a462436e41c77d4cb26d4bfbba3fd55cee358
Rui Chen <rui@chenrui.dev>
2024-05-11 17:20:27 -0400

    tkdiff 5.7
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    tkdiff: switch to use brew tcl-tk (8.6.14)
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/tkdiff.rb
+++ Formula/t/tkdiff.rb
@@ -1,9 +1,9 @@
 class Tkdiff < Formula
   desc "Graphical side by side diff utility"
   homepage "https://tkdiff.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/5.6/tkdiff-5-6.zip"
-  version "5.6"
-  sha256 "628c7541d486996b6f984f4f702cec7cb3a7fd959d3e87104192bc3e946620bc"
+  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/5.7/tkdiff-5-7.zip"
+  version "5.7"
+  sha256 "e2dec98e4c2f7c79a1e31290d3deaaa5915f53c8220c05728f282336bb2e405d"
   license "GPL-2.0-only"
 
   livecheck do
@@ -15,7 +15,8 @@ class Tkdiff < Formula
     sha256 cellar: :any_skip_relocation, all: "fc433422fcd956c6826fca97e0dc7c8ce6a0e9c4cd3c7d17be9e15bca261c6e8"
   end
 
-  uses_from_macos "tcl-tk", since: :monterey
+  # upstream bug report about running with system tcl-tk, https://sourceforge.net/p/tkdiff/bugs/98/
+  depends_on "tcl-tk"
 
   def install
     bin.install "tkdiff"
