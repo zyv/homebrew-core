8e11dade04c2488199e020407abed5c14fcb9199
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-11 18:35:32 +0000

    goreleaser 1.26.0

--- Formula/g/goreleaser.rb
+++ Formula/g/goreleaser.rb
@@ -2,8 +2,8 @@ class Goreleaser < Formula
   desc "Deliver Go binaries as fast and easily as possible"
   homepage "https://goreleaser.com/"
   url "https://github.com/goreleaser/goreleaser.git",
-      tag:      "v1.25.1",
-      revision: "6a7a9ba3995728a4bdf7b5fff770d8caeb3d2cc8"
+      tag:      "v1.26.0",
+      revision: "0481e63fb3d9d6d1e1d8cbee16789d644b652ab1"
   license "MIT"
   head "https://github.com/goreleaser/goreleaser.git", branch: "main"
 
