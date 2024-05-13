c58de169635fbfdb61d18b9daf94af12e66a3a7f
Zxilly <31370133+Zxilly@users.noreply.github.com>
2024-05-13 20:12:23 +0000

    go-size-analyzer 1.0.4 (new formula)
    follow style requirement
    
    update args position
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    
    add basic test
    
    update test to handle error case
    
    use tarball
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    
    update test
    
    add more test case
    
    test also relies golang

---
+++ Formula/g/go-size-analyzer.rb
@@ -0,0 +1,51 @@
+class GoSizeAnalyzer < Formula
+  desc "Analyzing the dependencies in compiled Golang binaries"
+  homepage "https://github.com/Zxilly/go-size-analyzer"
+  url "https://github.com/Zxilly/go-size-analyzer/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "c92ebee6298fc1c6b25e5ea2ed0324abd047ef0ac3dc80b110030562e93f89ae"
+  license "AGPL-3.0-only"
+  head "https://github.com/Zxilly/go-size-analyzer.git", branch: "master"
+
+  depends_on "go" => [:build, :test]
+  depends_on "node" => :build
+  depends_on "pnpm" => :build
+
+  def install
+    system "pnpm", "--dir", "ui", "install"
+    system "pnpm", "--dir", "ui", "build"
+
+    mv "ui/dist/index.html", "internal/webui/index.html"
+
+    ldflags = %W[
+      -s -w
+      -X github.com/Zxilly/go-size-analyzer.version=#{version}
+      -X github.com/Zxilly/go-size-analyzer.buildDate=#{Time.now.iso8601}
+      -X github.com/Zxilly/go-size-analyzer.dirtyBuild=false
+    ]
+
+    system "go", "build", *std_go_args(ldflags:, output: bin/"gsa"), "-tags", "embed", "./cmd/gsa"
+  end
+
+  test do
+    assert_includes shell_output("#{bin}/gsa --version"), version
+
+    assert_includes shell_output("#{bin}/gsa invalid", 1), "Usage"
+
+    (testpath/"hello.go").write <<~EOS
+      package main
+
+      import "fmt"
+
+      func main() {
+        fmt.Println("Hello, World")
+      }
+    EOS
+
+    system "go", "build", "-o", testpath/"hello", testpath/"hello.go"
+
+    output = shell_output("#{bin}/gsa #{testpath}/hello")
+
+    assert_includes output, "runtime"
+    assert_includes output, "main"
+  end
+end
