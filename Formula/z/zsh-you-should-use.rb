c31f23ded1c14669cf84c1055ed66e5cbff3566d
Rui Chen <rui@chenrui.dev>
2024-05-13 13:18:17 -0400

    zsh-you-should-use 1.7.4
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    zsh-you-should-use: need zsh for linux build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/z/zsh-you-should-use.rb
+++ Formula/z/zsh-you-should-use.rb
@@ -1,13 +1,15 @@
 class ZshYouShouldUse < Formula
   desc "ZSH plugin that reminds you to use existing aliases for commands you just typed"
   homepage "https://github.com/MichaelAquilina/zsh-you-should-use"
-  url "https://github.com/MichaelAquilina/zsh-you-should-use/archive/refs/tags/1.7.3.tar.gz"
-  sha256 "db4486cd12974332ec858d446aff9393dae6be430d425a56d7036d2ce4edeb9e"
+  url "https://github.com/MichaelAquilina/zsh-you-should-use/archive/refs/tags/1.7.4.tar.gz"
+  sha256 "2db370cca3e11a55838961948e85e8887ba9a4dbe0e1d02abb0c8cd18ea5ba88"
   license "GPL-3.0-or-later"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "559c5067d040915063806f345fbf5470c5508766bb91208a92d2038b6d683f3b"
   end
+
+  uses_from_macos "zsh"
 
   def install
     pkgshare.install "you-should-use.plugin.zsh"
