27d277ef04ad3cba667c7fe04ee8a4bfec6a52b8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-13 18:41:39 +0000

    minikube 1.33.1

--- Formula/m/minikube.rb
+++ Formula/m/minikube.rb
@@ -2,8 +2,8 @@ class Minikube < Formula
   desc "Run a Kubernetes cluster locally"
   homepage "https://minikube.sigs.k8s.io/"
   url "https://github.com/kubernetes/minikube.git",
-      tag:      "v1.33.0",
-      revision: "86fc9d54fca63f295d8737c8eacdbb7987e89c67"
+      tag:      "v1.33.1",
+      revision: "248d1ec5b3f9be5569977749a725f47b018078ff"
   license "Apache-2.0"
   head "https://github.com/kubernetes/minikube.git", branch: "master"
 
