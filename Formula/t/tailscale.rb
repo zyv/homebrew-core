d5a52760ade30285a95f938ca90bb9f4fb13a327
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-14 21:56:16 +0000

    tailscale 1.66.3

--- Formula/t/tailscale.rb
+++ Formula/t/tailscale.rb
@@ -2,8 +2,8 @@ class Tailscale < Formula
   desc "Easiest, most secure way to use WireGuard and 2FA"
   homepage "https://tailscale.com"
   url "https://github.com/tailscale/tailscale.git",
-      tag:      "v1.66.1",
-      revision: "88e23b6cfd4b9a9cad5ce858b73ece1f07970fee"
+      tag:      "v1.66.3",
+      revision: "eae73f821381ce653468ba3c4eec3453c0808953"
   license "BSD-3-Clause"
 
   livecheck do
