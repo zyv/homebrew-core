698ad2b8bdb4e6d40e3806bc663781e4019e5c28
Tilt Dev <it@tilt.dev>
2024-05-15 16:33:14 +0000

    tilt 0.33.14

--- Formula/t/tilt.rb
+++ Formula/t/tilt.rb
@@ -2,8 +2,8 @@ class Tilt < Formula
   desc "Define your dev environment as code. For microservice apps on Kubernetes"
   homepage "https://tilt.dev/"
   url "https://github.com/tilt-dev/tilt.git",
-    tag:      "v0.33.13",
-    revision: "050162248092ae90d3aa6a9ba8a62f9cb9311bac"
+    tag:      "v0.33.14",
+    revision: "df9cf55f841dce02874f8f6992f96bd6908432a8"
   license "Apache-2.0"
   head "https://github.com/tilt-dev/tilt.git", branch: "master"
 
