950d47567a80d568c8df6ee02199ab5b15def597
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-15 04:06:50 +0000

    xdot: update 1.4 bottle.

--- Formula/x/xdot.rb
+++ Formula/x/xdot.rb
@@ -9,14 +9,13 @@ class Xdot < Formula
   head "https://github.com/jrfonseca/xdot.py.git", branch: "master"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a5fa743c1cd886c911a0b7be7e0c0ce047cb2137456dffd781df01a70bec5139"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a5fa743c1cd886c911a0b7be7e0c0ce047cb2137456dffd781df01a70bec5139"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a5fa743c1cd886c911a0b7be7e0c0ce047cb2137456dffd781df01a70bec5139"
-    sha256 cellar: :any_skip_relocation, sonoma:         "6388495e914b6a72dc929489a99f638305c3bbc1a3d9afe7f8e825dc844345e5"
-    sha256 cellar: :any_skip_relocation, ventura:        "6388495e914b6a72dc929489a99f638305c3bbc1a3d9afe7f8e825dc844345e5"
-    sha256 cellar: :any_skip_relocation, monterey:       "6388495e914b6a72dc929489a99f638305c3bbc1a3d9afe7f8e825dc844345e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "94dafcfccfdbb31a72bfbdd8840569763505d54610b024969e2b2cffa5fe8898"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "fea41f59e6cf3192c8d6bc352f1d6f219672ec3815b0317c4c86a78eae14e2dd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "674284613775b93ddca70f21cc26ccbc195c24d50c2f57ce2a489dbbc661a132"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0b997eba3b1a3b41a6a15e64a0728cf6f43d59ef0e62d95c227199ce4c24c461"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ca942ec49815794f7a49442c754a7ce193d1702c2002297e869f6f8dcb3f8362"
+    sha256 cellar: :any_skip_relocation, ventura:        "6d8747eb0f97917daf9c1c46d02c1348c4e4e7586d7dce7e99caf72eba057243"
+    sha256 cellar: :any_skip_relocation, monterey:       "c479f17574ea512de3a1a152ab8423fcd95f0c2c93032233363750240b79bc12"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7d4ee5d0df84d35ecda8615029d5ad07067f9f8984b536750f3de9ab8a3ec47b"
   end
 
   depends_on "adwaita-icon-theme"
