79bccc8cf6589368ddeaa994bcac84338a58e702
Rui Chen <rui@chenrui.dev>
2024-05-15 19:15:10 -0400

    nzbget 24.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    nzbget: update dep for zlib
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/n/nzbget.rb
+++ Formula/n/nzbget.rb
@@ -1,8 +1,8 @@
 class Nzbget < Formula
   desc "Binary newsgrabber for nzb files"
   homepage "https://nzbget.com"
-  url "https://github.com/nzbgetcom/nzbget/archive/refs/tags/v24.0.tar.gz"
-  sha256 "f8b66551b943f72442a0fb00d8872a0e9c92c829e63d6a74c35888b7cb658dca"
+  url "https://github.com/nzbgetcom/nzbget/archive/refs/tags/v24.1.tar.gz"
+  sha256 "9b823f5be183573cc51a7e4db5ac0529f5e3b98260676a7ce400784fdf230393"
   license "GPL-2.0-or-later"
   head "https://github.com/nzbgetcom/nzbget.git", branch: "develop"
 
@@ -23,6 +23,7 @@ class Nzbget < Formula
 
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
+  uses_from_macos "zlib"
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
