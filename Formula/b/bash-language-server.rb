9bf5d0420155cde64ae1fa155f2ecc516d1bbec8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-15 17:51:06 +0000

    bash-language-server: update 5.3.3 bottle.

--- Formula/b/bash-language-server.rb
+++ Formula/b/bash-language-server.rb
@@ -8,13 +8,13 @@ class BashLanguageServer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e719a0f5cd83250621dc2b8662a0e524573b416fc77386cde887b703be0ff0ce"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "15542a7e01f66b77eaf2094d3bf7ad30343652cca9781cef9d87567f790a5fcb"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "adf002bc2947ddf104dcb48f4abeee8b78fdade3e6331e1f4c858fe388d07d64"
-    sha256 cellar: :any_skip_relocation, sonoma:         "2114cf9cb1085469a492a603cb1af9aac87c4c8bc0a3a82af26e1b8561edfd6c"
-    sha256 cellar: :any_skip_relocation, ventura:        "f09cd486425d9d47ecee193ceda1e5bb98526631b3fd7d3a4296f6aa19f3dbff"
-    sha256 cellar: :any_skip_relocation, monterey:       "7867f6f561eeedee4501cc727a3e06def5dd90fb846b17ad2b217fab57df0578"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2e3fbd85686b86f49fdbbcc3e2d6539a1b90041630239cbf2bf31c2761ff39eb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b2ffc008f0f95a0efdb2f5f5e1580406e30af4a450f3fbda814991d78d100326"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a9949e6bcdcf9951ee51a6f878db3dfed44d3af74c3bcd9eb1c090693df17373"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b104dfeaef93a5bdd62a42759fbe44cf9bc48f300b78850ad7f47650611b523b"
+    sha256 cellar: :any_skip_relocation, sonoma:         "69d69575ebb9ee49f8ff5a3275bb2bbc710757fa7d9ba4c7632bc38fc9da9c26"
+    sha256 cellar: :any_skip_relocation, ventura:        "bf2044c22effacf605a2f5e5f6652bc93e5aa8f05e8cd7cec3c025f0e9bb5484"
+    sha256 cellar: :any_skip_relocation, monterey:       "53f2adf982c27f94304e5df3183dfd2e0ad0f251760cc50a5a0d764fdb657035"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "34a8b6eaea2b65683d9eca55ffc3ae87e8e06f128ff731a43ad26f6af756759a"
   end
 
   depends_on "node"
