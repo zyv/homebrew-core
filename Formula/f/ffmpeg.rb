df048543794ed381bcd5986bfda18d8309c6eef9
Bevan Kay <email@bevankay.me>
2024-05-16 00:25:28 +1000

    ffmpeg: add linked indirect dependencies

--- Formula/f/ffmpeg.rb
+++ Formula/f/ffmpeg.rb
@@ -45,6 +45,8 @@ class Ffmpeg < Formula
   depends_on "libvmaf"
   depends_on "libvorbis"
   depends_on "libvpx"
+  depends_on "libx11"
+  depends_on "libxcb"
   depends_on "opencore-amr"
   depends_on "openjpeg"
   depends_on "opus"
@@ -69,8 +71,15 @@ class Ffmpeg < Formula
   uses_from_macos "libxml2"
   uses_from_macos "zlib"
 
+  on_macos do
+    depends_on "libarchive"
+    depends_on "libogg"
+    depends_on "libsamplerate"
+  end
+
   on_linux do
     depends_on "alsa-lib"
+    depends_on "libxext"
     depends_on "libxv"
   end
 
