247253fc7668351ee5e54a05e8e0d1d09a0c8e41
Niklas Wagner <Skaro@Skaronator.com>
2024-05-17 17:27:24 +0200

    kubectl-cnpg 1.23.1 (new formula)

---
+++ Formula/k/kubectl-cnpg.rb
@@ -0,0 +1,26 @@
+class KubectlCnpg < Formula
+  desc "CloudNativePG plugin for kubectl"
+  homepage "https://cloudnative-pg.io/"
+  url "https://github.com/cloudnative-pg/cloudnative-pg.git",
+      tag:      "v1.23.1",
+      revision: "336ddf5308fe0d5cf78c4da1d03959fa02a60c70"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/cloudnative-pg/cloudnative-pg/pkg/versions.buildVersion=#{version}
+      -X github.com/cloudnative-pg/cloudnative-pg/pkg/versions.buildCommit=#{Utils.git_head}
+      -X github.com/cloudnative-pg/cloudnative-pg/pkg/versions.buildDate=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/kubectl-cnpg"
+    generate_completions_from_executable(bin/"kubectl-cnpg", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/kubectl-cnpg version")
+    assert_match "connect: connection refused", shell_output("#{bin}/kubectl-cnpg status dummy-cluster 2>&1", 1)
+  end
+end
