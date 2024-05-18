95737bc8077818855802538f4485006dc74b2881
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-18 12:44:57 +0000

    mariadb@11.0 11.0.6
    mariadb@11.0: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    mariadb@11.0: declare indirect deps with linkage
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mariadb@11.0.rb
+++ Formula/m/mariadb@11.0.rb
@@ -1,8 +1,8 @@
 class MariadbAT110 < Formula
   desc "Drop-in replacement for MySQL"
   homepage "https://mariadb.org/"
-  url "https://archive.mariadb.org/mariadb-11.0.5/source/mariadb-11.0.5.tar.gz"
-  sha256 "94fb34a6c161cbbc4365073f45e94ce32a8c2c974be395ec7b587ed3d794910b"
+  url "https://archive.mariadb.org/mariadb-11.0.6/source/mariadb-11.0.6.tar.gz"
+  sha256 "0189d62946c37c6db46bf1a468ba9580bcba8e80f05958ec483c3387eccf9a00"
   license "GPL-2.0-only"
 
   livecheck do
@@ -38,11 +38,15 @@ class MariadbAT110 < Formula
   depends_on "fmt" => :build
   depends_on "pkg-config" => :build
   depends_on "groonga"
+  depends_on "lz4"
   depends_on "openssl@3"
   depends_on "pcre2"
+  depends_on "xz"
   depends_on "zstd"
 
   uses_from_macos "bzip2"
+  uses_from_macos "krb5"
+  uses_from_macos "libedit"
   uses_from_macos "libxcrypt"
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
@@ -54,13 +58,6 @@ class MariadbAT110 < Formula
   end
 
   fails_with gcc: "5"
-
-  # upstream patch ref, https://github.com/MariaDB/server/pull/3064
-  # remove when it got merged and released
-  patch do
-    url "https://github.com/MariaDB/server/commit/3624a36aed0346380255b141cb8a59998aaca4ee.patch?full_index=1"
-    sha256 "c9d0aa64b34c43ac9e3077d74c18532125c459d9d867ade69ce283d27b595b22"
-  end
 
   def install
     ENV.cxx11
