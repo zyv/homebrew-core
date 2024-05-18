905bb19d6f46c4f61e3793178c95165cc1c5d21b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-18 06:52:46 +0000

    svt-av1 2.1.0
    svt-av1: change assembler
    
    svt-av1: add patch from upstream

--- Formula/s/svt-av1.rb
+++ Formula/s/svt-av1.rb
@@ -1,8 +1,8 @@
 class SvtAv1 < Formula
   desc "AV1 encoder"
   homepage "https://gitlab.com/AOMediaCodec/SVT-AV1"
-  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v2.0.0/SVT-AV1-v2.0.0.tar.bz2"
-  sha256 "f9c076c377e504be15e195db8dd36d91233bc37cb8e82530382f38bc1926df02"
+  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v2.1.0/SVT-AV1-v2.1.0.tar.bz2"
+  sha256 "2bfd098770bba185cd1ced8e1ff389837e3dca0d8b5cfb0d97c925a61dbbf955"
   license "BSD-3-Clause"
   head "https://gitlab.com/AOMediaCodec/SVT-AV1.git", branch: "master"
 
@@ -17,14 +17,18 @@ class SvtAv1 < Formula
   end
 
   depends_on "cmake" => :build
-
-  on_intel do
-    depends_on "yasm" => :build
-  end
+  depends_on "nasm" => :build
 
   resource "homebrew-testvideo" do
     url "https://github.com/grusell/svt-av1-homebrew-testdata/raw/main/video_64x64_yuv420p_25frames.yuv"
     sha256 "0c5cc90b079d0d9c1ded1376357d23a9782a704a83e01731f50ccd162e246492"
+  end
+
+  # Remove patch in next release.
+  # Upstream PR: https://gitlab.com/AOMediaCodec/SVT-AV1/-/merge_requests/2240
+  patch do
+    url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/commit/c0c4e12d5a50dfce0e53e375492b4280911b2fe6.diff"
+    sha256 "7c496363dc5380335fb2d7750a9acfe7146201ae2f0c97d1cb5a0cb6bc01bfbe"
   end
 
   def install
