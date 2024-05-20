71e7c301d2c61ae28e9a0df96dacc04bbc61e155
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-20 21:31:29 +0000

    iniparser 4.2.2
    iniparser: rewrite formula
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/i/iniparser.rb
+++ Formula/i/iniparser.rb
@@ -1,10 +1,10 @@
 class Iniparser < Formula
   desc "Library for parsing ini files"
-  homepage "https://github.com/ndevilla/iniparser"
-  url "https://github.com/ndevilla/iniparser/archive/refs/tags/v4.2.1.tar.gz"
-  sha256 "9120fd13260be1dbec74b8aaf47777c434976626f3b3288c0d17b70e21cce2d2"
+  homepage "https://gitlab.com/iniparser/iniparser"
+  url "https://gitlab.com/iniparser/iniparser/-/archive/v4.2.2/iniparser-v4.2.2.tar.bz2"
+  sha256 "2e0e448377c5ff69f809160824a7af60846ddf6055d19a96c269b9682aea761e"
   license "MIT"
-  head "https://github.com/ndevilla/iniparser.git", branch: "master"
+  head "https://gitlab.com/iniparser/iniparser.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5bd9e7fd492164e6e74b2eb9b3ac322f13b0a383b55ac5c7d60ae20063093367"
@@ -16,12 +16,44 @@ class Iniparser < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c601ef4b4010ba20349b8a3098894b2fc1f6303cc6c126b3786e395584fe1a24"
   end
 
+  depends_on "cmake" => :build
+  depends_on "doxygen" => :build
+
   conflicts_with "fastbit", because: "both install `include/dictionary.h`"
 
   def install
-    # Only make the *.a file; the *.so target is useless (and fails).
-    system "make", "libiniparser.a", "CC=#{ENV.cc}", "RANLIB=ranlib"
-    lib.install "libiniparser.a"
-    include.install Dir["src/*.h"]
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    test_config = testpath/"test.ini"
+    test_config.write <<~EOS
+      [section]
+      key = value
+    EOS
+
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <string.h>
+      #include <iniparser/iniparser.h>
+
+      int main() {
+        dictionary *ini;
+        ini = iniparser_load("#{test_config}");
+        const char *value = iniparser_getstring(ini, "section:key", NULL);
+        if (value == NULL || strcmp(value, "value") != 0) {
+          fprintf(stderr, "value not found or incorrect\\n");
+          return 1;
+        }
+        printf("Parsed value: %s", value);
+        iniparser_freedict(ini);
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-o", "test", "-L#{lib}", "-liniparser"
+    assert_equal "Parsed value: value", shell_output("./test")
   end
 end
