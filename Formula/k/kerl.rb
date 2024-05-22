799450569de33806b55a5de45b2f817a2a370467
Rui Chen <rui@chenrui.dev>
2024-05-21 22:51:46 -0400

    kerl 4.1.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/k/kerl.rb
+++ Formula/k/kerl.rb
@@ -1,8 +1,8 @@
 class Kerl < Formula
   desc "Easy building and installing of Erlang/OTP instances"
   homepage "https://github.com/kerl/kerl"
-  url "https://github.com/kerl/kerl/archive/refs/tags/4.1.0.tar.gz"
-  sha256 "5b754ba985f04125dc6824dbdab966f734615db6ec57cb544b4eff5d7f4c059d"
+  url "https://github.com/kerl/kerl/archive/refs/tags/4.1.1.tar.gz"
+  sha256 "b30d3e68f6af3cd2b51b9556eecf9203c4509ac5e3b1a62985b3df0309e04752"
   license "MIT"
   head "https://github.com/kerl/kerl.git", branch: "master"
 
@@ -12,11 +12,12 @@ class Kerl < Formula
 
   def install
     bin.install "kerl"
+
     bash_completion.install "bash_completion/kerl"
     zsh_completion.install "zsh_completion/_kerl"
   end
 
   test do
-    system "#{bin}/kerl", "list", "releases"
+    system bin/"kerl", "list", "releases"
   end
 end
