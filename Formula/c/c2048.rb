5e0839457b4647a0e43c2538dc283ec9758345ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-22 06:36:56 +0000

    c2048: update 1.0.1 bottle.

--- Formula/c/c2048.rb
+++ Formula/c/c2048.rb
@@ -7,13 +7,13 @@ class C2048 < Formula
   head "https://github.com/mevdschee/2048.c.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "962c09271158f054500831c31e2ae086a822c11e7413e4bc418a2ba381beb571"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "06c804d9e463608954c0ffb2e2050981804e0de735102b148fab60eb3f91a187"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3b5beb2e4022e20bd1c70bb87eee3eff84e2d61f29fcd5bd680bf718ef61f25d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "1484849f518d784dfb375383e8990820ade07f1d16967f0de5f01f1b538111aa"
-    sha256 cellar: :any_skip_relocation, ventura:        "b2a51f432958efe2e681cb6637a32179e8389b9edae4fec6e468bf90a073f8ec"
-    sha256 cellar: :any_skip_relocation, monterey:       "db92512deb0a3d3caa3ceef23494835eba84c6cee98b7c84cc09a3f3a097c0a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "378375cf53b4223cec906c0d00e9eb5c424bd8b8b75cb021bcbcd8bd2a35bfa1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "70383d07768ee266589fb68153f475d2a532a52d65c9b4735bb591e595247756"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6a00b75193488ac82165f59f6c8cd5999436a1fad98a21302a2f72fc2a363cff"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9c2e0fda330d41c94bd5ce41e82e04d9e897a4e1fecc8d7951dca3fccabeff0b"
+    sha256 cellar: :any_skip_relocation, sonoma:         "d88c36a1c245aa7c108c9bed1389c4762dbf0e9325a891ecc3c7aeece155dbf7"
+    sha256 cellar: :any_skip_relocation, ventura:        "119185f87ee1a7368b93b6a081b21d4e84e10bcf080deab904d76d90d4bd890f"
+    sha256 cellar: :any_skip_relocation, monterey:       "f9b5374da1d1e2926e5c734632570d9a217d5d20ddaceef28171fef326cc285a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6863918a25b7da026077d19d297b4c6e54185d1ed790027de0c44ae6566baef2"
   end
 
   def install
