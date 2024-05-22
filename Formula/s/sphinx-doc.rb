a0647e25526b7722a4cc2d4a6514de491f5f275b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-22 19:38:40 +0000

    sphinx-doc: update 7.3.7_2 bottle.

--- Formula/s/sphinx-doc.rb
+++ Formula/s/sphinx-doc.rb
@@ -9,13 +9,13 @@ class SphinxDoc < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "022f66c695afc969866a4089c730c3a0819165fc07d0035512a9e760e96da2f7"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b2bac7d045434efe88d8145b316c7b44e6d0dbaeda6abf41445f72f25df46187"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "58b64d71e8d98e35815c38fdff25b538e466b88339d75ed152347a3e48a7564a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "6978da95ff0918618eadaefb114f026c31ea7e1f16252945e8f8a8b97124da2e"
-    sha256 cellar: :any_skip_relocation, ventura:        "39b34fd064913fffdc07582aa327c6fe0b04773266f94d695c7afde15b024c30"
-    sha256 cellar: :any_skip_relocation, monterey:       "b845851390d59d3a7515f769028d31624fefecd63cbf7546d0dcff121a62bc1c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "631735645fa5bc69afd34bf656db80c541204e7738a753fdca7c9702223a9700"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "48f9add3a85c1a56485192ae5bc3a0273002c5aa002fb9a0ab266ba54839e234"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7510d9cc7402f251d607b640e930b9aa427eb76b6a13fe18ae7fd0fc509af42d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ec7994aa88f304fecbbf50232c3fecaf918896631521faf5754e235075c8ddee"
+    sha256 cellar: :any_skip_relocation, sonoma:         "8bd577657867a31fe01d80d0c042a83de54022da4892060d1bd0cfd7589045e2"
+    sha256 cellar: :any_skip_relocation, ventura:        "83cf6cc9da26829f5d652e80d34eaa521c542f34990a76cb47b58bd94083fa90"
+    sha256 cellar: :any_skip_relocation, monterey:       "e1347bab6b2a5a289013017540bac056fe84c1e454d00993ed2c016a55670172"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2666598ef4ab7764401772b326ea25bd0c6539c0060349fff429f2644745433a"
   end
 
   keg_only <<~EOS
