e345d84c6adf2c9133a3c24cd7aaef2a77fc4161
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-22 10:19:18 +0000

    sigma-cli: update 1.0.2_1 bottle.

--- Formula/s/sigma-cli.rb
+++ Formula/s/sigma-cli.rb
@@ -10,13 +10,13 @@ class SigmaCli < Formula
   head "https://github.com/SigmaHQ/sigma-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "6b6e701817b106320edb36fec0cf1510f82c8c49e55ec94fffeaeeaf7068480a"
-    sha256 cellar: :any,                 arm64_ventura:  "814e141c723946566290a9fa2b4d8af367e3c766c19af2c213566d1877ac8632"
-    sha256 cellar: :any,                 arm64_monterey: "7693bea3f840d315573968ca20c7ab5af80a77400b1ec36063ac16efdd07110b"
-    sha256 cellar: :any,                 sonoma:         "76edd487f5668b1c65dabd348e5059a7b09ce43af74c1c8eed1665ced2712ce7"
-    sha256 cellar: :any,                 ventura:        "f5d26f222f1800fa6079eadadc513fe255fc34e7208f6d98ac49c4d02ec6746a"
-    sha256 cellar: :any,                 monterey:       "a10d17679c2253a82e387deb3ecea12f9a69d7afd8d6922c72d18aa13ccd8d26"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e98fb83cf6151163c436b4e9554f45fa910f066fbff4bd030d70bcbd372072d1"
+    sha256 cellar: :any,                 arm64_sonoma:   "568b0b540e6b4b1e39086924b8248a3941eff86cda7e9b06160d83717fb959f3"
+    sha256 cellar: :any,                 arm64_ventura:  "db5fd465095a4fe2a40bfd104abac95f3b75795d6c3350f70112f5bc09e948c8"
+    sha256 cellar: :any,                 arm64_monterey: "230bdeead57d6a895214015dd211d6ce76eaa8171f0a80f19d42df3efbf8abb7"
+    sha256 cellar: :any,                 sonoma:         "4738dbbb9d5c9089ac2f1605a9440fe5bed3ed1ec518b7ddfbed0b25ba556ab2"
+    sha256 cellar: :any,                 ventura:        "5a13a5a5b222c788d498fde6b5a59ce3ce02b143189e00272d0ab4742c7ceede"
+    sha256 cellar: :any,                 monterey:       "3e3e70e3cb829a0d5c569efdbe1e805a6d662952e60757e562d8364d3b0ece47"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8ba3e30c3d176641410b1a353e50c2b2dcc729b2731263b7e37bae0280b6e350"
   end
 
   depends_on "certifi"
