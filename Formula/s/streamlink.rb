2b42bce2ba54e422970338d8b794826a5cfba2e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-22 19:26:22 +0000

    streamlink: update 6.7.4_1 bottle.

--- Formula/s/streamlink.rb
+++ Formula/s/streamlink.rb
@@ -10,13 +10,13 @@ class Streamlink < Formula
   head "https://github.com/streamlink/streamlink.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "8c423b9c7e3ca766bf64bc5c7eb2e9f40478394522d14c6753c11a30683706dc"
-    sha256 cellar: :any,                 arm64_ventura:  "d63eb4077faee39d57c1645cb51e64915964789f2c96d87b43d0a4107ebce2ba"
-    sha256 cellar: :any,                 arm64_monterey: "5414c880ba142436f8b90e0f92cc8750b805356418e146ed719dcc1d69708f7b"
-    sha256 cellar: :any,                 sonoma:         "b4bb7d8e2618d58cf710a71cc21dfa6f9794dcd5e43e02439cef5f5d8a6f4877"
-    sha256 cellar: :any,                 ventura:        "c1e77ee2c63a5b51e7d2eed4034a4e0e4450e34f025cfb3fc72f450d50b1d9e6"
-    sha256 cellar: :any,                 monterey:       "9d55aca7a1a5080e3c7dd7b5c82504c9e36074aee0dd889268418a6811e1fdd1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d80684abb874c621921f5a9eae6574c2e526d79839564ab65c018fa2d4a90981"
+    sha256 cellar: :any,                 arm64_sonoma:   "996c805b2cbf94d83562f61eff65487b2753544167d455d9feeb29b543ba71a0"
+    sha256 cellar: :any,                 arm64_ventura:  "959881d11771d6cf22c46d7a0f3d9e79a332476f0d89d0fb48112b3abd610cf7"
+    sha256 cellar: :any,                 arm64_monterey: "721345ec76d8663004eb0a5b11e6548b89670bc740f2243dd12fd7a3cf737cbf"
+    sha256 cellar: :any,                 sonoma:         "fa9887d3e7fa6fe4081352fee2826b905e85821aaceff52b8eeecaf098b91553"
+    sha256 cellar: :any,                 ventura:        "b8ce0d82f1030e9efcba23f1f0184f74979d72019957f288535ba4e05b9e570c"
+    sha256 cellar: :any,                 monterey:       "76f2ec0e84c5af02dad6308a37743d6664617aa030e6e9e7a2482acfcfaba09a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "229d0997bf8243f8c9bf8f54fca16486da5eb4e98b016f0e6a62a0cf6d3acf14"
   end
 
   depends_on "certifi"
