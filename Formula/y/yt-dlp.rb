f2a1ce75cb664802c0881432ee9570b606dc802b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-22 21:25:42 +0000

    yt-dlp: update 2024.4.9_2 bottle.

--- Formula/y/yt-dlp.rb
+++ Formula/y/yt-dlp.rb
@@ -9,13 +9,13 @@ class YtDlp < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6a62c50f8b025430a3d24c3e71f578fe0f1621e36e9a3453ef0d8c20a8d38642"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1119d348452eed057adfb373ec265d5bec0bec1882ad5d8858068d84ea024a09"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "13104118842e21ae42e52030c80d7c25c507b37e1d53b92c3a14f6bb5d009d8f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "aed735dcbc238344e7d19bf28a2c97816cc48cadbf4a899c35cef290a8cc2a84"
-    sha256 cellar: :any_skip_relocation, ventura:        "031811f4256499239512b5a730beb2bbe4a435ff719029ff1a82b228f99c4c0c"
-    sha256 cellar: :any_skip_relocation, monterey:       "a2104dd5337a3eb8ff34833f9fc587a07f40a462d1a9190328048a5901523a08"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e8b325c9bd2313cdd4a1c47c46eebcfcda82608d0259c2695fb6387d249dd453"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bfa845d154114b0ffa981c32432cdfc69221ed905cfbe20557c1daffca873cf1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "23da52c4bffad80a4f24a3e520ee366e0308613ac7c376d139af9a753a95775c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6da489a9dead040c9a15501db4c30fac10721bc659bf4b1d599e1aa23e6e4fc1"
+    sha256 cellar: :any_skip_relocation, sonoma:         "bbbd0160b493a8197938f6ee24fddb8fbbb4c9e42f43a30c56f8842fd4b398f2"
+    sha256 cellar: :any_skip_relocation, ventura:        "4ce1fb9e8894b690fa729064dd655aa9b107c0a623ed02910202146df3ec64c9"
+    sha256 cellar: :any_skip_relocation, monterey:       "fc9cf6f22d5b3707fc97eb817af61c6ca3f7881596f5f714b71c4b4ff4b3d617"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1841d565542c64414111fdfd7410d79926e1bcab2cbeeb1a610985507c1e9cf1"
   end
 
   head do
