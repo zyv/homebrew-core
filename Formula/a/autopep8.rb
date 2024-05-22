0511e612f6f4939081217e5444d0949c08c6efb6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-22 17:21:01 +0000

    autopep8: update 2.1.1 bottle.

--- Formula/a/autopep8.rb
+++ Formula/a/autopep8.rb
@@ -8,13 +8,13 @@ class Autopep8 < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "415d815c9b4e5ee07c93dd0cf243ba53d4a000fad65666282860584ccc6c91b6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "415d815c9b4e5ee07c93dd0cf243ba53d4a000fad65666282860584ccc6c91b6"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "415d815c9b4e5ee07c93dd0cf243ba53d4a000fad65666282860584ccc6c91b6"
-    sha256 cellar: :any_skip_relocation, sonoma:         "415d815c9b4e5ee07c93dd0cf243ba53d4a000fad65666282860584ccc6c91b6"
-    sha256 cellar: :any_skip_relocation, ventura:        "415d815c9b4e5ee07c93dd0cf243ba53d4a000fad65666282860584ccc6c91b6"
-    sha256 cellar: :any_skip_relocation, monterey:       "415d815c9b4e5ee07c93dd0cf243ba53d4a000fad65666282860584ccc6c91b6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "42c6fabcffce03e577b1ba5417255ab1a80206828aea8bc1286a82b5bbe66e69"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "221d06470dde7031f83ea1770521f0b0c6736539dbb438c6f3d3e6de90702b48"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b62d7d4a99b876276efcb110a28228717b9fe02fe4f215906607abe28350e135"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d6ab9266be16e905428377c5bd053087278b6249922bdc7e2d09ec9a65b0f214"
+    sha256 cellar: :any_skip_relocation, sonoma:         "1d056c9d590c3831d0e992e20c7f1daf2328783a4221c01485f4bb7c361498b1"
+    sha256 cellar: :any_skip_relocation, ventura:        "2fc9ffc07a604842734af0a2fbfa2cd8e0d7b7b0c3b87449526c2cd626483799"
+    sha256 cellar: :any_skip_relocation, monterey:       "bb8dbd435d8153b37bf5276456e1bb6b618f2142bbd51c52cef0215cc415f1d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bb2b7bed34e7255105e4ba121a041aa419c973aab579b1ad6bf7b09980158222"
   end
 
   depends_on "python@3.12"
