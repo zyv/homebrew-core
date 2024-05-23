2cb4c19290393ebb8bd55cc91c1a8b74ec80dcc8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-23 03:44:03 +0000

    php-code-sniffer: update 3.10.1 bottle.

--- Formula/p/php-code-sniffer.rb
+++ Formula/p/php-code-sniffer.rb
@@ -6,13 +6,13 @@ class PhpCodeSniffer < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d2a0788487e78fcdd143003bcba72b4f6b3b519bb15df70322821431b4ab3c35"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1e212bc24fcebc5620c6954747fdbbf5b2817bc316d77891911e8f7c9b9821fc"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "063c9e95791066cc86fc83d8bb35eb033171f31b08956479e78640ff060b58c5"
-    sha256 cellar: :any_skip_relocation, sonoma:         "fd461ab85880d2ff90f1cf2c01dfdb9edf460ec5482b54f53afd64a393a45769"
-    sha256 cellar: :any_skip_relocation, ventura:        "6cac86808a9a7a19ed0233639030c3dcfb6646d2dc8271ae6e19c1eb8dd1346d"
-    sha256 cellar: :any_skip_relocation, monterey:       "757f64852da179ba03ababef74a497a9e6ba196c46c3f447f2687433ff281d71"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a60cf66992e5586cb3af617adbfa7e01fc82b9ad89cc87a4a8f8b0e848ce92d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d2bd0e30c9e5516e76aafdfa4b71ca44ef23e6722b0407cf41e0d559653b019b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5a0c74b70622527afa5915133073577597d5ac6551c4fff7b0d908c80bce5969"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fffa1a2a53c59758986777968ab8848605a692856c34008694d7180b4cde1eb2"
+    sha256 cellar: :any_skip_relocation, sonoma:         "173c759de0133cf7997863f35b126cadf18f6ba08ad3118c428319fa71c53043"
+    sha256 cellar: :any_skip_relocation, ventura:        "4c355847ef5448a3b597646ef9a1458bc6736a2a15c1087263676f2a979164ce"
+    sha256 cellar: :any_skip_relocation, monterey:       "d44ff452131376e8eb8086f2ec7d5b34e96cead7908bb449dd949132b5b6432c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c767b4df265c21a1a4ccb607838737b374bef4d0eb972dd61c8dc7d8487e328e"
   end
 
   depends_on "php"
