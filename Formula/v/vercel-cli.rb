d1133b73184fe91dc187a59c22120ad25c9ef629
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-23 12:34:28 +0000

    vercel-cli: update 34.2.1 bottle.

--- Formula/v/vercel-cli.rb
+++ Formula/v/vercel-cli.rb
@@ -8,13 +8,13 @@ class VercelCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "93ce115a00d6d44b4802006f07638e8126f5f6c58bdf6885deee23448d176718"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "edbf7d445e88eb5027d03c7ba659733125789a0f34917cbe864a01c039aecb49"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6da7ae9ff35ad93d2ce73b4f897fc83ceda1e5ea0b80e268af4b295788e43e4f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "ce153b64ccf1d50229c0774b07dd0a42e2046761def05a9178b475d0cee5b750"
-    sha256 cellar: :any_skip_relocation, ventura:        "300517fa93c58ee4a62398e4b2e22ec7e1ab6ecfe9d8755a3e8b0d8271f281e2"
-    sha256 cellar: :any_skip_relocation, monterey:       "a52fcc0a1d532065d6ae4b11cb0a1c3db6f8a614ab40a327e94832ebfac094c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee12f7654150f9d947281b0cca5cc6713e22ee8812a25c459b2523d85d7647a8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "20d54bf598033cf902e1b53b85f94783171d551e0673c65e5f074eb85cf45168"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e0955a9af73ec4a95be9713982c6c70caa316ccd43b70e89ae44d7d363c13e18"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fd223ef148d873deb3f5a46088cf203f4b4861969170ab84bf9dd600f411baae"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ad8edd97e65342f08619e1c2a1f6cc060e5ee42e65d62c6496e693febe221d29"
+    sha256 cellar: :any_skip_relocation, ventura:        "bdda313717e587e5dd3fef6b259ea54a33581341b15496eb66fd1a75164e597a"
+    sha256 cellar: :any_skip_relocation, monterey:       "f97471fdd09b83cb0e616e9f82388d7e07dc8169001e254ab7feab4c1344802e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0cd81f47e9577481608cd811181657a92f6dfa6524188c56efabf6c512fb2149"
   end
 
   depends_on "node"
