02133a1f1c45eb609ecaad54867b30a4f62335c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-25 19:35:59 +0000

    maven: update 3.9.7 bottle.

--- Formula/m/maven.rb
+++ Formula/m/maven.rb
@@ -12,13 +12,13 @@ class Maven < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6ff7772d39d4ca91511aba210961c89e042297f42277213f6a658e7874405392"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6ff7772d39d4ca91511aba210961c89e042297f42277213f6a658e7874405392"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6ff7772d39d4ca91511aba210961c89e042297f42277213f6a658e7874405392"
-    sha256 cellar: :any_skip_relocation, sonoma:         "21ada71ddbdf8f184165ac8f64e4dbf1373beec79c0781e16f0e5b407a99d2dd"
-    sha256 cellar: :any_skip_relocation, ventura:        "21ada71ddbdf8f184165ac8f64e4dbf1373beec79c0781e16f0e5b407a99d2dd"
-    sha256 cellar: :any_skip_relocation, monterey:       "21ada71ddbdf8f184165ac8f64e4dbf1373beec79c0781e16f0e5b407a99d2dd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6ff7772d39d4ca91511aba210961c89e042297f42277213f6a658e7874405392"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0e31233aec51e9b37653798984741209a44e769c8914aba43b9d62955fa6e588"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2ecc0f30c8411183c4121022589b65ee6ba9944e483fa8205c5a8185faff5ea8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ef7c2e9b52a57117fa0b52d44984598da24fc8e94eacff8354d50712c7d4915c"
+    sha256 cellar: :any_skip_relocation, sonoma:         "eac400de7adb33167cccc137f3350d859092ca4b43a1048d1f92a90e89abc63c"
+    sha256 cellar: :any_skip_relocation, ventura:        "dd488fd2cc5fc663b4187b1273e0b8b0a9fe510fc4f0fc8b0adaaeaee89cb932"
+    sha256 cellar: :any_skip_relocation, monterey:       "240f0b40a436c827d062016c3ffbf677f3fa77caca41b2ced9fcdedaf18cf39e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "20b539139ee4c37f58643fad4733075874085ee0236b33660754981f08c2db3e"
   end
 
   depends_on "openjdk"
