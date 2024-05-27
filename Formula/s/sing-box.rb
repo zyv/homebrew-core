220641971e7df26fc288c6a4d82350b7d92d62f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-27 03:49:37 +0000

    sing-box 1.9.0

--- Formula/s/sing-box.rb
+++ Formula/s/sing-box.rb
@@ -2,8 +2,8 @@ class SingBox < Formula
   desc "Universal proxy platform"
   homepage "https://sing-box.sagernet.org"
   # using `:homebrew_curl` to work around audit failure from TLS 1.3-only homepage
-  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.8.14.tar.gz", using: :homebrew_curl
-  sha256 "2ba7cfa097f5963ba304d47606e7a6b61bf881eb86cbed78fa6e4efae44a0a5f"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.9.0.tar.gz", using: :homebrew_curl
+  sha256 "cb1d91e362f4dd7c35f7bb040514414861a045a76301af8257134c65f7a45c36"
   license "GPL-3.0-or-later"
 
   bottle do
