c11fc070b76de4efc22c6ad9d92aad3792b4aba4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-28 21:38:46 +0000

    byteman: update 4.0.23 bottle.

--- Formula/b/byteman.rb
+++ Formula/b/byteman.rb
@@ -12,7 +12,13 @@ class Byteman < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f08c5658f72ec1cf550c83af45cdcb1b346499db993d1d7feba063943f9ded2a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "aac8e27679828cb79920b27d0e134477c136f3a2cad9fff8e4c1676401fa5736"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "aac8e27679828cb79920b27d0e134477c136f3a2cad9fff8e4c1676401fa5736"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "aac8e27679828cb79920b27d0e134477c136f3a2cad9fff8e4c1676401fa5736"
+    sha256 cellar: :any_skip_relocation, sonoma:         "aac8e27679828cb79920b27d0e134477c136f3a2cad9fff8e4c1676401fa5736"
+    sha256 cellar: :any_skip_relocation, ventura:        "aac8e27679828cb79920b27d0e134477c136f3a2cad9fff8e4c1676401fa5736"
+    sha256 cellar: :any_skip_relocation, monterey:       "aac8e27679828cb79920b27d0e134477c136f3a2cad9fff8e4c1676401fa5736"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "25a8d6c3fab2ebafd41e9663e8881c9934ad850b428f467404de710997f6fdf8"
   end
 
   depends_on "openjdk"
