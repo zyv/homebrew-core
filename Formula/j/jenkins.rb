1e3f52321f3f1fa7276c71cac2199f11a94db602
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-28 15:41:12 +0000

    jenkins 2.460

--- Formula/j/jenkins.rb
+++ Formula/j/jenkins.rb
@@ -1,8 +1,8 @@
 class Jenkins < Formula
   desc "Extendable open source continuous integration server"
   homepage "https://www.jenkins.io/"
-  url "https://get.jenkins.io/war/2.459/jenkins.war"
-  sha256 "38fba7e40fc9cc2122efe54e9218a96c67483b4584b1c060017071efe0e118aa"
+  url "https://get.jenkins.io/war/2.460/jenkins.war"
+  sha256 "019bd48783af961a129d2a9a89808e5e8eac59fc02c7ca2effc9c987385c7cc4"
   license "MIT"
 
   livecheck do
