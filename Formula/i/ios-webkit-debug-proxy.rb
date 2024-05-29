6b8a8a0f11eb309e89e7f3665ef9dde90128bc88
Rui Chen <rui@chenrui.dev>
2024-05-28 20:05:23 -0400

    ios-webkit-debug-proxy 1.9.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    ios-webkit-debug-proxy: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    ios-webkit-debug-proxy: declare indirect deps with linkage
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    ios-webkit-debug-proxy: update linux test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/i/ios-webkit-debug-proxy.rb
+++ Formula/i/ios-webkit-debug-proxy.rb
@@ -1,10 +1,9 @@
 class IosWebkitDebugProxy < Formula
   desc "DevTools proxy for iOS devices"
   homepage "https://github.com/google/ios-webkit-debug-proxy"
-  url "https://github.com/google/ios-webkit-debug-proxy/archive/refs/tags/v1.9.0.tar.gz"
-  sha256 "ba9bb2feaa976ad999e9e405d8cd8794cdf3546130a79f4785235200ead3c96c"
+  url "https://github.com/google/ios-webkit-debug-proxy/archive/refs/tags/v1.9.1.tar.gz"
+  sha256 "6b7781294cc84d383c7e7ecd05af08ca8d9b2af7a49ba648178ae4d84837c52b"
   license "BSD-3-Clause"
-  revision 3
   head "https://github.com/google/ios-webkit-debug-proxy.git", branch: "master"
 
   bottle do
@@ -22,14 +21,8 @@ class IosWebkitDebugProxy < Formula
   depends_on "pkg-config" => :build
   depends_on "libimobiledevice"
   depends_on "libplist"
+  depends_on "libusbmuxd"
   depends_on "openssl@3"
-
-  # Patch ios_webkit_debug_proxy to work with libplist 2.3.0
-  # Remove this once ios_webkit_debug_proxy gets a new release.
-  patch do
-    url "https://github.com/google/ios-webkit-debug-proxy/commit/94e4625ea648ece730d33d13224881ab06ad0fce.patch?full_index=1"
-    sha256 "39e7c648f1ecc96368caa469bd9aa0a552a272d72fafc937210f10d0894551e6"
-  end
 
   def install
     system "./autogen.sh", *std_configure_args
@@ -37,6 +30,11 @@ class IosWebkitDebugProxy < Formula
   end
 
   test do
+    assert_match version.to_s, shell_output("#{bin}/ios_webkit_debug_proxy --version")
+
+    # Fails in Linux CI with "`No device found, is it plugged in?`"
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
     base_port = free_port
     (testpath/"config.csv").write <<~EOS
       null:#{base_port},:#{base_port + 1}-#{base_port + 101}
