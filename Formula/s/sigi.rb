236a79c2d1da5403968ff316d5abbac94a052c30
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-29 14:30:51 +0000

    sigi: update 3.7.0 bottle.

--- Formula/s/sigi.rb
+++ Formula/s/sigi.rb
@@ -7,13 +7,13 @@ class Sigi < Formula
   head "https://github.com/sigi-cli/sigi.git", branch: "core"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "720f2b3d7db7549d9971cc1e6b06600fd4093ddf79f8930c5b231744fe96a7aa"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f4e8c67b3aed0bdda931fb117f499f6a23b0c3f7d908be500937551de386c8f9"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f182faeae4553760dfdcc17f0ea9364e2e7247f3432d92a25a41100fe1ec8f14"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a8a9b22a2f70df6648d4bf188ef119c8d2425be1bf8d7a958c8ae1556f51dd21"
-    sha256 cellar: :any_skip_relocation, ventura:        "46ad59f3b76ed015b310f1f45a8c3b4554c9eea9b26489baedc7db11103d3726"
-    sha256 cellar: :any_skip_relocation, monterey:       "183f2a38088157ddaed35b702bffcb6f87905c26a1ea9e9e9be2d8a0cf8a8f65"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "91d48b85d3a4962aad922b3877bcf860178bd94eccec9a2a46571365e7c1997e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9ce5392cefa3f7b743fa91bd13525fc602d8bc4289e4b4f49687fb24c73c1c0b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9732c37a1092aa6dd5f1e7bb15492a6450b7d9d256d1728a7b8255bcec4f8222"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "18fb2ecebd41c576e0b4e8aeb04121f924334157c01f576b860e2f234e523c53"
+    sha256 cellar: :any_skip_relocation, sonoma:         "3b867c702c536bb0243978b4e1a9e08c3e6a9650429b115baaf838bbcd01478b"
+    sha256 cellar: :any_skip_relocation, ventura:        "4151fa5bc0db6b3cbd11102c8ce8ba731fecf161cc533652bb3be0539562555e"
+    sha256 cellar: :any_skip_relocation, monterey:       "10c2e66baa41005b3a96a1212745bd18e6dbc871c951608281b1d3d8573b9f4d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d063619f613046b9203e321135443b92300f5f00a2fcf50933448d368efa87c2"
   end
 
   depends_on "rust" => :build
