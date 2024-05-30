a3ccc8f107f33f32124903135bc1ccbec9a90153
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-30 23:52:36 +0000

    brew-gem: update 1.2.0 bottle.

--- Formula/b/brew-gem.rb
+++ Formula/b/brew-gem.rb
@@ -15,7 +15,13 @@ class BrewGem < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "fc319ba05f5f17b0f516292f5fb2d55eccb6c03a11cacc438b1c2c2fb5ccb0db"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "694721fcbd4b8c2c8d7db6135ca73aadf0cfe63646a1bfa9dc917d2fd138597a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "694721fcbd4b8c2c8d7db6135ca73aadf0cfe63646a1bfa9dc917d2fd138597a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "694721fcbd4b8c2c8d7db6135ca73aadf0cfe63646a1bfa9dc917d2fd138597a"
+    sha256 cellar: :any_skip_relocation, sonoma:         "694721fcbd4b8c2c8d7db6135ca73aadf0cfe63646a1bfa9dc917d2fd138597a"
+    sha256 cellar: :any_skip_relocation, ventura:        "694721fcbd4b8c2c8d7db6135ca73aadf0cfe63646a1bfa9dc917d2fd138597a"
+    sha256 cellar: :any_skip_relocation, monterey:       "694721fcbd4b8c2c8d7db6135ca73aadf0cfe63646a1bfa9dc917d2fd138597a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5249679d614de6163b40a3a676d3895635ca03c916b35837c0d8ad701ecbd71a"
   end
 
   uses_from_macos "ruby"
