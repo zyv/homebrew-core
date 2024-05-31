509c7207eb5062cf6394c904150f9c22a98bfd13
Rui Chen <rui@chenrui.dev>
2024-05-30 23:46:00 -0400

    gtksourceview5 5.12.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/gtksourceview5.rb
+++ Formula/g/gtksourceview5.rb
@@ -1,8 +1,8 @@
 class Gtksourceview5 < Formula
   desc "Text view with syntax, undo/redo, and text marks"
   homepage "https://projects.gnome.org/gtksourceview/"
-  url "https://download.gnome.org/sources/gtksourceview/5.12/gtksourceview-5.12.0.tar.xz"
-  sha256 "daf32ff5d3150d6385917d3503a85b9e047ba158b2b03079314c9c00813fa01f"
+  url "https://download.gnome.org/sources/gtksourceview/5.12/gtksourceview-5.12.1.tar.xz"
+  sha256 "84c82aad985c5aadae7cea7804904a76341ec82b268d46594c1a478f39b42c1f"
   license "LGPL-2.1-or-later"
 
   livecheck do
