7036a44b24c6def1f03e34edb627ecc662f6618d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-31 03:41:12 +0000

    jhipster 8.5.0

--- Formula/j/jhipster.rb
+++ Formula/j/jhipster.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Jhipster < Formula
   desc "Generate, develop and deploy Spring Boot + Angular/React applications"
   homepage "https://www.jhipster.tech/"
-  url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-8.4.0.tgz"
-  sha256 "5d9ddde6054c9b2c4a55907886fb97c1504b9370679ae00e59f5321c265b967c"
+  url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-8.5.0.tgz"
+  sha256 "5c40bb3225142e9536a8262b4844470fa52e31cad580de6830f26a3cc5feee66"
   license "Apache-2.0"
 
   bottle do
